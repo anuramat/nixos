957ba11071868e5c06e7888a82b1e03e676d38c2
Arsen Nuramatov <anuramat@pm.me>
2024-12-28 06:37:36 +0100

    auto: anuramat-ll7

--- config/bash/config.d/00-pandoc.sh
+++ config/bash/config.d/00-pandoc.sh
@@ -16,28 +16,28 @@ pandoc-md() {
 hotdoc() {
 	# renders $1.md to pdf, opens in zathura, rerenders on save
 	# usage: $0 $target
-
-	# create file
-	local -r path="$(mktemp -p /tmp XXXXXXXX.pdf)"
+	local -r md=$(realpath $1)
+	local -r pdf="$(mktemp -p /tmp XXXXXXXX.pdf)"
 	# initialize it with a basic pdf so that zathura doesn't shit itself
 	echo 'JVBERi0xLgoxIDAgb2JqPDwvUGFnZXMgMiAwIFI+PmVuZG9iagoyIDAgb2JqPDwvS2lkc1szIDAgUl0vQ291bnQgMT4+ZW5kb2JqCjMgMCBvYmo8PC9QYXJlbnQgMiAwIFI+PmVuZG9iagp0cmFpbGVyIDw8L1Jvb3QgMSAwIFI+Pg==' \
-		| base64 -d > "$path"
+		| base64 -d > "$pdf"
+	# trap interrupts to do the cleanup
+	trap 'echo trapped SIGINT' INT
 
 	# open zathura
-	zathura "$path" &> /dev/null &
+	nohup zathura "$pdf" &> /dev/null &
 	local -r zathura_pid="$!"
 
 	# start watching, recompile on change
 	export -f pandoc-md
-	cmd=$(printf 'pandoc-md "%s" "%s"' "$(realpath $1)" "$path")
-	(echo "$1" | entr -cs "$cmd") &
-	# for some reason -r flag for entr breaks stuff!!! XXX
+	local -r cmd=$(printf 'pandoc-md "%s" "%s"' "$md" "$pdf")
+	entr -rcsn "$cmd" < <(echo "$md") &
 	local -r entr_pid="$!"
 
 	# stop watching if zathura is closed
 	wait "$zathura_pid"
 	kill "$entr_pid"
 
-	# clean up
-	rm "$path"
+	echo cleaning...
+	rm "$pdf"
 }
