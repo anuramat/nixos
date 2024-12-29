35005c375971aa00faf237f01f6d0082176cd999
Arsen Nuramatov <anuramat@pm.me>
2024-12-29 14:53:09 +0100

    auto: anuramat-ll7

--- config/git/merge_sets.sh
+++ config/git/merge_sets.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+export LC_ALL=C
+
 fst=$(sort -u "$1")
 root=$(sort -u "$2")
 snd=$(sort -u "$3")
 
 all=$(printf "%s\n" "$fst" "$snd" | sort -u)
-common=$(comm -12 <(echo "$fst") <(echo "$snd") | sort -u)
-deleted=$(comm -23 <(echo "$root") <(echo $common))
+intersection=$(comm -12 <(echo "$fst") <(echo "$snd") | sort -u)
+deleted=$(comm -23 <(echo "$root") <(echo "$intersection") | sort -u)
+res=$(comm -23 <(echo "$all") <(echo "$deleted"))
 
-echo "$deleted" | grep -vxF -f - <(printf '%s' "$all") | awk NF | sort -u > "$1"
+echo "$res" | awk NF | sort -u
