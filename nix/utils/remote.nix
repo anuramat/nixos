6fe75c302d0c9f57f21136022fb9d234d6b5af7f
Arsen Nuramatov <anuramat@pm.me>
2025-01-02 04:45:25 +0100

    auto: anuramat-ll7

--- nix/utils/remote.nix
+++ nix/utils/remote.nix
@@ -1,12 +1,18 @@
 { user, ... }:
+let
+  name = "remotebuild";
+in
 {
-  users.users.remotebuild = {
+  users.users.${name} = {
     isNormalUser = true;
     createHome = false;
-    group = "remotebuild";
+    group = name;
     openssh.authorizedKeys.keys = user.keys;
   };
-  users.groups.remotebuild = { };
+  users.groups.${name} = { };
+  services.openssh.settings.AllowUsers = [
+    name
+  ];
   # not sure if we need this
-  # nix.settings.trusted-users = [ "remotebuild" ];
+  # nix.settings.trusted-users = [ name ];
 }
