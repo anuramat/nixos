196f0524a6bb07ae40f35c8d05cc8112550a2e0a
Arsen Nuramatov <anuramat@pm.me>
2025-01-03 07:31:07 +0100

    auto: anuramat-ll7

--- nix/machines/anuramat-ll7/out.nix
+++ nix/machines/anuramat-ll7/out.nix
@@ -2,4 +2,5 @@
   builder = true;
   cacheKey = "anuramat-ll7:aFFmygZTV872vjBs+mugpBgkTObki/bi5xfJspLKeSc=";
   sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIORDkTNsAaKxMF/VIfoI+FXvcLARbswddfqtHNkuTsxR anuramat-ll7";
+  hostKeys = builtins.readFile ./hostkeys;
 }
--- nix/user.nix
+++ nix/user.nix
@@ -4,12 +4,27 @@
   lib,
 }:
 let
+  inherit (builtins)
+    filter
+    attrValues
+    concatStringsSep
+    mapAttrs
+    attrNames
+    ;
+
   # TODO rename somehow idk
-  machinesValid = builtins.mapAttrs (n: v: v // { builder = v.builder or false; }) machines;
-  isBuilder = machinesValid.${hostname}.builder;
+  machinesValid = mapAttrs (
+    n: v:
+    v
+    // {
+      hostKeys = v.hostKeys or "";
+      sshKey = "";
+    }
+  ) machines;
+  isBuilder = machinesValid.${hostname}.builder or false;
   others = lib.attrsets.filterAttrs (n: v: n != hostname) machinesValid;
   builders = lib.attrsets.filterAttrs (n: v: v.builder or false) others;
-  builderHostnames = builtins.attrNames builders;
+  builderHostnames = attrNames builders;
 in
 {
   # WARN don't fuck this up
@@ -21,9 +36,11 @@ in
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl0YHcx+ju+3rsPerkAXoo2zI4FXRHaxzfq8mNHCiSD anuramat-iphone16"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBre248H/l0+aS5MJ+nr99m10g44y+UsaKTruszS6+D anuramat-ipad"
-  ] ++ map (x: x.sshKey) (builtins.attrValues others);
+  ] ++ (others |> attrValues |> filter (x: x ? sshKey) |> map (x: x.sshKey));
   # TODO disentangle ssh keys, move to builder_config.nix or something idk
   substituters = map (x: "ssh-ng://${x}") builderHostnames;
-  trusted-public-keys = map (x: x.cacheKey) (builtins.attrValues builders);
+  trusted-public-keys = map (x: x.cacheKey) (attrValues builders);
   builderUsername = "builder";
+  hostKeys =
+    others |> attrValues |> filter (x: x ? hostKeys) |> map (x: x.hostKeys) |> concatStringsSep "\n";
 }
