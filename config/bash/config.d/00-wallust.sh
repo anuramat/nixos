75c0c985d8532b47780298d7b6939dca4d034c32
Arsen Nuramatov <anuramat@pm.me>
2025-01-10 14:51:13 +0100

    auto: anuramat-t480

--- config/bash/config.d/00-wallust.sh
+++ config/bash/config.d/00-wallust.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-__wallust_cs_dir="$XDG_CONFIG_HOME/wallust/colorschemes"
-
 # set wallust theme, and apply post hooks
 __wallust_wrapped() {
+	local -r __wallust_cs_dir="$XDG_CONFIG_HOME/wallust/colorschemes"
+	local -r __wallust_base_cmd="wallust -q"
+
 	if [ "$1" = "sex" ]; then
-		wallust cs -f terminal-sexy "$__wallust_cs_dir/$2.json" || return
+		$__wallust_base_cmd cs -f terminal-sexy "$__wallust_cs_dir/$2.json" || return
 	else
-		wallust "$@" || return
+		$__wallust_base_cmd "$@" || return
 	fi
+
 	(
 		cd "$XDG_CONFIG_HOME/mako" || {
 			echo "Couldn't cd to the mako folder, skipping"
@@ -17,5 +19,9 @@ __wallust_wrapped() {
 		cat main.conf generated-colors.conf apps.conf > "./config"
 		makoctl reload
 	)
+
+	# BUG reload somehow fucks up the terminal, sleep somehow helps
+	# probably keyd causes problems
+	sleep 0.03
 	swaymsg reload
 }
