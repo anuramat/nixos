91e1802c51763744d86e557b7b7b56b34b3f3b11
Arsen Nuramatov <anuramat@pm.me>
2025-02-18 18:38:33 +0100

    auto: anuramat-t480

--- config/nvim/lua/adhoc/greeter.lua
+++ config/nvim/lua/adhoc/greeter.lua
@@ -23,12 +23,17 @@ local function figlet(text, font)
   return figlet_res.stdout
 end
 
+-- returns a version string
+---@return string
 local function version_string()
   local version = vim.version()
   local nvim_version_info = ' ' .. version.major .. '.' .. version.minor .. '.' .. version.patch
   return nvim_version_info
 end
 
+-- adds a prefix to every string in a list
+---@param lines string[]
+---@param prefix string
 local function add_prefix(lines, prefix)
   local result = {}
   for i, v in ipairs(lines) do
@@ -37,6 +42,9 @@ local function add_prefix(lines, prefix)
   return result
 end
 
+-- returns a list of empty strings
+---@param n integer
+---@return string[]
 local function empty(n)
   local result = {}
   for _ = 1, n do
@@ -45,8 +53,9 @@ local function empty(n)
   return result
 end
 
--- calc padding, add version string
-local function pook()
+-- returns a function that builds lines for the greeter
+---@return function
+local function make_renderer()
   local body = figlet(input)
   local ver_string = version_string()
   local lines = vim.split(body, '\n', { trimempty = true })
@@ -87,36 +96,54 @@ local function pook()
   end
 end
 
--- TODO create autocommand to redraw on resize
+-- TODO maybe cache body
 
+-- maps keys to noop
+---@param keys string
 local function unmap(keys)
   for i = 1, #keys do
     vim.api.nvim_buf_set_keymap(0, 'n', string.sub(keys, i, i), '<nop>', {})
   end
 end
 
-local render = pook()
+local render = make_renderer()
+
+local function set()
+  vim.opt_local.modifiable = true
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, render())
+  vim.opt_local.modifiable = false
+end
+
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
+    -- skip greeter when opening files
     if vim.fn.argc() ~= 0 then
       return
     end
-    vim.cmd('enew')
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, render())
 
-    -- vim.api.nvim_create_autocmd('WinLeave', {
-    --   callback = function()
-    --     vim.cmd('enew')
-    --   end,
-    -- })
+    -- greet
+    set()
 
+    -- when a new window is created, replace the greeter buffer with an empty buffer
+    local old = vim.fn.win_getid()
     vim.api.nvim_create_autocmd('WinNew', {
-      callback = function()
-        vim.cmd('enew')
-        vim.cmd('silent bd 1')
+      callback = function(ev)
+        local buf = vim.api.nvim_create_buf(true, false)
+        vim.api.nvim_win_set_buf(old, buf)
+        if ev.buf == 1 then
+          vim.api.nvim_win_set_buf(0, buf)
+        end
       end,
+      once = true,
     })
 
+    -- rerender on resize
+    vim.api.nvim_create_autocmd('VimResized', {
+      buffer = 0,
+      callback = set,
+    })
+
+    -- make it special
     vim.opt_local.bufhidden = 'wipe'
     vim.opt_local.buftype = 'nofile'
     vim.opt_local.cursorline = false
@@ -129,6 +156,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
     vim.opt_local.stl = ' '
     vim.opt_local.swapfile = false
 
+    -- some remaps
     vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<cmd>quit<cr>', {})
     vim.api.nvim_buf_set_keymap(0, 'n', 'i', '<cmd>enew<cr>i', {})
     vim.api.nvim_buf_set_keymap(0, 'n', 'a', '<cmd>enew<cr>a', {})
