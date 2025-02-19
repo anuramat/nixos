210785c7eadbfc7b99189b265a9a96cd7dbaa6a1
Arsen Nuramatov <anuramat@pm.me>
2025-02-19 20:50:30 +0100

    auto: anuramat-t480

--- config/bash/config.d/99-ps1.sh
+++ config/bash/config.d/99-ps1.sh
@@ -3,62 +3,44 @@
 # depends on _git_prompt defined outside
 
 __git() {
-	tput setaf 5
 	local -r prompt=$(_git_prompt)
-	[ -n "$prompt" ] && printf ' %s' "$prompt"
-	tput sgr0
+	printf '%s' "${prompt:+ $(tput setaf 5)$prompt$(tput sgr0)}"
 }
 
 __code() {
-	tput bold setaf 1
-	local err=$__last_return_code
-	[ "$err" -ne 0 ] && {
-		printf ' %s\n' "ERR:$err"
-	}
-	tput sgr0
+	local -r err=$__last_return_code
+	[ "$err" -ne 0 ] && printf ' %s\n' "$(tput bold setaf 1)ERR:$err"
+	tput sgr0 # TODO for some reason if we put this into a printf arg, it eats the newline???
 }
 
 __jobs() {
-	tput setaf 3
-	((__n_jobs > 0)) && {
-		printf %s " J:$__n_jobs"
-	}
-	tput sgr0
+	((__n_jobs > 0)) && printf %s " $(tput setaf 3)J:$__n_jobs$(tput sgr0)"
 }
 
 __time() {
-	tput bold setaf 7
-	printf %s " $(date +%H:%M)"
-	tput sgr0
+	printf %s " $(tput bold setaf 7)$(date +%H:%M)$(tput sgr0)"
 }
 
 __ssh() {
 	if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
 		printf ' %s\n' "ssh: $(whoami)@$(hostname)"
 	fi
-	tput sgr0
 }
 
 __path() {
-	tput bold
 	cwd=$(pwd)
 	len=${#HOME}
 	[[ $cwd == "$HOME"* ]] \
 		&& cwd="~${cwd:len}"
-	printf %s "$cwd"
-	tput sgr0
+	printf %s "$(tput bold)$cwd$(tput sgr0)"
 }
 
 __shlvl() {
-	tput setaf 2
-	[ -n "$SHLVL" ] && ((SHLVL > 1)) && printf %s " L$SHLVL"
-	tput sgr0
+	((SHLVL > 1)) && printf %s " $(tput setaf 2)L$SHLVL$(tput sgr0)"
 }
 
 __nix() {
-	tput setaf 2
-	[ -n "$IN_NIX_SHELL" ] && printf %s " $IN_NIX_SHELL"
-	tput sgr0
+	printf %s "${IN_NIX_SHELL:+ $(tput setaf 2)$IN_NIX_SHELL$(tput sgr0)}"
 }
 
 __set_vars() {
