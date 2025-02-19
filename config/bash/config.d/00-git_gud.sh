08d98efb35dd522508aa34629ea9c6b5dee5723e
Arsen Nuramatov <anuramat@pm.me>
2025-02-19 19:35:58 +0100

    auto: anuramat-t480

--- config/bash/config.d/00-git_gud.sh
+++ config/bash/config.d/00-git_gud.sh
@@ -162,7 +162,7 @@ __gup() {
 	fi
 
 	local prompt
-	prompt=$(_git_prompt 1) && echo "status:$prompt" # TODO refactor to not use cut?
+	echo "status: $(_git_prompt)" # TODO refactor to not use cut?
 	echo 'done'
 }
 
