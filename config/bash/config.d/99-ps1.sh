2d9e85cc77568a9f0284ecd75faa751efddb2cc7
Arsen Nuramatov <anuramat@pm.me>
2025-02-20 16:11:13 +0100

    auto: anuramat-t480

--- config/bash/config.d/99-ps1.sh
+++ config/bash/config.d/99-ps1.sh
@@ -2,32 +2,32 @@
 
 # depends on _git_prompt defined outside
 
-__git() {
+__git_ps1() {
 	local -r prompt=$(_git_prompt)
 	printf '%s' "${prompt:+ $(tput setaf 5)$prompt$(tput sgr0)}"
 }
 
-__code() {
+__code_ps1() {
 	local -r err=$__last_return_code
 	[ "$err" -ne 0 ] && printf ' %s\n' "$(tput bold setaf 1)ERR:$err"
 	tput sgr0 # TODO for some reason if we put this into a printf arg, it eats the newline???
 }
 
-__jobs() {
-	((__n_jobs > 0)) && printf %s " $(tput setaf 3)J:$__n_jobs$(tput sgr0)"
+__jobs_ps1() {
+	((__n_bg_jobs > 0)) && printf %s " $(tput setaf 3)J:$__n_bg_jobs$(tput sgr0)"
 }
 
-__time() {
+__time_ps1() {
 	printf %s " $(tput bold setaf 7)$(date +%H:%M)$(tput sgr0)"
 }
 
-__ssh() {
+__ssh_ps1() {
 	if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
 		printf ' %s\n' "ssh: $(whoami)@$(hostname)"
 	fi
 }
 
-__path() {
+__path_ps1() {
 	cwd=$(pwd)
 	len=${#HOME}
 	[[ $cwd == "$HOME"* ]] \
@@ -35,25 +35,25 @@ __path() {
 	printf %s "$(tput bold)$cwd$(tput sgr0)"
 }
 
-__shlvl() {
+__shlvl_ps1() {
 	((SHLVL > 1)) && printf %s " $(tput setaf 2)L$SHLVL$(tput sgr0)"
 }
 
-__nix() {
+__nix_ps1() {
 	printf %s "${IN_NIX_SHELL:+ $(tput setaf 2)$IN_NIX_SHELL$(tput sgr0)}"
 }
 
 __set_vars() {
 	__last_return_code=$?
-	__n_jobs=$(jobs | wc -l)
+	__n_bg_jobs=$(jobs | wc -l)
 }
 precmd_functions=(__set_vars "${precmd_functions[@]}")
 
 PS1=''
-PS1+='$(__code)'
+PS1+='$(__code_ps1)'
 PS1+='\n'
-PS1+='$(__ssh)'
-PS1+=' $(__path)$(__git)$(__shlvl)$(__nix)$(__time)$(__jobs)\n'
+PS1+='$(__ssh_ps1)'
+PS1+=' $(__path_ps1)$(__git_ps1)$(__shlvl_ps1)$(__nix_ps1)$(__time_ps1)$(__jobs_ps1)\n'
 PS1+=' \$ '
 
 PS2='│'
