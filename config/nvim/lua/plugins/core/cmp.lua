8084d260fc6c79860efca44e7d6fa13e662f6664
Arsen Nuramatov <anuramat@pm.me>
2025-02-20 03:56:18 +0100

    auto: anuramat-ll7

--- config/nvim/lua/plugins/core/cmp.lua
+++ config/nvim/lua/plugins/core/cmp.lua
@@ -43,34 +43,24 @@ return {
         virtualtext = {
           auto_trigger_ft = { '*' },
           auto_trigger_ignore_ft = {},
-          show_on_completion_menu = true,
+          show_on_completion_menu = true, -- show when menu is visible
           keymap = {
-            accept = '<A-A>',
-            accept_line = '<A-a>',
-            accept_n_lines = '<A-z>',
-            prev = '<A-[>',
-            next = '<A-]>',
+            accept = '<A-a>',
+            accept_line = '<A-y>',
+            prev = '<A-p>',
+            next = '<A-n>',
             dismiss = '<A-e>',
           },
         },
         provider = 'openai_fim_compatible',
-        n_completions = 1, -- recommend for local model for resource saving
-        -- I recommend beginning with a small context window size and incrementally
-        -- expanding it, depending on your local computing power. A context window
-        -- of 512, serves as an good starting point to estimate your computing
-        -- power. Once you have a reliable estimate of your local computing power,
-        -- you should adjust the context window to a larger value.
-        context_window = 512,
+        n_completions = 5,
+        context_window = 512, -- isn't this too large
         provider_options = {
           openai_fim_compatible = {
             api_key = 'TERM',
             name = 'Ollama',
-            end_point = 'http://localhost:11434/api/generate',
+            end_point = 'http://localhost:11434/v1/completions',
             model = 'deepseek-coder-v2:16b',
-            optional = {
-              max_tokens = 56,
-              top_p = 0.9,
-            },
           },
         },
       }
