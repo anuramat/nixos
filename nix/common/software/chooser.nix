7e416583bc0ea05cebfee84d70970cc7271c5d19
Arsen Nuramatov <anuramat@pm.me>
2025-02-26 22:51:54 +0100

    auto: anuramat-ll7

--- nix/common/software/chooser.nix
+++ nix/common/software/chooser.nix
@@ -7,14 +7,6 @@
 }:
 let
   user = config.user;
-  # wrapper = ./wrapper.sh;
-
-  # wrapper = pkgs.writeText "nix_wrapper.sh" ''
-  #   #!/bin/sh
-  #   export PATH="${pkgs.foot}/bin:${pkgs.yazi}''${PATH:+:$PATH}"
-  #   ${unstable.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh "$@"
-  # '';
-
   wrapper = pkgs.writeTextFile {
     name = "nix_wrapper.sh";
     text = ''
