0c5ad26afb05ead62b613575735190968d6ce210
Arsen Nuramatov <anuramat@pm.me>
2025-03-04 22:04:50 +0100

    auto: anuramat-t480

--- nix/common/software/misc.nix
+++ nix/common/software/misc.nix
@@ -5,7 +5,6 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    # Misc {{{1
     chatterino2 # gui twitch chat client
     spotify
     steam
@@ -26,4 +25,11 @@
     enableVirtualCamera = true; # set up the v4l2loopback kernel module
   };
   programs.gphoto2.enable = true; # dslr interface
+
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+    # pull models on service start
+    loadModels = [ ];
+  };
 }
