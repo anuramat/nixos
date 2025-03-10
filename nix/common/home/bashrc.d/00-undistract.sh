786d0afcb5878e34799af63e7f18098fd15a81e3
Arsen Nuramatov <anuramat@pm.me>
2025-03-10 14:32:12 +0100

    auto: anuramat-t480

--- nix/common/home/bashrc.d/00-undistract.sh
+++ nix/common/home/bashrc.d/00-undistract.sh
@@ -12,6 +12,8 @@ __undistract() {
 	[ -z "$__last_command_start_time" ] && return
 	local diff="$(($(date +%s) - __last_command_start_time))"
 	((diff > UNDISTRACT_TOLERANCE)) && tput bel && printf "\e]777;notify;%s;%s\e\\" "Executed" "$__last_command"
+	# 777: foot, ghostty
+	# 99: foot
 	# '\e]99;;%s\e\\'
 }
 precmd_functions+=(__undistract)
