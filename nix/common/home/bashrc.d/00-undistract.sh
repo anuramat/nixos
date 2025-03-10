3a40fa6be3b828280d0a706a2d4ff2c7afe9825f
Arsen Nuramatov <anuramat@pm.me>
2025-03-10 14:30:49 +0100

    auto: anuramat-t480

--- nix/common/home/bashrc.d/00-undistract.sh
+++ nix/common/home/bashrc.d/00-undistract.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export UNDISTRACT_TOLERANCE=9999999 # TODO turn on once kitty is figured out
+export UNDISTRACT_TOLERANCE=5 # TODO turn on once kitty is figured out
 
 __undistract_preexec() {
 	__last_command_start_time=$(date +%s)
@@ -12,5 +12,6 @@ __undistract() {
 	[ -z "$__last_command_start_time" ] && return
 	local diff="$(($(date +%s) - __last_command_start_time))"
 	((diff > UNDISTRACT_TOLERANCE)) && tput bel && printf "\e]777;notify;%s;%s\e\\" "Executed" "$__last_command"
+	# '\e]99;;%s\e\\'
 }
 precmd_functions+=(__undistract)
