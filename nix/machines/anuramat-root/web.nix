febbd7dca1a9dc73511e62b834edfbd42c09b7b6
Arsen Nuramatov <anuramat@pm.me>
2025-03-18 23:02:49 +0100

    auto: anuramat-root

--- nix/machines/anuramat-root/web.nix
+++ nix/machines/anuramat-root/web.nix
@@ -1,6 +1,8 @@
-{ config, ... }:
+{ lib, config, ... }:
 let
   home = config.users.users.${config.user}.home;
+  root = "${home}/public";
+  domain = "ctrl.sn";
 in
 {
   # web {{{1
@@ -10,16 +12,35 @@ in
   ];
   services.static-web-server = {
     enable = true;
-    listen = "[::]:80";
-    root = "${home}/public";
+    listen = "[::]:443";
     configuration = {
       general = {
         directory-listing = true;
+        http2 = true;
+        http2-tls-cert = "/var/lib/acme/${domain}/fullchain.pem";
+        http2-tls-key = "/var/lib/acme/${domain}/key.pem";
       };
     };
   };
-  services.certbot = {
-    enable = true;
-    agreeTerms = true;
+
+  security.acme = {
+    acceptTerms = true;
+    certs."${domain}" = {
+      reloadServices = [ "static-web-server" ];
+      listenHTTP = ":80";
+      group = "www-data";
+    };
   };
+  users.groups.www-data = { };
+
+  # TODO wtf is this paragraph for
+  systemd.services.static-web-server.serviceConfig.SupplementaryGroups = lib.mkForce [
+    ""
+    "www-data"
+  ];
+  systemd.services.static-web-server.serviceConfig.BindReadOnlyPaths = lib.mkForce [
+    root
+    "/var/lib/acme/${domain}"
+  ];
+
 }
