de162be8ec3e7e19207894f4e8764ca1f4b2e292
Arsen Nuramatov <anuramat@pm.me>
2025-03-18 20:45:16 +0100

    auto: anuramat-t480

---
+++ nix/generic/desktop/misc.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+{
+  # TODO move the file contents
+  environment.systemPackages = with pkgs; [
+    # comms {{{1
+    element-desktop # matrix client
+    onionshare # tor-based file-sharing etc
+    onionshare-gui # p2p file sharing, chat, website hosting
+    # qtox # p2p IM XXX broken
+    # slack # XXX broken
+    telegram-desktop
+    whatsapp-for-linux
+
+    # browsers {{{1
+    tor-browser-bundle-bin
+    librewolf
+    google-chrome
+
+    # terminals {{{1
+    cool-retro-term
+    foot
+    unstable.ghostty
+    wezterm
+
+    # misc {{{1
+    chatterino2 # gui twitch chat client
+    spotify
+    steam
+  ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      # obs-backgroundremoval # breaks on CUDA
+      obs-gstreamer
+      obs-multi-rtmp # multi-site
+      obs-nvfbc
+      obs-pipewire-audio-capture
+      obs-tuna # song info
+      obs-vaapi
+      wlrobs # screen capture for wlroots
+    ];
+    enableVirtualCamera = true; # set up the v4l2loopback kernel module
+  };
+  programs.gphoto2.enable = true; # dslr interface
+}
