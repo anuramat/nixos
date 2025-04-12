090c0cc65e341cfb779fd29d4e7645b556ea96f3
Arsen Nuramatov <anuramat@pm.me>
2025-04-13 01:23:52 +0200

    auto: anuramat-ll7

--- nix/generic/home/default.nix
+++ nix/generic/home/default.nix
@@ -53,9 +53,14 @@ in
           settings = {
             "widget.use-xdg-desktop-portal.file-picker" = 1;
             "identity.fxaccounts.enabled" = true;
+            # since it breaks a lot of pages
             "privacy.resistFingerprinting" = false;
-            "sidebar.revamp" = true;
             "sidebar.verticalTabs" = true;
+            # required by vertical tabs
+            "sidebar.revamp" = true;
+            # rejecting all; fallback -- do nothing
+            "cookiebanners.service.mode" = 1;
+            "cookiebanners.service.mode.privateBrowsing" = 1;
           };
 
         };
