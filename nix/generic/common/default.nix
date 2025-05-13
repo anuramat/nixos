a31972b7cd4a365db631f8115d796427c6392da5
Arsen Nuramatov <anuramat@pm.me>
2025-05-13 17:05:54 +0200

    cluster aliases

--- nix/generic/common/default.nix
+++ nix/generic/common/default.nix
@@ -79,7 +79,18 @@ in
   };
 
   # ssh etc {{{1
-  programs.ssh.knownHostsFiles = cluster.hostKeysFiles;
+  programs.ssh = {
+    knownHostsFiles = cluster.hostKeysFiles;
+    extraConfig =
+      let
+        prefix = config.user + "-";
+        mkAliasEntry = hostname: ''
+          Host ${lib.strings.removePrefix prefix hostname}
+            HostName ${hostname}
+        '';
+      in
+      lib.strings.concatMapStringsSep "\n" mkAliasEntry cluster.hostnames;
+  };
   services = {
     fail2ban.enable = true; # intrusion prevention
     tailscale.enable = true;
