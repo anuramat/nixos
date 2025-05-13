0cbb1c24d7a595893c3591703d06169b6a5a2e5c
Arsen Nuramatov <anuramat@pm.me>
2025-05-13 10:49:22 +0200

    trw script rewritten with subcommands

--- nix/generic/home/trw.sh
+++ nix/generic/home/trw.sh
@@ -1,48 +1,62 @@
 #!/usr/bin/env bash
 
-trw_up() {
-	__trw_check_args "$@" || return 1
-	target="$1"
-	path="$(realpath "$2")/"
-	rsync -av --ignore-existing "$path" "$target:$path"
+trw() {
+	local -r subcommand=$1 && shift
+	local force && [ "$1" = '-f' ] && shift && force=true
+	local -r remote=$1
+	local -r path=$(realpath "$2")/
+
+	local from=$path
+	local to=$remote:$path
+	local shortflags=-av
+	local longflag=
+
+	case "$subcommand" in
+		h)
+			echo 'trw down/up/check [-f] $REMOTE $PATH'
+			;;
+		down)
+			temp=$to
+			to=$from
+			from=$temp
+			;&
+		up)
+			longflag=--ignore-existing
+			[ "$force" = true ] && __trw_sure && longflag=--delete || return 1
+			;;
+		check)
+			shortflags+=n
+			longflag=--delete
+			[ "$force" = true ] && __trw_sure && shortflags+=c || return 1
+			;;
+		*)
+			echo 'illegal subcommand'
+			return 1
+			;;
+	esac
+
+	__trw_check_ends "$remote" "$path" || return 1
+
+	rsync "$shortflags" "$longflag" "$from" "$to"
 }
 
-trw_down() {
-	__trw_check_args "$@" || return 1
-	target="$1"
-	path="$(realpath "$2")/"
-	rsync -av --ignore-existing "$target:$path" "$path"
-}
-
-trw_up_force() {
-	__trw_check_args "$@" || return 1
-	target="$1"
-	path="$(realpath "$2")/"
-
-	read -p "are you sure?" -n 1 -r REPLY
-	[ "$REPLY" != y ] && return 1
-
-	rsync -av --delete "$path" "$target:$path"
-}
-
-trw_check() {
-	__trw_check_args "$@" || return 1
-	target="$1"
-	path="$(realpath "$2")/"
-	rsync -avn --delete "$path" "$target:$path"
-}
-
-trw_check_full() {
-	__trw_check_args "$@" || return 1
-	target="$1"
-	path="$(realpath "$2")/"
-	rsync -avnc --delete "$path" "$target:$path"
-}
-
-__trw_check_args() {
-	target="$1"
-	path="$(realpath -q "$2")/" && [ -n "$target" ] && [ -n "$path" ] && [ -d "$path" ] || {
-		echo "invalid arguments"
+__trw_check_ends() {
+	local remote="$1"
+	local path
+	path="$(realpath -q "$2")/" && [ -n "$path" ] && [ -d "$path" ] || {
+		echo "error: invalid path: $path"
 		return 1
 	}
+	[ -n "$remote" ] || {
+		echo "error: empty remote"
+		return 1
+	}
+}
+
+__trw_sure() {
+	local reply
+	echo "are you sure? (y/*)"
+	read -t 1 -n 1 -r reply
+	echo
+	[ "$reply" == y ]
 }
