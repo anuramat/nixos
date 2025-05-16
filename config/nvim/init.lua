ef570139a45499c086b95f64d0a4a77d171e41a6
Arsen Nuramatov <anuramat@pm.me>
2025-05-16 18:57:34 +0200

    auto: anuramat-t480

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,19 +5,4 @@ vim.diagnostic.config({
   signs = false,
 })
 vim.deprecate = function() end -- shut up
--- TODO gets stuck sometimes; not that important anyway
--- if os.getenv('SSH_TTY') then
---   vim.g.clipboard = {
---     name = 'OSC 52',
---     copy = {
---       ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
---       ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
---     },
---     paste = {
---       ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
---       ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
---     },
---   }
--- end
 require('plugins')
-require('adhoc')
--- config/nvim/lua/adhoc/templates.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd([[
-autocmd BufNewFile *.tex 0r $XDG_CONFIG_HOME/latex/template.tex
-]])
