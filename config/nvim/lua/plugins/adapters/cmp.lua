97a8004f64b9ee66c530a9fdd9dfc069d5a698a1
Arsen Nuramatov <anuramat@pm.me>
2025-05-19 17:18:32 +0200

    auto: anuramat-ll7

--- config/nvim/lua/plugins/adapters/cmp.lua
+++ config/nvim/lua/plugins/adapters/cmp.lua
@@ -20,7 +20,6 @@ return {
   },
   -- llm autocomplete, chat, agents/tools
   {
-    enabled = false,
     'olimorris/codecompanion.nvim',
     -- BUG chat hangs up after one message
     event = 'BufEnter',
