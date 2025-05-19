626b00e35fb48d3c0459a0bf252653b2e8cb9b03
Arsen Nuramatov <anuramat@pm.me>
2025-05-19 15:10:23 +0200

    auto: anuramat-ll7

--- config/nvim/lua/plugins/adapters/cmp.lua
+++ config/nvim/lua/plugins/adapters/cmp.lua
@@ -67,6 +67,7 @@ return {
       ollama = {
         endpoint = 'http://127.0.0.1:11434',
         model = modelname,
+        reasoning_effort = 'low', -- low|medium|high, only used for reasoning models
       },
     },
     build = 'make',
