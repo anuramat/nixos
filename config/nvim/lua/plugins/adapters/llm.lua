0078f7dae327b4e6ac02199373e7105d82a79f90
Arsen Nuramatov <anuramat@pm.me>
2025-05-20 15:56:30 +0200

    auto: anuramat-t480

--- config/nvim/lua/plugins/adapters/llm.lua
+++ config/nvim/lua/plugins/adapters/llm.lua
@@ -1,34 +1,41 @@
-local modelname = 'llama3.2'
+local modelname = 'qwen3:8b'
 return {
-  'yetone/avante.nvim',
-  event = 'VeryLazy',
-  version = false,
-  opts = {
-    system_prompt = '\\no_think',
-    mode = 'legacy', -- required by models that don't support tools (tools are broken for ollama)
-    provider = 'ollama',
-    vendors = {
-      pollinations = {
-        __inherited_from = 'openai',
-        api_key_name = '',
-        endpoint = 'https://text.pollinations.ai/openai',
-        model = 'openai',
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false,
+    opts = {
+      system_prompt = '\\no_think',
+      -- mode = 'legacy', -- BUG required by models that don't support tools (tools are broken for ollama)
+      provider = 'pollinations',
+      vendors = {
+        pollinations = {
+          __inherited_from = 'openai',
+          api_key_name = '',
+          endpoint = 'https://text.pollinations.ai/openai',
+          model = 'openai',
+        },
+        -- TODO github models is not available yet <https://github.com/yetone/avante.nvim/issues/2042>
       },
-      -- github models is not available yet <https://github.com/yetone/avante.nvim/issues/2042>
+      ollama = {
+        endpoint = 'http://anuramat-ll7:11434',
+        model = modelname,
+        reasoning_effort = 'low', -- low|medium|high, only used for reasoning models
+      },
     },
-    ollama = {
-      endpoint = 'http://anuramat-ll7:11434',
-      model = modelname,
-      reasoning_effort = 'low', -- low|medium|high, only used for reasoning models
+    build = 'make',
+    dependencies = {
+      'Kaiser-Yang/blink-cmp-avante',
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'MunifTanjim/nui.nvim',
+      'ibhagwan/fzf-lua',
     },
   },
-  build = 'make',
-  dependencies = {
-    'Kaiser-Yang/blink-cmp-avante',
-    'nvim-treesitter/nvim-treesitter',
-    'stevearc/dressing.nvim',
-    'MunifTanjim/nui.nvim',
-    'ibhagwan/fzf-lua',
-  },
-  -- TODO github models
+  -- {
+  --   'ravitemer/mcphub.nvim',
+  --   build = 'npm install -g mcp-hub@latest', -- installs `mcp-hub` node binary globally
+  --   opts = {},
+  -- },
+  -- <https://ravitemer.github.io/mcphub.nvim/configuration.html>
 }
