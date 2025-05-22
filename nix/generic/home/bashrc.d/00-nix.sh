717cf2c8a3f1084c6a541a640c3b04aaefca60ed
Arsen Nuramatov <anuramat@pm.me>
2025-05-22 17:46:00 +0200

    meh

--- nix/generic/home/bashrc.d/00-nix.sh
+++ nix/generic/home/bashrc.d/00-nix.sh
@@ -3,28 +3,27 @@
 reflake() {
 	if [ "$#" -ne 1 ]; then
 		echo "Usage: ${FUNCNAME[0]} .../flake.lock"
-		exit 1
+		return 1
 	fi
 
 	local target="$1"
 	local source="/etc/nixos/flake.lock"
 
-	for i in "$source" "$target"; do
-		[ -f "$i" ] || {
-			echo "$i not found"
+	for file in "$source" "$target"; do
+		[ -f "$file" ] || {
+			echo "$file not found"
 			return 1
 		}
 	done
 
-	# Create a temporary file
-	TMP_FILE=$(mktemp)
-
-	# Use jq to update the flake-utils entry
-
-	local fields=("flake-utils" "nixpkgs-unstable")
-	for i in "${fields[@]}"; do
-		# TODO add "original" check
-		jq --arg input "$(jq -r ".nodes.\"$i\".locked | tojson" "$source")" ".nodes.\"$i\".locked = (\$input | fromjson)" "$target" | sponge "$target"
+	local locked
+	local replace_locked_expr
+	jq --raw-output0 ".nodes | keys[]" "$target" | while IFS= read -r -d '' input; do
+		# TODO skip keys from input that are not in target
+		# TODO check that .nodes.input.original are equal, otherwise skip
+		locked=$(jq -r ".nodes.\"$input\".locked | tojson" "$source")
+		replace_locked_expr=".nodes.\"$input\".locked = (\$input | fromjson)"
+		jq --arg input "$locked" "$replace_locked_expr" "$target" | sponge "$target"
 	done
 
 	echo "synced $target with system flake.lock"
