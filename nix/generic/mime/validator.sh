12ed3c48083761ffc434a555e0ef6d13ed63945e
Arsen Nuramatov <anuramat@pm.me>
2025-05-28 16:58:30 +0200

    minor refactor validation

--- nix/generic/mime/validator.sh
+++ nix/generic/mime/validator.sh
@@ -2,6 +2,8 @@
 
 # get csv files
 
+
+# TODO refactor
 dir=data
 [ "$(basename "$PWD")" = mime ] || exit 1
 [ -f "$dir/media-types.xhtml" ] || wget -O "$dir/media-types.xhtml" https://www.iana.org/assignments/media-types/media-types.xhtml
@@ -14,7 +16,8 @@ while IFS= read -r link; do
 done < <(htmlq -f "$dir/media-types.xhtml" -b https://www.iana.org/assignments/media-types -a href 'li > a' | sed s@#@/@ | sed 's/$/.csv/')
 
 all=$(cat "$dir"/*.csv | sort | uniq)
-mentioned=$(nix eval --raw --expr "let x = $(nix eval -f default.nix xdg.mime.defaultApplications); in x |> builtins.attrNames |> builtins.concatStringsSep \"\\n\"" | sort | uniq)
+defaultApplications=$(nix eval -f default.nix xdg.mime.defaultApplications --show-trace) || exit 1
+mentioned=$(nix eval --raw --expr "let x = $defaultApplications; in x |> builtins.attrNames |> builtins.concatStringsSep \"\\n\"" | sort | uniq)
 
 comm -23 <(echo "$mentioned") <(echo "$all")
 
