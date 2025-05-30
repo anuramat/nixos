8fb98a91a3af28fb301ba4941af03996c3ce02da
Arsen Nuramatov <anuramat@pm.me>
2025-05-30 04:04:12 +0200

    auto: anuramat-ll7

--- nix/generic/overlays/default.nix
+++ nix/generic/overlays/default.nix
@@ -1,10 +1,39 @@
 {
   inputs,
-  helpers,
   ...
 }:
-with helpers.overlays;
 let
+
+  # TODO move to helpers
+  # HACK here we pepegapray that the pname and attrname in nixpkgs are equal (I think it's not true for neovim for example, pname is neovim-unwrapped or something)
+  mkUnstablePackages =
+    pkgfunc: final: prev:
+    let
+      unstable = import inputs.nixpkgs-unstable { inherit (prev) config system; };
+    in
+    prev.lib.listToAttrs (
+      map (pkg: {
+        name = pkg.pname;
+        value = pkg;
+      }) (pkgfunc unstable)
+    );
+  unwrapOverlays = map (input: input.overlays.default);
+  unwrapPackages = (
+    inputs: final: prev:
+    map (
+      input:
+      let
+        pkg = input.packages.${prev.system}.default;
+      in
+      {
+        name = pkg.pname;
+        value = pkg;
+      }
+    ) inputs
+    |> builtins.listToAttrs
+  );
+  # ------------------- end of helpers
+
   unstablePackages = mkUnstablePackages (
     unstable: with unstable; [
       github-mcp-server
--- nix/helpers/overlays.nix
+++ nix/helpers/overlays.nix
@@ -3,31 +3,4 @@
   ...
 }:
 {
-  # HACK here we pepegapray that the pname and attrname in nixpkgs are equal (I think it's not true for neovim for example, pname is neovim-unwrapped or something)
-  mkUnstablePackages =
-    pkgfunc: final: prev:
-    let
-      unstable = import inputs.nixpkgs-unstable { inherit (prev) config system; };
-    in
-    builtins.listToAttrs (
-      map (pkg: {
-        name = pkg.pname;
-        value = pkg;
-      }) (pkgfunc unstable)
-    );
-  unwrapOverlays = map (input: input.overlays.default);
-  unwrapPackages = (
-    inputs: final: prev:
-    map (
-      input:
-      let
-        pkg = input.packages.${prev.system}.default;
-      in
-      {
-        name = pkg.pname;
-        value = pkg;
-      }
-    ) inputs
-    |> builtins.listToAttrs
-  );
 }
