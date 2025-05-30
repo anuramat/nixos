4abfe027ce6b097a3ae28af30341e2924628d676
Arsen Nuramatov <anuramat@pm.me>
2025-05-30 04:04:33 +0200

    auto: anuramat-ll7

--- nix/generic/overlays/default.nix
+++ nix/generic/overlays/default.nix
@@ -36,20 +36,18 @@ let
 
   unstablePackages = mkUnstablePackages (
     unstable: with unstable; [
-      github-mcp-server
-      keymapp
       cheese
-      nixd
-      xdg-ninja
-      yazi
-      tgpt
-      wallust
-      xdg-desktop-portal-termfilechooser
-      foot
-      yazi
       foot
       ghostty
+      github-mcp-server
+      keymapp
+      nixd
+      tgpt
+      wallust
       wezterm
+      xdg-desktop-portal-termfilechooser
+      xdg-ninja
+      yazi
     ]
   );
   overlays = unwrapOverlays (
