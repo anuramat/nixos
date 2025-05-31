4113b6055b176dd334e629a05ce0ac4ae5732a28
Arsen Nuramatov <anuramat@pm.me>
2025-05-31 16:54:53 +0200

    auto: anuramat-t480

--- config/nvim/lua/plugins/adapters/llm/copilot.lua
+++ config/nvim/lua/plugins/adapters/llm/copilot.lua
@@ -3,7 +3,9 @@
 local models = {
   gpt41 = 'gpt-4.1', -- free
   -- 4.5 is not available for some reason, 50!!!
-  -- opus 4 waiting -- 10
+  -- opus 4 waiting -- 10, top 2 kagi reasoning
+  -- o3 -- 5 -- top 1 kagi reasoning
+  -- gemini 2.5 pro -- 1 -- top 3 kagi reasoning, huge context
   claude37cot = 'claude-3.7-sonnet-thought', -- 1.25
   claude40 = 'claude-sonnet-4', -- 1
 }
