542324275bc7bd1915bd092b69d50d486c1d75ec
Arsen Nuramatov <x@ctrl.sn>
2025-06-01 16:55:57 +0200

    auto: anuramat-t480

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -6,6 +6,8 @@ public=/etc/nix/cache.pem.pub
 [ ! -e "$private" ] && [ ! -e "$public" ] \
 	&& sudo nix-store --generate-binary-cache-key "$(hostname)" "$private" "$public"
 
+sudo chown builder:builder "$private" "$public" # TODO hide somewhere in nix
+
 [ ! -e "$HOME/.ssh" ] && yes '' | ssh-keygen -N ''
 
 # TODO root ssh config
