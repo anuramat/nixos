10eaa85d40fcbde19f643bb15d079e056830a45a
Arsen Nuramatov <x@ctrl.sn>
2025-06-01 17:19:35 +0200

    auto: anuramat-t480

--- nix/generic/common/builder.nix
+++ nix/generic/common/builder.nix
@@ -7,6 +7,8 @@
 lib.mkIf (!config.nix.distributedBuilds) {
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [
     5000 # nix-serve
+    # TODO check, maybe it gets open automatically
+    # or maybe not even used at all
   ];
   users.users.${cluster.builderUsername} = {
     isNormalUser = true; # TODO maybe not?
@@ -20,6 +22,7 @@ lib.mkIf (!config.nix.distributedBuilds) {
     cluster.builderUsername
   ];
   # sign the derivations so that we can use the builder as a cache
+  # unsigned derivations don't get saved to nix store (evidently, but not 100% sure)
   nix.settings.secret-key-files = "/etc/nix/cache.pem";
   # NOTE might wanna add gc and autoUpgrade later
 }
