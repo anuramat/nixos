fe4ce3e18a1b557660b2a788d96377f74314abc4
Arsen Nuramatov <x@ctrl.sn>
2025-06-01 19:58:10 +0200

    auto: anuramat-t480

--- nix/generic/home/default.nix
+++ nix/generic/home/default.nix
@@ -1,93 +1,18 @@
-{
-  config,
-  inputs,
-  pkgs,
-  ...
-}:
+v:
+
 let
-  user = config.user.username;
+  user = v.config.user.username;
 in
 {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
+    v.inputs.home-manager.nixosModules.home-manager
   ];
 
   home-manager = {
-
     backupFileExtension = "backup";
     useGlobalPkgs = true;
     useUserPackages = true;
 
-    users.${user} = {
-      xdg.enable = true; # TODO what does this even do
-
-      home.file = {
-        # made for nvi
-        ".exrc" = {
-          source = ./files/exrc;
-        };
-      };
-
-      programs = {
-
-        home-manager.enable = true; # TODO same here
-
-        gh = {
-          enable = true;
-          settings = {
-            extensions = with pkgs; [
-              gh-f
-              gh-copilot
-              # # wait until they appear
-              # copilot-insights
-              # token
-            ];
-            git_protocol = "ssh";
-            prompt = true;
-          };
-        };
-
-        bash = {
-          enable = true;
-          # TODO move everything around ffs
-          bashrcExtra = # bash
-            ''
-              source ${./files/xdg_shims.sh}
-              [[ $- == *i* ]] || return
-              for f in "${./files/bashrc.d}"/*; do source "$f"; done
-              source ${./files/bashrc.sh}
-            '';
-        };
-
-        git = import ./git.nix config;
-        readline = {
-          enable = true;
-          extraConfig = builtins.readFile ./files/inputrc;
-        };
-
-        librewolf = {
-          enable = true;
-          settings = {
-            "browser.urlbar.suggest.history" = false;
-            "widget.use-xdg-desktop-portal.file-picker" = 1;
-            "identity.fxaccounts.enabled" = true;
-
-            # since it breaks a lot of pages
-            "privacy.resistFingerprinting" = false;
-
-            "sidebar.verticalTabs" = true;
-            # required by vertical tabs
-            "sidebar.revamp" = true;
-
-            # rejecting all; fallback -- do nothing
-            "cookiebanners.service.mode" = 1;
-            "cookiebanners.service.mode.privateBrowsing" = 1;
-          };
-
-        };
-
-        neovim = import ./neovim.nix pkgs;
-      };
-    };
+    users.${user} = import ./hm.nix;
   };
 }
---
+++ nix/generic/home/hm.nix
@@ -0,0 +1,82 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+
+  xdg.enable = true; # TODO what does this even do
+
+  home.file = {
+    # made for nvi
+    ".exrc" = {
+      source = ./files/exrc;
+    };
+  };
+
+  programs = {
+    home-manager.enable = true; # TODO same here
+    password-store = {
+      enable = true;
+      settings = {
+        PASSWORD_STORE_DIR = "${config.xdg.dataHome}/pass";
+      };
+    };
+
+    gh = {
+      enable = true;
+      settings = {
+        extensions = with pkgs; [
+          gh-f
+          gh-copilot
+          # # wait until they appear
+          # copilot-insights
+          # token
+        ];
+        git_protocol = "ssh";
+        prompt = true;
+      };
+    };
+
+    bash = {
+      enable = true;
+      # TODO move everything around ffs
+      bashrcExtra = # bash
+        ''
+          source ${./files/xdg_shims.sh}
+          [[ $- == *i* ]] || return
+          for f in "${./files/bashrc.d}"/*; do source "$f"; done
+          source ${./files/bashrc.sh}
+        '';
+    };
+
+    git = import ./git.nix config;
+    readline = {
+      enable = true;
+      extraConfig = builtins.readFile ./files/inputrc;
+    };
+
+    librewolf = {
+      enable = true;
+      settings = {
+        "browser.urlbar.suggest.history" = false;
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "identity.fxaccounts.enabled" = true;
+
+        # since it breaks a lot of pages
+        "privacy.resistFingerprinting" = false;
+
+        "sidebar.verticalTabs" = true;
+        # required by vertical tabs
+        "sidebar.revamp" = true;
+
+        # rejecting all; fallback -- do nothing
+        "cookiebanners.service.mode" = 1;
+        "cookiebanners.service.mode.privateBrowsing" = 1;
+      };
+
+    };
+
+    neovim = import ./neovim.nix pkgs;
+  };
+}
