270341ef2a694f79837032380c68bbb9ce8716a8
Arsen Nuramatov <x@ctrl.sn>
2025-06-02 04:12:57 +0200

    auto: anuramat-t480

--- config/nvim/lua/plugins/adapters/llm/copilot.lua
+++ config/nvim/lua/plugins/adapters/llm/copilot.lua
@@ -1,12 +1,9 @@
--- https://docs.github.com/en/copilot/managing-copilot/monitoring-usage-and-entitlements/about-premium-requests#user-content-fnref-1
+-- https://docs.github.com/en/copilot/managing-copilot/monitoring-usage-and-entitlements/about-premium-requests
 -- 300 premium requests per month
 local models = {
   gpt41 = 'gpt-4.1', -- free
-  -- 4.5 is not available for some reason, 50!!!
-  -- opus 4 waiting -- 10, top 2 kagi reasoning
-  -- o3 -- 5 -- top 1 kagi reasoning
-  -- gemini 2.5 pro -- 1 -- top 3 kagi reasoning, huge context
-  claude37cot = 'claude-3.7-sonnet-thought', -- 1.25
+  -- other decent openai models aren't available right now
+  gemini = 'gemini-2.5-pro', -- 1 -- top 2 aider, top 3 kagi reasoning, huge context
   claude40 = 'claude-sonnet-4', -- 1
 }
 
