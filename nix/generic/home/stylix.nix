cf6c0379ab01b15f24800eb97609c416997f1da3
Arsen Nuramatov <x@ctrl.sn>
2025-06-02 17:48:38 +0200

    auto: anuramat-t480

--- nix/generic/home/stylix.nix
+++ nix/generic/home/stylix.nix
@@ -1,5 +1,13 @@
+{ config, pkgs, ... }:
 {
-  stylix.targets = {
-    bemenu.enable = true;
+  stylix = {
+    targets = {
+      bemenu.enable = true;
+    };
+    base16Scheme = {
+      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    };
   };
+  # lib.stylix.colors = {
+  # };
 }
