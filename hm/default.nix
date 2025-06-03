182c319e4ed327f46fad560b4faddf962e3f2be9
Arsen Nuramatov <x@ctrl.sn>
2025-06-03 04:48:05 +0200

    auto: anuramat-ll7

--- hm/default.nix
+++ hm/default.nix
@@ -18,6 +18,37 @@ in
     ./sway.nix
   ];
 
+  yazi = {
+    enable = true;
+    settings = {
+      plugin.preloaders = [ ];
+      plugin.prepend_previewers = {
+        name = "/media/**";
+        run = "noop";
+      };
+      manager.sort_by = "natural";
+    };
+    keymap = {
+      manager.prepend_keymap = [
+        {
+          on = "y";
+          run = [
+            ''shell -- for path in "$@"; do echo "file://$path"; done | wl-copy -t text/uri-list''
+            "yank"
+          ];
+        }
+        {
+          on = "<C-n>";
+          run = ''shell -- dragon -x -i -T "$1"'';
+        }
+      ];
+    };
+    enableBashIntegration = true;
+  };
+  htop = {
+    enable = true;
+    settings = { };
+  };
   xdg.enable = true; # set XDG vars in .profile
   home = {
     packages = with pkgs; [
