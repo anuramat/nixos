4cb05e44d65599a1ac678c8f1257a310d07d74be
Arsen Nuramatov <x@ctrl.sn>
2025-06-03 21:30:13 +0200

    auto: anuramat-t480

--- home/sway.nix
+++ home/sway.nix
@@ -1,13 +1,62 @@
+{ pkgs, ... }:
 {
+  programs = {
+    swaylock = {
+      # needs pam; it's already configured by programs.sway on nixos
+      enable = true;
+      settings = {
+        ignore-empty-password = true;
+        indicator-caps-lock = true;
+
+        # red
+        color = "ff0000";
+
+        inside-color = "ff0000";
+        line-color = "000000";
+        ring-color = "ff0000";
+
+        inside-clear-color = "ff0000";
+        line-clear-color = "ff0000";
+        ring-clear-color = "ff0000";
+
+        inside-wrong-color = "ff0000";
+        line-wrong-color = "000000";
+        ring-wrong-color = "ff0000";
+
+        inside-ver-color = "ff0000";
+        line-ver-color = "ff0000";
+        ring-ver-color = "ff0000";
+
+        layout-bg-color = "00000000";
+        layout-border-color = "00000000";
+        layout-text-color = "000000";
+
+        key-hl-color = "000000";
+
+        # remove the text
+        text-caps-lock-color = "00000000";
+        text-clear-color = "00000000";
+        text-color = "00000000";
+        text-ver-color = "00000000";
+        text-wrong-color = "00000000";
+      };
+    };
+  };
   services = {
     swayidle =
       let
-        lock = "swaylock -f";
-        lockKeys = "killall -s SIGHUP gpg-agent";
-        unlock = "killall -s USR1 swaylock";
-        sleep = "systemctl suspend";
-        screenOff = "swaymsg 'output * dpms off'";
-        screenOn = "swaymsg 'output * dpms on'";
+        inherit (pkgs)
+          swaylock
+          sway
+          killall
+          systemd
+          ;
+        lock = "${swaylock}/bin/swaylock -f";
+        lockKeys = "${killall}/bin/killall -s SIGHUP gpg-agent";
+        unlock = "${killall}/bin/killall -s USR1 swaylock";
+        sleep = "${systemd}/bin/systemctl suspend";
+        screenOff = "${sway}/bin/swaymsg 'output * dpms off'";
+        screenOn = "${sway}/bin/swaymsg 'output * dpms on'";
       in
       {
         enable = true;
