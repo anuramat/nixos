cdd83a2401488c5f464aa9b4fb681f6ef34d9546
Arsen Nuramatov <x@ctrl.sn>
2025-06-03 20:13:46 +0200

    auto: anuramat-t480

--- home/pass.nix
+++ home/pass.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 {
   services.pass-secret-service.enable = true; # secret service api -- exposes password-store over dbus
+  # WARN locking only marks keyring as locked, secrets are still accessible
   programs.password-store = {
     enable = true;
+    # uses gpg-agent to encrypt/decrypt secrets
   };
   services.gpg-agent = {
     enable = true;
