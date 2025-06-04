44242a37fcbb09e80ed346d51446e2d698f36a24
Arsen Nuramatov <x@ctrl.sn>
2025-06-04 23:47:54 +0200

    auto: anuramat-t480

--- nix/generic/default.nix
+++ nix/generic/default.nix
@@ -37,7 +37,7 @@
       };
     };
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   };
   programs.npm = {
     npmrc = ''
