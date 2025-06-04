0807c340f045aecbf0e4a613a3661cc33a880f8d
Arsen Nuramatov <x@ctrl.sn>
2025-06-04 10:34:29 +0200

    auto: anuramat-t480

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -18,7 +18,6 @@
       include /etc/nixos/config/sway/config.d/00-external_commands
       include /etc/nixos/config/sway/config.d/00-outputs
       include /etc/nixos/config/sway/config.d/01-keys
-      include /etc/nixos/config/sway/config.d/02-bar
       include /etc/nixos/config/sway/config.d/xx-autostart
       include /etc/nixos/config/sway/config.d/xx-inputs
       include /etc/nixos/config/sway/config.d/xx-misc
