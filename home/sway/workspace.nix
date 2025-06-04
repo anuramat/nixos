f07726f8a32915dae776128efc8790ad0fefe81e
Arsen Nuramatov <x@ctrl.sn>
2025-06-04 17:32:47 +0200

    auto: anuramat-t480

--- home/sway/workspace.nix
+++ home/sway/workspace.nix
@@ -1,14 +1,38 @@
-{ }:
+{ lib, ... }:
 let
   internal = "eDP-1";
-  external = "DP-1 DP-2 DP-3 HDMI-A-2";
+  externals = builtins.concatStringsSep " " [
+    "DP-1"
+    "DP-2"
+    "DP-3"
+    "HDMI-A-2"
+  ];
+
+  mod = x: y: y - x * (builtins.div y x); # TODO lib
+
+  mkAssign =
+    output: numbers:
+    map (n: {
+      workspace = "${toString n}:${n |> mod 10 |> toString}";
+      output = output;
+    }) numbers;
+
 in
 {
-  wayland.windowManager.sway.config.workspaceOutputAssign = [
-    map
-    {
-      output = "";
-      workspace = "";
-    }
-  ];
+  wayland.windowManager.sway.config.workspaceOutputAssign =
+
+    mkAssign internal [
+      1
+      2
+      3
+      4
+      5
+    ]
+    ++ mkAssign externals [
+      6
+      7
+      8
+      9
+      10
+    ];
 }
