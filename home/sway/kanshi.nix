e4437706122041b28603fdde5ac837a53f88a641
Arsen Nuramatov <x@ctrl.sn>
2025-06-04 20:00:36 +0200

    auto: anuramat-t480

--- home/sway/kanshi.nix
+++ home/sway/kanshi.nix
@@ -1,76 +1,44 @@
+{ lib, ... }:
+let
+  t480 = {
+    criteria = "LG Display 0x0521 Unknown";
+    position = "0,0";
+  };
+  ll7 = {
+    criteria = "California Institute of Technology 0x1626 0x00006002";
+    position = "0,0";
+    scale = 1.5;
+  };
+  ext = {
+    criteria = "Dell Inc. DELL S2722QC 192SH24";
+    scale = 1.5;
+    adaptiveSync = false;
+  };
+
+  rawProfiles = {
+    ll7 = [
+      (ll7 // { scale = 1.5; })
+    ];
+    ll7-home = [
+      (ll7 // { scale = 2.0; })
+      (ext // { position = "1600,0"; })
+    ];
+    t480 = [
+      t480
+    ];
+    t480-home = [
+      t480
+      (ext // { position = "0,-2000"; })
+    ];
+  };
+  profiles = lib.mapAttrs (n: v: {
+    outputs = v;
+    name = n;
+  }) rawProfiles;
+in
 {
   services.kanshi = {
     enable = true;
-
-    settings = [
-      # Define output aliases
-      {
-        output = {
-          criteria = "LG Display 0x0521 Unknown";
-          alias = "t480";
-          position = "0,0";
-        };
-      }
-      {
-        output = {
-          criteria = "California Institute of Technology 0x1626 0x00006002";
-          alias = "ll7";
-          position = "0,0";
-          scale = 1.5;
-        };
-      }
-      {
-        output = {
-          criteria = "Dell Inc. DELL S2722QC 192SH24";
-          alias = "ext";
-          scale = 1.5;
-          adaptiveSync = false;
-        };
-      }
-    ];
-
-    profiles = {
-      ll7-home = {
-        outputs = [
-          {
-            criteria = "ll7";
-            scale = 2.0;
-          }
-          {
-            criteria = "ext";
-            position = "1600,0";
-          }
-        ];
-      };
-
-      ll7 = {
-        outputs = [
-          {
-            criteria = "ll7";
-            scale = 1.5;
-          }
-        ];
-      };
-
-      t480-home = {
-        outputs = [
-          {
-            criteria = "t480";
-          }
-          {
-            criteria = "ext";
-            position = "0,-2000";
-          }
-        ];
-      };
-
-      t480 = {
-        outputs = [
-          {
-            criteria = "t480";
-          }
-        ];
-      };
-    };
+    profiles = profiles;
   };
 }
