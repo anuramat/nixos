244ff1cfd428cb4e0139a035250abf61fbfb0f3f
Arsen Nuramatov <x@ctrl.sn>
2025-06-05 19:57:58 +0200

    kinda done main part of script

--- find_unused_nix_files.sh
+++ find_unused_nix_files.sh
@@ -8,8 +8,8 @@ paths+=$(fd -e nix | grep -v 'default.nix$' | grep -v '^flake.nix$')
 
 while IFS= read -r path; do
 	file=$(basename "$path")
-	exp=$(printf './%s$' "$file")
 	while IFS=: read -r matchfile matchstring; do
-		printf 'file %s match %s' "$matchfile" "$matchstring"
-	done < <(rg --no-config -g '*.nix' "$exp" --with-filename)
+		echo "$matchstring"
+	done < <(rg --no-config -g '*.nix' "^\s*(\./.*$file)$" -r '${1}' --only-matching --with-filename /etc/nixos)
+	break
 done < <(echo "$paths")
