cf3e89bb2be1d7875db4d56a302e283befe64e63
Arsen Nuramatov <x@ctrl.sn>
2025-06-09 21:15:46 +0200

    auto: anuramat-ll7

--- home/editor.nix
+++ home/editor.nix
@@ -87,6 +87,59 @@ in
         "github"
       ];
       extraPackages = lsp;
+      userSettings = {
+        agent = {
+          default_model = {
+            model = "qwen3:8b";
+            provider = "ollama";
+          };
+          default_profile = "write";
+          model_parameters = [ ];
+          preferred_completion_mode = "normal";
+          profiles = {
+            write-dumb = {
+              context_servers = { };
+              enable_all_context_servers = true;
+              name = "Write (no CoT)";
+              tools = {
+                copy_path = true;
+                create_directory = true;
+                create_file = true;
+                delete_path = true;
+                diagnostics = true;
+                edit_file = true;
+                fetch = true;
+                find_path = true;
+                grep = true;
+                list_directory = true;
+                move_path = true;
+                now = true;
+                read_file = true;
+                terminal = true;
+                thinking = false;
+                web_search = true;
+              };
+            };
+          };
+          single_file_review = false;
+          version = "2";
+        };
+        buffer_font_size = 16;
+        theme = {
+          dark = "One Dark";
+          light = "One Light";
+          mode = "dark";
+        };
+        ui_font_size = 16;
+        vim = {
+          default_mode = "normal";
+          highlight_on_yank_duration = 100;
+          toggle_relative_line_numbers = true;
+          use_multiline_find = false;
+          use_smartcase_find = true;
+        };
+        vim_mode = true;
+      };
     };
   };
   home.packages = with pkgs; [
