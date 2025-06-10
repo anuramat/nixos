cc1370e766828355661dbd890392f017b6ead06b
Arsen Nuramatov <x@ctrl.sn>
2025-06-10 20:42:20 +0200

    auto: anuramat-t480

---
+++ home/cli/packages.nix
@@ -0,0 +1,155 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # tops
+    btop # best
+    ctop # containers
+    gotop # cute
+    htop # basic
+    iotop # detailed io info, requires sudo
+    nvitop # nvidia gpu
+    podman-tui # podman container status
+    nvtopPackages.full # top for GPUs # maybe switch to .nvidia and .intel
+    zenith-nvidia # top WITH nvidia GPUs
+
+    # docs
+    slides # markdown presentation in terminal
+    glow # markdown viewer
+    poppler_utils # pdf utils
+    ghostscript # postscript/pdf utils
+    readability-cli # extracts main content from pages
+    easyocr # neural OCR
+    pandoc # markup converter (latex, markdown, etc)
+    djvulibre # djvu tools
+
+    # audio
+    sox # CLI audio processing
+    lame # mp3
+    piper-tts # good neural TTS
+
+    # ai
+    tgpt
+    aider-chat-full
+    claude-code
+    llama-cpp
+    codex
+
+    # backend
+    dive # look into docker image layers
+    grpcui
+    grpcurl
+    httpie # curl++
+    kubectl
+    kubectx
+    podman-compose
+
+    # img
+    imagemagickBig # CLI image manipulation
+    libwebp # tools for WebP image format
+    exiftool # read/write EXIF metadata
+    chafa # sixel, kitty, iterm2, unicode
+    timg # sixel, kitty, iterm2, unicode; faster than chafa
+
+    # video
+    ffmpeg-full
+    yt-dlp # download youtube videos
+
+    # file managers
+    yazi # big, simple, hackable/lua -- ~ for help
+    felix-fm # smallest, image previews -- :help<cr> for help; waiting for picker: <https://github.com/kyoheiu/felix/issues/261>
+    nnn # small, simple, ubiquitous -- ? for help
+    yazi # big, simple, hackable/lua -- ~ for help
+    felix-fm # smallest, image previews -- :help<cr> for help; waiting for picker: <https://github.com/kyoheiu/felix/issues/261>
+    nnn # small, simple, ubiquitous -- ? for help
+
+    # misc TODO categorize
+    asciinema
+    ansifilter
+    pstree
+    age # file encryption
+    wine
+    aria # downloader
+    banner
+    croc # send/receive files through relay with encryption TODO might be broken, unstable worked tho
+    makefile2graph
+    mermaid-cli
+    ddgr # ddg search
+    mermaid-filter
+    graphviz
+    graph-easy
+    distrobox
+    exercism # CLI for exercism.org
+    fastfetch
+    figlet # fancy banners
+    gnuplot
+    mprocs # job runner
+    prettyping # ping++
+    progress # progress status for cp etc
+    pv # pipe viewer
+    rsbkb # rust blackbag - encode/decode tools
+    nixtract # dependency graph of derivations
+    scc # sloc cloc and code: dick measuring tool
+    speedtest-cli
+    xdg-ninja # checks $HOME for junk
+    git-filter-repo # rewrite/analyze repository history
+    mosh # ssh over unstable connections
+    python311Packages.pyicloud
+    qrcp # send files to mobile over Wi-Fi using QR
+    rclone # rsync for cloud
+    tree-sitter
+    cowsay
+    fortune # random quotes
+
+    # network
+    aircrack-ng
+    mtr # net diagnostics
+    dig # dns utils
+    gsocket # get shit through nat
+    inetutils # common network stuff
+    netcat
+    nmap
+    socat # socket cat
+    wirelesstools # iwconfig etc
+    openconnect_openssl
+
+    # core
+    inputs.subcat.packages.${pkgs.system}.default
+    bat # cat++: syntax hl
+    delta # pretty diff
+    du-dust # du++
+    duf # df++
+    entr # file watcher - runs command on change
+    fd # find++
+    ghq # git repository manager
+    libqalculate # `qalc` - advanced calculator
+    ncdu # du++: interactive
+    parallel # run parallel jobs
+    ripgrep # grep++
+    ripgrep-all # ripgrep over docs, archives, etc
+    rmtrash # rm but to trash
+    trashy
+    tealdeer # tldr reimplementation: rust + xdg
+    devenv
+    eza # ls++
+    watchman # another file watcher TODO try and compare to entr
+    wayidle # runs a command on idle
+    zellij # tmux++
+    expect # automating tuis
+
+    # hardware
+    acpi # battery status etc
+    dmidecode # read hw info from bios using smbios/dmi
+    efibootmgr # EFI boot manager editor
+    hwinfo
+    libusb1 # user-mode USB access lib
+    libva-utils # vainfo - info on va-api
+    lshw # hw info
+    pciutils
+    procps # info from /proc
+    smartmontools # storage
+    nvme-cli
+    smem # ram usage
+    usbutils
+    v4l-utils # camera stuff
+  ];
+}
--- os/generic/common/cli.nix
+++ os/generic/common/cli.nix
@@ -7,12 +7,12 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    # docs {{{1
+    # docs
     man-pages
     man-pages-posix
-    manix # nix doc search
+    manix # nix doc search with caching
 
-    # posix {{{1
+    # minimal set of tools
     bc # simple calculator
     coreutils-full
     curl
@@ -34,153 +34,5 @@
     util-linux # I think it's already installed but whatever
     wget
     zip
-
-    # hardware {{{1
-    acpi # battery status etc
-    dmidecode # read hw info from bios using smbios/dmi
-    efibootmgr # EFI boot manager editor
-    hwinfo
-    libusb1 # user-mode USB access lib
-    libva-utils # vainfo - info on va-api
-    lshw # hw info
-    pciutils
-    procps # info from /proc
-    smartmontools # storage
-    nvme-cli
-    smem # ram usage
-    usbutils
-    v4l-utils # camera stuff
-
-    # network {{{1
-    aircrack-ng
-    mtr # net diagnostics
-    dig # dns utils
-    gsocket # get shit through nat
-    inetutils # common network stuff
-    netcat
-    nmap
-    socat # socket cat
-    wirelesstools # iwconfig etc
-    openconnect_openssl
-
-    # core {{{1
-    inputs.subcat.packages.${pkgs.system}.default
-    bat # cat++: syntax hl
-    delta # pretty diff
-    du-dust # du++
-    duf # df++
-    entr # file watcher - runs command on change
-    fd # find++
-    ghq # git repository manager
-    libqalculate # `qalc` - advanced calculator
-    ncdu # du++: interactive
-    parallel # run parallel jobs
-    ripgrep # grep++
-    ripgrep-all # ripgrep over docs, archives, etc
-    rmtrash # rm but to trash
-    trashy
-    tealdeer # tldr reimplementation: rust + xdg
-    devenv
-    eza # ls++
-    watchman # another file watcher TODO try and compare to entr
-    wayidle # runs a command on idle
-    zellij # tmux++
-    expect # automating tuis
-
-    # ai {{{1
-    tgpt
-    aider-chat-full
-    claude-code
-    llama-cpp
-    codex
-
-    # misc {{{1
-    asciinema
-    ansifilter
-    pstree
-    age # file encryption
-    wine
-    aria # downloader
-    banner
-    croc # send/receive files through relay with encryption TODO might be broken, unstable worked tho
-    makefile2graph
-    mermaid-cli
-    ddgr # ddg search
-    mermaid-filter
-    graphviz
-    graph-easy
-    distrobox
-    exercism # CLI for exercism.org
-    fastfetch
-    figlet # fancy banners
-    gnuplot
-    mprocs # job runner
-    prettyping # ping++
-    progress # progress status for cp etc
-    pv # pipe viewer
-    rsbkb # rust blackbag - encode/decode tools
-    nixtract # dependency graph of derivations
-    scc # sloc cloc and code: dick measuring tool
-    speedtest-cli
-    xdg-ninja # checks $HOME for junk
-    git-filter-repo # rewrite/analyze repository history
-    mosh # ssh over unstable connections
-    python311Packages.pyicloud
-    qrcp # send files to mobile over Wi-Fi using QR
-    rclone # rsync for cloud
-    tree-sitter
-    cowsay
-    fortune # random quotes
-    # file managers
-    yazi # big, simple, hackable/lua -- ~ for help
-    felix-fm # smallest, image previews -- :help<cr> for help; waiting for picker: <https://github.com/kyoheiu/felix/issues/261>
-    nnn # small, simple, ubiquitous -- ? for help
-
-    # backend {{{1
-    dive # look into docker image layers
-    grpcui
-    grpcurl
-    httpie # curl++
-    kubectl
-    kubectx
-    podman-compose
-
-    # tops {{{1
-    btop # best
-    ctop # containers
-    gotop # cute
-    htop # basic
-    iotop # detailed io info, requires sudo
-    nvitop # nvidia gpu
-    podman-tui # podman container status
-    nvtopPackages.full # top for GPUs # maybe switch to .nvidia and .intel
-    zenith-nvidia # top WITH nvidia GPUs
-
-    # img {{{2
-    imagemagickBig # CLI image manipulation
-    libwebp # tools for WebP image format
-    exiftool # read/write EXIF metadata
-    chafa # sixel, kitty, iterm2, unicode
-    timg # sixel, kitty, iterm2, unicode; faster than chafa
-
-    # video {{{1
-    ffmpeg-full
-    yt-dlp # download youtube videos
-
-    # audio {{{1
-    sox # CLI audio processing
-    lame # mp3
-    piper-tts # good neural TTS
-
-    # docs {{{1
-    slides # markdown presentation in terminal
-    glow # markdown viewer
-    poppler_utils # pdf utils
-    ghostscript # postscript/pdf utils
-    readability-cli # extracts main content from pages
-    easyocr # neural OCR
-    pandoc # markup converter (latex, markdown, etc)
-    djvulibre # djvu tools
-    # }}}
   ];
 }
