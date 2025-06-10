25de7885529b4d37f1aba0e431c342f51525f8d7
Arsen Nuramatov <x@ctrl.sn>
2025-06-10 20:34:08 +0200

    auto: anuramat-t480

--- home/desktop/notifications.nix
+++ home/desktop/notifications.nix
@@ -11,20 +11,6 @@ in
       anchor = "top-center";
       text-alignment = "center";
       output = "eDP-1";
-      background-color = "${c.base00}";
-      text-color = "${c.base01}";
-      border-color = "${c.base01}";
-      "urgency=low" = {
-        border-color = "${c.base01}";
-      };
-      "urgency=normal" = {
-        border-color = "${c.base01}";
-      };
-      "urgency=high" = {
-        border-color = "#000000";
-        background-color = "#FFFF00";
-        text-color = "#000000";
-      };
       "app-name=Spotify" = {
         invisible = "1";
       };
