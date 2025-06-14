bffe2286030b540ea38a5ff7ff2d022b40a1ad06
Arsen Nuramatov <x@ctrl.sn>
2025-06-14 18:49:07 +0200

    migrate git plugins (gitsigns, gitlinker-nvim) to nvf

--- legacyNvimConfig/lua/plugins/core/git.lua
+++ legacyNvimConfig/lua/plugins/core/git.lua
@@ -1,63 +1,9 @@
 -- vim: fdl=1
 local u = require('utils.helpers')
 return {
-        -- gitsigns - gutter, binds, etc.
-        {
-                -- tanvirtin/vgit.nvim
-                'lewis6991/gitsigns.nvim',
-                event = 'VeryLazy',
-                opts = {
-                        sign_priority = 1000,
-                        signs_staged = {
-                                add = { text = '▎' },
-                                change = { text = '▎' },
-                                delete = { text = '▎' },
-                                topdelete = { text = '▎' },
-                                changedelete = { text = '▎' },
-                                untracked = { text = '▎' },
-                        },
-                        on_attach = function() end,
-                },
-                keys = u.wrap_lazy_keys({
-                        -- stage
-                        { 's', function(m) m.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, mode = 'v',    'Stage selection' },
-                        { 's', function(m) m.stage_hunk() end,                                       'Stage hunk' },
-                        { 'S', function(m) m.stage_buffer() end,                                     'Stage buffer' },
-                        -- reset
-                        { 'r', function(m) m.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, mode = 'v',    'Reset selection' },
-                        { 'r', function(m) m.reset_hunk() end,                                       'Reset hunk' },
-                        { 'R', function(m) m.reset_buffer() end,                                     'Reset buffer' },
-                        -- misc
-                        { 'b', function(m) m.blame_line({ full = true }) end,                        'Blame line' },
-                        { 'p', function(m) m.preview_hunk() end,                                     'Preview hunk' },
-                        { 'd', function(m) m.diffthis() end,                                         'Diff file' },
-                }, {
-                        lhs_prefix = '<leader>g',
-                        module = 'gitsigns',
-                        desc_prefix = 'Gitsigns',
-                        exceptions = {
-                                { 'ih', function() require('gitsigns').select_hunk() end,                  'Select hunk',  mode = { 'o', 'x' } },
-                                { 'ah', function() require('gitsigns').select_hunk({ greedy = true }) end, 'Select hunk',  mode = { 'o', 'x' } },
-                                { ']h', function() require('gitsigns').next_hunk() end,                    'Next hunk' },
-                                { '[h', function() require('gitsigns').prev_hunk() end,                    'Previous hunk' },
-                        },
-                }),
-        },
-        -- diffview
+        -- diffview - already enabled in nvf as utility.diffview-nvim
         {
                 'sindrets/diffview.nvim',
                 event = 'VeryLazy',
-        },
-        {
-                'ruifm/gitlinker.nvim',
-                event = 'VeryLazy',
-                opts = {
-                        opts = {
-                                add_current_line_on_normal_mode = false,
-                                print_url = true,
-                        },
-                        -- mappings = nil -- '<leader>gy',
-                        -- TODO move to lazy keys, remove event, add more mappings
-                },
         },
 }
--- nvf/default.nix
+++ nvf/default.nix
@@ -63,8 +63,41 @@
     # languages.html.treesitter.autotagHtml = true;
 
     git = {
-      gitlinker-nvim.enable = true;
-      gitsigns.enable = true;
+      gitlinker-nvim = {
+        enable = true;
+        setupOpts = {
+          opts = {
+            add_current_line_on_normal_mode = false;
+            print_url = true;
+          };
+        };
+      };
+      gitsigns = {
+        enable = true;
+        setupOpts = {
+          sign_priority = 1000;
+          signs_staged = {
+            add = { text = "▎"; };
+            change = { text = "▎"; };
+            delete = { text = "▎"; };
+            topdelete = { text = "▎"; };
+            changedelete = { text = "▎"; };
+            untracked = { text = "▎"; };
+          };
+          on_attach = "function() end";
+        };
+        mappings = {
+          stageHunk = "<leader>gs";
+          stageBuffer = "<leader>gS";
+          resetHunk = "<leader>gr";
+          resetBuffer = "<leader>gR";
+          blameLine = "<leader>gb";
+          previewHunk = "<leader>gp";
+          diffThis = "<leader>gd";
+          nextHunk = "]h";
+          previousHunk = "[h";
+        };
+      };
       vim-fugitive.enable = true;
     };
 
