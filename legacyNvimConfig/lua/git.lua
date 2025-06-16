4fc3139f693f3077758529328972f7445026f130
Arsen Nuramatov <x@ctrl.sn>
2025-06-16 21:23:40 +0200

    delete legacy nvim config

--- legacyNvimConfig/lua/git.lua
+++
@@ -1,9 +0,0 @@
--- vim: fdl=1
-local u = require('utils.helpers')
-return {
-  -- diffview - already enabled in nvf as utility.diffview-nvim
-  {
-    'sindrets/diffview.nvim',
-    event = 'VeryLazy',
-  },
-}
--- legacyNvimConfig/lua/html.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'windwp/nvim-ts-autotag',
-  ft = { 'html', 'xml', 'jsx', 'javascript' }, -- more are available
-  opts = {},
-}
