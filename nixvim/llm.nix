d390d2b4e02238474243c4a528abed965ab55eab
Arsen Nuramatov <x@ctrl.sn>
2025-06-17 00:09:39 +0200

    auto: anuramat-ll7

--- nixvim/llm.nix
+++ nixvim/llm.nix
@@ -1,10 +1,40 @@
 {
+  lib,
   inputs,
   pkgs,
   ...
 }:
 {
   plugins = {
+    avante = {
+      enable = true;
+      settings = {
+        behaviour = {
+          auto_suggestions = false;
+        };
+        providers = {
+          copilot = {
+            model = "claude-sonnet-4";
+          };
+        };
+        windows = {
+          ask = {
+            floating = true;
+            start_insert = false;
+          };
+          edit = {
+            start_insert = false;
+          };
+          input = {
+            height = 12;
+            prefix = "";
+          };
+          position = "bottom";
+          width = 40;
+          wrap = true;
+        };
+      };
+    };
     copilot-lua = {
       enable = true;
       settings = {
@@ -18,5 +48,37 @@
     };
   };
 
-  # inputs.avante.packages.${pkgs.system}.default
+  # extraPlugins = [
+  #   myInputs.avante.packages.${pkgs.system}.default
+  # ];
+  #
+  # extraConfigLua = ''
+  #   require('avante').setup({
+  #     behaviour = {
+  #       auto_suggestions = false,
+  #     },
+  #     providers = {
+  #       copilot = {
+  #         model = "claude-sonnet-4",
+  #       },
+  #     },
+  #     windows = {
+  #       ask = {
+  #         floating = true,
+  #         start_insert = false,
+  #       },
+  #       edit = {
+  #         start_insert = false,
+  #       },
+  #       input = {
+  #         height = 12,
+  #         prefix = "",
+  #       },
+  #       position = "bottom",
+  #       width = 40,
+  #       wrap = true,
+  #     },
+  #   })
+  # '';
+
 }
