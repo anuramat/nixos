c74ce7feb92c4bab289d4a9d0f9ed710bca0f648
Arsen Nuramatov <x@ctrl.sn>
2025-06-17 03:12:17 +0200

    auto: anuramat-ll7

--- nixvim/ide.nix
+++ nixvim/ide.nix
@@ -134,22 +134,21 @@
     #   sign('DapLogPoint', { text = 'L', texthl = 'DapLogPoint', linehl = '', numhl = '' })
     #   sign('DapStopped', { text = '→', texthl = 'DapStopped', linehl = '', numhl = '' })
 
+    # { 'b', function(m) m.toggle_breakpoint() end, 'Toggle Breakpoint' },
+    # { 'c', function(m) m.continue() end, 'Continue' },
+    # { 'd', function(m) m.run_last() end, 'Run Last Debug Session' },
+    # { 'i', function(m) m.step_into() end, 'Step Into' },
+    # { 'l', log_point, 'Set Log Point' },
+    # { 'n', function(m) m.step_over() end, 'Step Over' },
+    # { 'o', function(m) m.step_out() end, 'Step Out' },
+    # { 'r', function(m) m.repl.open() end, 'Open Debug REPL' },
 
-# { 'b', function(m) m.toggle_breakpoint() end, 'Toggle Breakpoint' },
-# { 'c', function(m) m.continue() end, 'Continue' },
-# { 'd', function(m) m.run_last() end, 'Run Last Debug Session' },
-# { 'i', function(m) m.step_into() end, 'Step Into' },
-# { 'l', log_point, 'Set Log Point' },
-# { 'n', function(m) m.step_over() end, 'Step Over' },
-# { 'o', function(m) m.step_out() end, 'Step Out' },
-# { 'r', function(m) m.repl.open() end, 'Open Debug REPL' },
+    # local function log_point(m) m.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end
 
-# local function log_point(m) m.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end
-
-# config = function()
-#   -- some of these are used in catppuccin
-#   -- sign('DapBreakpointRejected', { text = 'R', texthl = 'DapBreakpointRejected', linehl = '', numhl = '' })
-# end,
+    # config = function()
+    #   -- some of these are used in catppuccin
+    #   -- sign('DapBreakpointRejected', { text = 'R', texthl = 'DapBreakpointRejected', linehl = '', numhl = '' })
+    # end,
     dap-ui.enable = true;
     dap-ui.settings = {
       #   { 'u', function(m) m.toggle() end, 'Toggle Dap UI' },
