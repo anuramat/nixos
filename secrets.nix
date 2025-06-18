e8d778e325b997f6456a9ed552b98d71a6592a9a
Arsen Nuramatov <x@ctrl.sn>
2025-06-18 19:13:19 +0200

    auto: anuramat-ll7

--- secrets.nix
+++ secrets.nix
@@ -2,7 +2,8 @@ let
   # lib = import <lib>;
   lib = (import <nixpkgs> { }).lib;
   inherit (import ./hax/hosts.nix { inherit lib; }) getAllKeys;
-  keys = getAllKeys ./hosts;
+  keyPaths = getAllKeys ./hosts;
+  keys = map (v: builtins.readFile v) keyPaths;
 in
 {
   "secret.age".publicKeys = keys;
