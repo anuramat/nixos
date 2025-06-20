860661deb4d7ec7f2328b27ba2a37483411dbe86
Arsen Nuramatov <x@ctrl.sn>
2025-06-20 20:55:11 +0200

    auto: anuramat-ll7

--- home/term.nix
+++ home/term.nix
@@ -1,30 +1,25 @@
-let
-  font-family = "Hack Nerd Font";
-  font-size = "13";
-in
 {
   programs = {
     foot = {
       enable = true;
       settings = {
-        main.font = "Hack Nerd Font:size=13";
-
         scrollback.lines = 13337;
-
-        bell.urgent = "yes";
-        bell.visual = "yes";
-        bell.notify = "no";
-
-        key-bindings.show-urls-copy = "Control+Shift+y";
-        key-bindings.scrollback-home = "Shift+Home";
-        key-bindings.scrollback-end = "Shift+End";
+        bell = {
+          urgent = "yes";
+          visual = "yes";
+          notify = "no";
+        };
+        key-bindings = {
+          show-urls-copy = "Control+Shift+y";
+          scrollback-home = "Shift+Home";
+          scrollback-end = "Shift+End";
+        };
       };
     };
     ghostty = {
       enable = true;
       clearDefaultKeybinds = true;
       settings = {
-        inherit font-size font-family;
         cursor-style = "block";
         cursor-style-blink = "false";
         shell-integration-features = "no-cursor";
@@ -35,8 +30,6 @@ in
     kitty = {
       enable = true;
       settings = {
-        font_family = "Hack Nerd Font Regular";
-        font_size = 13;
         confirm_os_window_close = 0;
         tab_bar_edge = "top";
         tab_bar_style = "powerline";
