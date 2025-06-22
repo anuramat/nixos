8efe366da4a1ed8614334e354f9039e21972321d
Arsen Nuramatov <x@ctrl.sn>
2025-06-22 02:44:48 +0200

    auto: anuramat-ll7

--- home/desktop/sway/keys.nix
+++ home/desktop/sway/keys.nix
@@ -38,8 +38,7 @@ let
   screenshot_focused_output = "exec ${grim} -o $(swaymsg -t get_outputs | ${jq} -r '.[] | select(.focused) | .name') - | ${swappy} -f -";
 
   # Screencasting
-  screencast_mouse = "exec swaymsg -t get_tree | ${jq} -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | .rect | \"\\(.x),\\(.y) \\(.width)x\\(.height)\"' | ${slurp} | xargs -I {} ${wf-recorder} -g \"{}\" -f \"~/vid/screen/$(date +%Y-%m-%d_%H-%M-%S).mp4\"";
-  screencast_stop = "exec ${killall} ${wf-recorder}";
+  screencast_mouse = "exec killall --signal SIGINT || swaymsg -t get_tree | ${jq} -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | .rect | \"\\(.x),\\(.y) \\(.width)x\\(.height)\"' | ${slurp} | xargs -I {} ${wf-recorder} -g \"{}\" -f \"~/vid/screen/$(date +%Y-%m-%d_%H-%M-%S).mp4\"";
 
   # Special keys - brightness
   brightness =
@@ -96,6 +95,86 @@ let
   floatNotes = "${term_float} --working-directory=\"$HOME/notes\" -e bash $EDITOR ~/notes/scratchpad.md";
 in
 {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      windowrule = [
+        "float,title:^(foot-float)$"
+        "suppressevent maximize, class:.*"
+      ];
+      bind =
+        let
+          modifier = "SUPER";
+        in
+        [
+          "${modifier}, semicolon, exec foot"
+          "${modifier}, ${mod2}, semicolon, exec, foot -a foot-float -W 88x28"
+          # "${modifier}, apostrophe, ${floatNotes}"
+          "${modifier}, q, killactive"
+          # "${modifier}, ${mod2}, q, ${lock}"
+          # "${modifier}, ${mod3}, q, ${sleep}"
+          # "${modifier}, space, ${drun}"
+          # "${modifier}, r, ${books}"
+          # "${modifier}, t, ${todo_add}"
+          # "${modifier}, ${mod2}, t, ${todo_done}"
+          "${modifier}, ${mod2}, m, togglefloating"
+
+          "${modifier}, P, pseudo,"
+          "${modifier}, J, togglesplit,"
+
+          "${modifier}, h, movefocus, l"
+          "${modifier}, l, movefocus, r"
+          "${modifier}, k, movefocus, u"
+          "${modifier}, j, movefocus, d"
+
+          "${modifier}, 1, workspace, 1"
+          "${modifier}, 2, workspace, 2"
+          "${modifier}, 3, workspace, 3"
+          "${modifier}, 4, workspace, 4"
+          "${modifier}, 5, workspace, 5"
+          "${modifier}, 6, workspace, 6"
+          "${modifier}, 7, workspace, 7"
+          "${modifier}, 8, workspace, 8"
+          "${modifier}, 9, workspace, 9"
+          "${modifier}, 0, workspace, 10"
+
+          "${modifier} SHIFT, 1, movetoworkspace, 1"
+          "${modifier} SHIFT, 2, movetoworkspace, 2"
+          "${modifier} SHIFT, 3, movetoworkspace, 3"
+          "${modifier} SHIFT, 4, movetoworkspace, 4"
+          "${modifier} SHIFT, 5, movetoworkspace, 5"
+          "${modifier} SHIFT, 6, movetoworkspace, 6"
+          "${modifier} SHIFT, 7, movetoworkspace, 7"
+          "${modifier} SHIFT, 8, movetoworkspace, 8"
+          "${modifier} SHIFT, 9, movetoworkspace, 9"
+          "${modifier} SHIFT, 0, movetoworkspace, 10"
+
+          # # Example special workspace (scratchpad)
+          # bind = $mainMod, S, togglespecialworkspace, magic
+          # bind = $mainMod SHIFT, S, movetoworkspace, special:magic
+
+          # # Laptop multimedia keys for volume and LCD brightness
+          # bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+          # bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+          # bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+          # bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+          # bindel = ,XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+
+          # bindel = ,XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-
+
+          # # Requires playerctl
+          # bindl = , XF86AudioNext, exec, playerctl next
+          # bindl = , XF86AudioPause, exec, playerctl play-pause
+          # bindl = , XF86AudioPlay, exec, playerctl play-pause
+          # bindl = , XF86AudioPrev, exec, playerctl previous
+
+        ];
+      bindm = [
+        "${modifier}, mouse:272, movewindow"
+        "${modifier}, mouse:273, resizewindow"
+      ];
+    };
+  };
+
   wayland.windowManager.sway.config = {
 
     # Keybindings
@@ -140,8 +219,7 @@ in
       # Screenshots
       "${modifier}+p" = screenshot_mouse;
       "${modifier}+${mod2}+p" = screenshot_focused_output;
-      "${modifier}+${mod3}+p" = screencast_mouse;
-      "${modifier}+${mod4}+p" = screencast_stop;
+      "${modifier}+${mod4}+p" = screencast_mouse;
 
       # Moving focus
       "${modifier}+${left}" = "focus left";
