c93207daf55f9f7f49a9cfab5046d507b1e17906
Arsen Nuramatov <x@ctrl.sn>
2025-06-23 03:32:49 +0200

    auto: anuramat-ll7

--- home/gui/waybar.nix
+++ home/gui/waybar.nix
@@ -7,183 +7,167 @@
           padding: 0px 10px;
         }
       '';
-    settings = [
-      # TODO sort fields
-      {
-        backlight = {
-          format = "{percent}% {icon}";
-          format-icons = [
-            ""
-            ""
-            ""
-            ""
-            ""
-            ""
-            ""
-            ""
-            ""
-          ];
-        };
-        "battery#internal" = {
-          bat = "BAT0";
-          format = "I:{capacity}%";
-        };
-        "battery#external" = {
-          bat = "BAT1";
-          format = "E:{capacity}%";
-        };
-        battery = {
-          format = "{capacity}% {icon}";
-          format-alt = "{time} {icon}";
-          format-charging = "{capacity}% 󰂄";
-          format-icons = [
-            "󰂎"
-            "󰁺"
-            "󰁻"
-            "󰁼"
-            "󰁽"
-            "󰁾"
-            "󰁿"
-            "󰂀"
-            "󰂁"
-            "󰂂"
-            "󰁹"
-          ];
-          format-plugged = "{capacity}% 󰚥";
-          interval = 1;
-          states = {
-            critical = 15;
-            warning = 30;
+    settings =
+      let
+        main = {
+          mode = "dock";
+          position = "top";
+          ipc = true;
+          spacing = 5;
+          tray.spacing = 10;
+          clock = {
+            format = "{:%Y-%m-%d %H:%M:%S}";
+            interval = 1;
           };
         };
-        bluetooth = {
-          format = "{status} ";
-          on-click = "blueman-manager";
-        };
-        clock = {
-          format = "{:%Y-%m-%d %H:%M:%S}";
-          interval = 1;
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-        };
-        cpu = {
-          format = "{usage}% ";
-          tooltip = false;
-        };
-        disk = {
-          format = "{percentage_used}% ";
-          path = "/";
-        };
-        idle_inhibitor = {
-          format = "{icon}";
-          format-icons = {
-            activated = " ";
-            deactivated = " ";
-          };
-        };
-        ipc = true;
-        memory = {
-          format = "{}% ";
-        };
-        mode = "dock";
-        modules-center = [
-          "sway/workspaces"
-          "sway/scratchpad"
-        ];
-        modules-left = [
-          "pulseaudio"
-          "backlight"
-          "idle_inhibitor"
-          "mpris"
-        ];
-        spacing = 5;
-        tray = {
-          spacing = 10;
-        };
-        modules-right = [
-          "sway/window"
-          "tray"
-          "battery"
-          "battery#internal"
-          "battery#external"
-          "disk"
-          "sway/language"
-          "clock"
-        ];
-        mpris = {
-          dynamic-len = 1;
-          dynamic-priority = [
-            "length"
-            "position"
-            "album"
-          ];
-          format = "{player_icon}  {status_icon} {dynamic}";
-          interval = 1;
-          max-length = 999;
-          player = "spotify";
-          player-icons = {
-            default = "";
-            spotify = "󰓇";
-          };
-          status-icons = {
-            paused = "󰏤";
-            playing = "󰐊";
-          };
-        };
-        position = "top";
-        pulseaudio = {
-          format = "{volume}% {icon}  {format_source}";
-          format-bluetooth = "{volume}% {icon}   {format_source}";
-          format-bluetooth-muted = "{volume}% 󰖁 {icon}   {format_source}";
-          format-icons = {
-            car = "";
-            default = [
-              "󰕿"
-              "󰖀"
-              "󰕾"
+        controls = {
+          mpris = {
+            dynamic-len = 1;
+            dynamic-priority = [
+              "length"
+              "position"
+              "album"
             ];
-            hands-free = "󰋎";
-            headphone = "";
-            headset = "󰋎";
-            phone = "";
-            portable = "";
+            format = "{player_icon}  {status_icon} {dynamic}";
+            interval = 1;
+            max-length = 999;
+            player = "spotify";
+            player-icons = {
+              default = "";
+              spotify = "󰓇";
+            };
+            status-icons = {
+              paused = "󰏤";
+              playing = "󰐊";
+            };
           };
-          format-muted = "{volume}% 󰖁  {format_source}";
-          format-source = "{volume}% 󰍬";
-          format-source-muted = "{volume}% 󰍭";
-          on-click = "pavucontrol";
-          on-click-middle = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-          on-click-right = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          idle_inhibitor = {
+            format = "{icon}";
+            format-icons = {
+              activated = " ";
+              deactivated = " ";
+            };
+          };
+          pulseaudio = {
+            format = "{volume}% {icon}  {format_source}";
+            format-bluetooth = "{volume}% {icon}   {format_source}";
+            format-bluetooth-muted = "{volume}% 󰖁 {icon}   {format_source}";
+            format-icons = {
+              car = "";
+              default = [
+                "󰕿"
+                "󰖀"
+                "󰕾"
+              ];
+              hands-free = "󰋎";
+              headphone = "";
+              headset = "󰋎";
+              phone = "";
+              portable = "";
+            };
+            format-muted = "{volume}% 󰖁  {format_source}";
+            format-source = "{volume}% 󰍬";
+            format-source-muted = "{volume}% 󰍭";
+            on-click = "pavucontrol";
+            on-click-middle = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+            on-click-right = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          };
+          backlight = {
+            format = "{percent}% {icon}";
+            format-icons = [
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+            ];
+          };
         };
-        "sway/language" = {
-          tooltip-format = "{flag} {long} {variant}";
+        metrics = {
+          battery = {
+            format = "{capacity}% {icon}";
+            format-alt = "{time} {icon}";
+            format-charging = "{capacity}% 󰂄";
+            format-icons = [
+              "󰂎"
+              "󰁺"
+              "󰁻"
+              "󰁼"
+              "󰁽"
+              "󰁾"
+              "󰁿"
+              "󰂀"
+              "󰂁"
+              "󰂂"
+              "󰁹"
+            ];
+            format-plugged = "{capacity}% 󰚥";
+            interval = 1;
+            states = {
+              critical = 15;
+              warning = 30;
+            };
+          };
+          cpu.format = "{usage}% ";
+          memory.format = "{}% ";
+          disk = {
+            format = "{percentage_used}% ";
+            path = "/";
+          };
+          temperature = {
+            critical-threshold = 80;
+            format = "{temperatureC}°C {icon}";
+            format-icons = [
+              "󱃃"
+              "󰔏"
+              "󱃂"
+            ];
+          };
         };
-        "sway/scratchpad" = {
-          format = "{icon} {count}";
-          format-icons = [
-            ""
-            ""
+        modules = {
+          modules-left = [
+            "pulseaudio"
+            "backlight"
+            "idle_inhibitor"
+            "mpris"
           ];
-          show-empty = false;
-          tooltip = true;
-          tooltip-format = "{app}: {title}";
-        };
-        "sway/window" = {
-          icon = true;
-        };
-        "sway/workspaces" = {
-          disable-scroll = true;
-          format = "{name}";
-        };
-        temperature = {
-          critical-threshold = 80;
-          format = "{temperatureC}°C {icon}";
-          format-icons = [
-            "󱃃"
-            "󰔏"
-            "󱃂"
+          modules-center = [
+            "sway/workspaces"
+            "sway/scratchpad"
+          ];
+          modules-right = [
+            "sway/window"
+            "tray"
+            "battery"
+            "disk"
+            "sway/language"
+            "clock"
           ];
         };
-      }
-    ];
+        sway = {
+          "sway/scratchpad" = {
+            format = "{icon} {count}";
+            format-icons = [
+              ""
+              ""
+            ];
+            show-empty = false;
+          };
+          "sway/window" = {
+            icon = true;
+          };
+          "sway/workspaces" = {
+            disable-scroll = true;
+            format = "{name}";
+          };
+        };
+      in
+      [
+        (main // modules // sway // controls // metrics)
+      ];
   };
 }
