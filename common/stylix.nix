52f5b7954628759bf91982498d28bc8ec7b51a7c
Arsen Nuramatov <x@ctrl.sn>
2025-06-24 01:33:14 +0200

    auto: anuramat-ll7

--- common/stylix.nix
+++ common/stylix.nix
@@ -23,14 +23,28 @@
     };
     base16Scheme =
       let
-        # tokyo-night-dark
         # stella -- pastel purple bg, otherwise dracula vibes
         # black-metal -- mostly black and white, shades of very faint pastel red and blue otherwise
         # paraiso -- pastel burgundy bg, almost monochrome
         # greenscreen -- green monochrome
-        name = "greenscreen";
+        # grayscale-dark -- duh
+        # tokyo-night-dark -- colder dracula
+        # pandora -- pink/burgundy/red mostly
+        # caroline -- warm tones, red/brown
+        # digital-rain -- tons of green
+        # eris -- navy + pastel red
+        # heetch -- red with purple bg
+        # tarot -- red, burgundy, purple, kinda like darker heetch
+        # saga -- almost b/w
+        # onedark-dark -- retro vibes, like elf
+        # pasque -- kinda like helix, purple mostly
+        # pinky -- very colorful, but looks mostly red/blue
+        # pop -- darker elflord kind?
+        # windows-nt -- the highest contrast among windowsk, reminds me of elflord
+        # shades-of-purple
+        # zenbones
+        name = "windows-nt";
       in
       "${pkgs.base16-schemes}/share/themes/${name}.yaml";
-    # ./elflord.yaml;
   };
 }
