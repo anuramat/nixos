3b5ec9e4a15ada801e2cb39343acc3e608a9126e
Arsen Nuramatov <x@ctrl.sn>
2025-06-27 01:36:22 +0200

    auto: anuramat-ll7

--- home/nixvim/ide/completion.nix
+++ home/nixvim/ide/completion.nix
@@ -1,22 +1,6 @@
 {
   plugins = {
     friendly-snippets.enable = true;
-    blink-cmp = {
-      enable = true;
-      # TODO jesus christ that's ugly
-      # luaConfig.post = ''
-      #   require('blink.cmp').setup({
-      #     sources = {
-      #       providers = {
-      #         snippets = {
-      #           opts = {
-      #             search_paths = { vim.fn.stdpath('data') .. '/lazy/friendly-snippets' },
-      #           },
-      #         },
-      #       },
-      #     },
-      #   })
-      # '';
-    };
+    blink-cmp.enable = true;
   };
 }
