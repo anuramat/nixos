93aa5b9df72b92d66c663406a73aa82bfdcb460a
Arsen Nuramatov <x@ctrl.sn>
2025-07-04 17:58:59 +0200

    auto: anuramat-ll7

--- home/tui/pandoc.nix
+++ home/tui/pandoc.nix
@@ -1,71 +1,82 @@
 {
   pkgs,
-  lib,
   config,
   ...
 }:
 let
-  # TODO --citeproc?
-  # TODO maybe switch to --pdf-engine xelatex
-  inputFormat = "markdown+wikilinks_title_after_pipe+mark";
-  # mark: ==highlighted text==
-  preamblePath = "${config.xdg.configHome}/latex/preamble.tex";
-  # TODO replace notify-send with a proper ref from store
-  inherit (lib) getExe;
-  zathura = getExe pkgs.zathura;
-  entr = getExe pkgs.entr;
-  popupDuration = 1000; # ms
-  notify =
-    text: duration: id:
-    "${pkgs.libnotify}/bin/notify-send ${if id == "" then "" else "-r ${id}"} ${
-      if duration <= 0 then "" else "-t ${toString duration}"
-    } -p ${text}";
-  pdfPlaceholder = "JVBERi0xLgoxIDAgb2JqPDwvUGFnZXMgMiAwIFI+PmVuZG9iagoyIDAgb2JqPDwvS2lkc1szIDAgUl0vQ291bnQgMT4+ZW5kb2JqCjMgMCBvYmo8PC9QYXJlbnQgMiAwIFI+PmVuZG9iagp0cmFpbGVyIDw8L1Jvb3QgMSAwIFI+Pg==";
-  hotdoc =
-    pkgs.writeShellScriptBin "hotdoc"
+  hotdoc = pkgs.writeShellApplication {
+    name = "hotdoc";
+    runtimeInputs =
+      (with pkgs; [
+        entr
+        zathura
+      ])
+      ++ [ render ];
+    text =
+      let
+        pdfPlaceholder = "JVBERi0xLgoxIDAgb2JqPDwvUGFnZXMgMiAwIFI+PmVuZG9iagoyIDAgb2JqPDwvS2lkc1szIDAgUl0vQ291bnQgMT4+ZW5kb2JqCjMgMCBvYmo8PC9QYXJlbnQgMiAwIFI+PmVuZG9iagp0cmFpbGVyIDw8L1Jvb3QgMSAwIFI+Pg==";
+      in
       # bash
       ''
-        	# renders $1.md to pdf, opens in zathura, rerenders on save
-        	# usage: $0 $target
-        	md=$(realpath "$1")
-        	name=$(basename -s .md "$1")
-          shift
-          pdf="$(mktemp --tmpdir "$name-XXXXXXXX.pdf")"
+        main() {
+          	# renders $1.md to pdf, opens in zathura, rerenders on save
+          	# usage: $0 $target
+          	md=$(realpath "$1")
+          	name=$(basename -s .md "$1")
+            shift
+            pdf="$(mktemp --tmpdir "$name-XXXXXXXX.pdf")"
 
-        	# initialize it with a basic pdf so that zathura doesn't shit itself
-        	echo '${pdfPlaceholder}' | base64 -d > "$pdf"
+          	# initialize it with a basic pdf so that zathura doesn't shit itself
+          	echo '${pdfPlaceholder}' | base64 -d > "$pdf"
 
-        	# open zathura
-        	nohup ${zathura} "$pdf" &> /dev/null &
-        	zathura_pid="$!"
+          	# open zathura
+          	nohup zathura "$pdf" &> /dev/null &
+          	zathura_pid="$!"
 
-        	# start watching, recompile on change
-        	cmd="${render} $(printf "'%s' " "$md" "$pdf" "$@")"
-        	${entr} -rcsn "$cmd" < <(echo "$md") &
-        	entr_pid="$!"
+          	# start watching, recompile on change
+          	cmd="render $(printf "'%s' " "$md" "$pdf" "$@")"
+          	entr -rcsn "$cmd" < <(echo "$md") &
+          	entr_pid="$!"
 
-        	# stop watching if zathura is closed
-        	wait "$zathura_pid"
-        	kill "$entr_pid"
+          	# stop watching if zathura is closed
+          	wait "$zathura_pid"
+          	kill "$entr_pid"
+          }
+          main "$@" &>/dev/null & disown
       '';
-  render =
-    pkgs.writeShellScriptBin "render"
+  };
+  render = pkgs.writeShellApplication {
+    name = "render";
+    runtimeInputs = with pkgs; [
+      pandoc
+      libnotify
+    ];
+    text =
+      let
+        preamblePath = "${config.xdg.configHome}/latex/preamble.tex";
+        popupDuration = "1000"; # ms
+        inputFormat = "markdown+wikilinks_title_after_pipe+mark";
+        # TODO --citeproc?
+        # TODO maybe switch to --pdf-engine xelatex
+        # mark: ==highlighted text==
+      in
       # bash
       ''
         (( $# < 2 )) && {
           echo "usage: $0 input.md output.pdf [args...]"
           exit 1
         }
-
-        id=$(${notify "rendering" 0 ""})
         input=$1 && shift
         output=$1 && shift
-        if log=$(${getExe pkgs.pandoc} "$input" -o "$output" -f ${inputFormat} -H ${preamblePath} "$@" 2>&1); then
-          ${notify "done" popupDuration "$id"}
+
+        id=$(notify-send -p "rendering")
+        if log=$(pandoc "$input" -o "$output" -f ${inputFormat} -H ${preamblePath} "$@" 2>&1); then
+          notify-send -r "$id" -t ${popupDuration} "render ok"
         else
-          ${notify ''"$log"'' 0 "$id"}
+          notify-send -r "$id" -u critical "render error" "$log"
         fi
       '';
+  };
 in
 {
   home.packages = [
