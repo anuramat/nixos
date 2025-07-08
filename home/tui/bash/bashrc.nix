accb1c179bda1cbc552193e0a70335b56825bf4a
Arsen Nuramatov <x@ctrl.sn>
2025-07-08 10:12:06 +0200

    auto: anuramat-ll7

--- home/tui/bash/bashrc.nix
+++ home/tui/bash/bashrc.nix
@@ -8,7 +8,6 @@ let
   inherit (lib) getExe;
   undistract = # bash
     ''
-
       export UNDISTRACT_TOLERANCE=5
 
       __undistract_preexec() {
@@ -31,82 +30,107 @@ let
       precmd_functions+=(__undistract)
     '';
 
-  template = # bash
-    ''
-      template() (
-        path="$HOME/notes/templates/$1"
-        [ -d "$path" ] || {
-          echo "$path is not a directory"
-        }
-        shopt -s dotglob
-        cp "$path"/* ./
-      )
-    '';
-
-  reflake = # bash
-    ''
-      __jq_equals() {
-        # checks if prop is equal in both jsons
-        if [ "$#" -ne 3 ]; then
-          echo 'error(usage): __jq_equals: not enough args'
-          return 1
-        fi
-        local prop=$1
-        local a=$2
-        local b=$3
-
-        local prop_a=$(jq -r "$prop | tojson" "$a")
-        local match=$(jq -r --arg prop_a "$prop_a" "$prop == (\$prop_a | fromjson)" "$b")
-        [ "$match" == true ]
-      }
-      reflake() {
-        local target_file="$1"
-        [ -z "$target_file" ] && target_file=./flake.lock
-
-        local source_file="/etc/nixos/flake.lock"
-
-        for file in "$source_file" "$target_file"; do
-          [ -f "$file" ] || {
-            echo "$file not found"
-            return 1
+  template = pkgs.writeShellApplication {
+    name = "template";
+    text = # bash
+      ''
+        main() (
+          local root="$HOME/notes/templates"
+          [ -z "$1" ] && ls root
+          local path="$root/$1"
+          [ -d "$path" ] || {
+            echo "$path is not a directory"
           }
-        done
+          shopt -s dotglob
+          cp "$path"/* ./
+        )
+        main "$@"
+      '';
+  };
 
-        while IFS= read -r -d ''' input_name; do
-          local input=".nodes.\"$input_name\""
-          local lockProp="$input.locked"
-
-          # skip inputs without lock
-          local source_has_prop=$(jq -r "$lockProp != null" "$source_file")
-          # NOTE indirect is deprecated
-          local indirect=$(jq -r "$input.original.type == \"indirect\"" "$target_file")
-          [ "$indirect" == true ] && echo 'warning: deprecated input type "indirect" found'
-          if [ "$source_has_prop" == false ] || [ "$indirect" == true ]; then
-            continue
+  reflake = pkgs.writeShellApplication {
+    name = "reflake";
+    runtimeInputs = with pkgs; [
+      jq
+      moreutils
+    ];
+    text = # bash
+      ''
+        __jq_equals() {
+          # checks if prop is equal in both jsons
+          if [ "$#" -ne 3 ]; then
+            echo 'error(usage): __jq_equals: not enough args'
+            return 1
           fi
+          local prop=$1
+          local a=$2
+          local b=$3
 
-          # make sure it's actually the same input (compare the sources)
-          __jq_equals "$input.original" "$source_file" "$target_file" || {
-            echo ".original mismatch on $input"
-            return 1
-          }
+          local prop_a=$(jq -r "$prop | tojson" "$a")
+          local match=$(jq -r --arg prop_a "$prop_a" "$prop == (\$prop_a | fromjson)" "$b")
+          [ "$match" == true ]
+        }
+        main() {
+          local target_file="$1"
+          [ -z "$target_file" ] && target_file=./flake.lock
 
-          # replace the lock
-          local new_value=$(jq -r "$lockProp | tojson" "$source_file")
-          local replace_expr="$lockProp = (\$arg | fromjson)"
-          jq --arg arg "$new_value" "$replace_expr" "$target_file" | sponge "$target_file"
-        done < <(jq --raw-output0 ".nodes | keys[]" "$target_file")
+          local source_file="/etc/nixos/flake.lock"
 
-        echo "synced $target_file with system flake.lock"
-      }
-    '';
+          for file in "$source_file" "$target_file"; do
+            [ -f "$file" ] || {
+              echo "$file not found"
+              return 1
+            }
+          done
+
+          while IFS= read -r -d ''' input_name; do
+            local input=".nodes.\"$input_name\""
+            local lockProp="$input.locked"
+
+            # skip inputs without lock
+            local source_has_prop=$(jq -r "$lockProp != null" "$source_file")
+            # NOTE indirect is deprecated
+            local indirect=$(jq -r "$input.original.type == \"indirect\"" "$target_file")
+            [ "$indirect" == true ] && echo 'warning: deprecated input type "indirect" found'
+            if [ "$source_has_prop" == false ] || [ "$indirect" == true ]; then
+              continue
+            fi
+
+            # make sure it's actually the same input (compare the sources)
+            __jq_equals "$input.original" "$source_file" "$target_file" || {
+              echo ".original mismatch on $input"
+              return 1
+            }
+
+            # replace the lock
+            local new_value=$(jq -r "$lockProp | tojson" "$source_file")
+            local replace_expr="$lockProp = (\$arg | fromjson)"
+            jq --arg arg "$new_value" "$replace_expr" "$target_file" | sponge "$target_file"
+          done < <(jq --raw-output0 ".nodes | keys[]" "$target_file")
+
+          echo "synced $target_file with system flake.lock"
+        }
+        main "$@"
+      '';
+  };
+  restack = pkgs.writeShellApplication {
+    # runtimeInputs = with pkgs; [
+    name = "restack";
+    text = # bash
+      ''
+        find . -name stack.yaml -exec sed -i "s/^resolver:.*/resolver: $1/" {} \;
+      '';
+  };
 in
 {
+  home.packages = [
+    restack
+    reflake
+    template
+  ];
   programs.bash.bashrcExtra =
     # xdg shims should be here
     undistract
-    + reflake
-    + template
     +
       # bash
       ''
