e4b499a1d220b424dec55b4e3f0b48aa245c489a
Arsen Nuramatov <x@ctrl.sn>
2025-07-08 14:38:29 +0200

    auto: anuramat-ll7

--- common/overlays.nix
+++ common/overlays.nix
@@ -84,10 +84,10 @@ in
         };
       });
       claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
-        version = "1.0.31";
+        version = "1.0.44";
         src = prev.fetchzip {
           url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-          hash = "sha256-DrjR1LHnLXk2cW3zXO1jxc3octITMq6t28E2KJHBxZE=";
+          hash = "sha256-Dnooy0KNfhirTu7hv6DfwL7SHwf++CKtG8VHptNhcxU=";
         };
       });
     })
