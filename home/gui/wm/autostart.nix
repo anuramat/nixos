1fedb1dc084c0ab34c791ac6e705676b174d4878
Arsen Nuramatov <x@ctrl.sn>
2025-07-09 23:26:49 +0200

    auto: anuramat-ll7

--- home/gui/wm/autostart.nix
+++ home/gui/wm/autostart.nix
@@ -2,27 +2,37 @@
 {
   wayland.windowManager.sway = {
     systemd.xdgAutostart = true;
-    config.startup =
+    config.startup = [
+      {
+        # <https://github.com/nix-community/home-manager/issues/2797>
+        command = "${pkgs.kanshi}/bin/kanshictl reload";
+        always = true;
+      }
+      # {
+      #   # shouldn't be necessary but sometimes waybar bugs out
+      #   command = "pgrep waybar || systemctl restart --user waybar.service";
+      #   always = true;
+      # }
+    ];
+  };
+
+  systemd.user.services = {
+    wl-clip-persist =
       let
-        clip = lib.getExe pkgs.wl-clip-persist;
-        killall = lib.getExe pkgs.killall;
+        # TODO target
+        target = "graphical-session.target";
       in
-      [
-        {
-          # TODO maybe use a different clipboard manager?
-          command = "${killall} wl-clip-persist || ${clip} --clipboard regular";
-          always = true;
-        }
-        {
-          # <https://github.com/nix-community/home-manager/issues/2797>
-          command = "${pkgs.kanshi}/bin/kanshictl reload";
-          always = true;
-        }
-        {
-          # shouldn't be necessary but sometimes waybar bugs out
-          command = "systemctl restart --user waybar.service";
-          always = true;
-        }
-      ];
+      {
+        Unit = {
+          Description = "Persistent clipboard for Wayland";
+          PartOf = [ target ];
+          After = [ target ];
+        };
+        Service = {
+          ExecStart = "${lib.getExe pkgs.wl-clip-persist} --clipboard regular";
+          Restart = "always";
+        };
+        Install.WantedBy = [ target ];
+      };
   };
 }
