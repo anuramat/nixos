c81244cff42d727deaf75380415e8f0b40145290
Arsen Nuramatov <x@ctrl.sn>
2025-07-17 18:15:55 +0200

    auto: anuramat-ll7

--- home/tui/search.nix
+++ home/tui/search.nix
@@ -23,7 +23,6 @@ let
           ${bat} --style=numbers --color=always "$1" && exit
         fi
       '';
-  fd = "fd -u --exclude .git/";
 in
 {
 
@@ -58,40 +57,44 @@ in
       enable = true;
     };
 
-    fzf = {
-      enable = true;
-      defaultCommand = fd;
+    fzf =
+      let
+        fd = "fd"; # TODO delete if everything works right
+      in
+      {
+        enable = true;
+        defaultCommand = fd;
 
-      changeDirWidgetCommand = "${fd} -t d";
-      # changeDirWidgetOptions = "$default_preview";
-      # fileWidgetCommand = "$FZF_DEFAULT_COMMAND";
-      # fileWidgetOptions = "$default_preview";
+        changeDirWidgetCommand = "${fd} -t d";
+        # changeDirWidgetOptions = "$default_preview";
+        # fileWidgetCommand = "$FZF_DEFAULT_COMMAND";
+        # fileWidgetOptions = "$default_preview";
 
-      defaultOptions = [
-        "--layout=reverse"
-        "--keep-right"
-        "--info=inline"
-        "--tabstop=2"
-        "--multi"
-        "--height=50%"
+        defaultOptions = [
+          "--layout=reverse"
+          "--keep-right"
+          "--info=inline"
+          "--tabstop=2"
+          "--multi"
+          "--height=50%"
 
-        "--tmux=center,90%,80%"
+          "--tmux=center,90%,80%"
 
-        "--bind='ctrl-/:change-preview-window(down|hidden|)'"
-        "--bind='ctrl-j:accept'"
-        "--bind='tab:toggle+down'"
-        "--bind='btab:toggle+up'"
+          "--bind='ctrl-/:change-preview-window(down|hidden|)'"
+          "--bind='ctrl-j:accept'"
+          "--bind='tab:toggle+down'"
+          "--bind='btab:toggle+up'"
 
-        "--bind='ctrl-y:preview-up'"
-        "--bind='ctrl-e:preview-down'"
-        "--bind='ctrl-u:preview-half-page-up'"
-        "--bind='ctrl-d:preview-half-page-down'"
-        "--bind='ctrl-b:preview-page-up'"
-        "--bind='ctrl-f:preview-page-down'"
+          "--bind='ctrl-y:preview-up'"
+          "--bind='ctrl-e:preview-down'"
+          "--bind='ctrl-u:preview-half-page-up'"
+          "--bind='ctrl-d:preview-half-page-down'"
+          "--bind='ctrl-b:preview-page-up'"
+          "--bind='ctrl-f:preview-page-down'"
 
-        "--preview='${preview} {}'"
-      ];
-    };
+          "--preview='${preview} {}'"
+        ];
+      };
 
     zoxide = {
       enable = true;
