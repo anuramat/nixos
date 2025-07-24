9a484d5bd4ae2c113649b1ba6e899e93cc9fddd3
Arsen Nuramatov <x@ctrl.sn>
2025-07-24 20:29:49 +0200

    auto: anuramat-ll7

--- home/llm/goose.nix
+++ home/llm/goose.nix
@@ -1,1 +1,15 @@
-{ }
+{ pkgs, ... }:
+{
+  home.packages = [
+    (hax.agents.mkSandbox {
+      inherit pkgs;
+      pname = "gse";
+      agentName = name;
+      cmd = "${lib.getExe pkgs.claude-code}";
+      extraRwDirs = [
+        "$XDG_CONFIG_HOME/goose"
+        "$XDG_STATE_HOME/goose"
+      ];
+    })
+  ];
+}
