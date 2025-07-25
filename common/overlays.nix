021e7b70379d41eb86a455869f11401056cf0c4f
Arsen Nuramatov <x@ctrl.sn>
2025-07-25 19:22:02 +0200

    auto: anuramat-ll7

--- common/overlays.nix
+++ common/overlays.nix
@@ -96,10 +96,10 @@ in
         };
       });
       claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
-        version = "1.0.44";
+        version = "1.0.60";
         src = prev.fetchzip {
           url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-          hash = "sha256-Dnooy0KNfhirTu7hv6DfwL7SHwf++CKtG8VHptNhcxU=";
+          hash = "sha256-ygeitenu4z9ACbezO53I2Xnk6NtE1fWVzCi3mZS7wF8=";
         };
       });
       ${opencodeName} = prev.callPackage (
