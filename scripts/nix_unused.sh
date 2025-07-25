a23d4488cf9f1f0aee4daa76ea827269e473cef9
Arsen Nuramatov <x@ctrl.sn>
2025-07-26 00:12:24 +0200

    auto: anuramat-ll7

--- scripts/nix_unused.sh
+++ scripts/nix_unused.sh
@@ -2,40 +2,25 @@
 
 set -euo pipefail
 
-onlyfails=t
-
-red() {
-	printf '\033[38;2;255;0;0m%s\033[0m\n' "$1"
-}
-
 while IFS= read -r path; do
-	echo "$path" | rg --no-config -vq 'hosts/[^/]+$' || continue
 	filename=$(basename "$path")
-	use_counter=0
 	exp="(\./.*$filename)"
 	rg_args=(--no-config -g '*.nix' -r '${1}' --only-matching --with-filename)
+
+	used=false
 	while IFS=: read -r matchfile matchstring; do
 		normalized=$(realpath -q "$(dirname "$matchfile")/$matchstring") || continue
 		if [ "$path" = "$normalized" ]; then
-			((use_counter++)) || true
-			[ -n "$onlyfails" ] && break
+			used=true && break
 		fi
 	done < <(
 		rg "${rg_args[@]}" "^\s*$exp$" /etc/nixos || true
 		rg "${rg_args[@]}" "import $exp" /etc/nixos || true
 	)
-	if ((use_counter > 0)); then
-		[ -z "$onlyfails" ] && {
-			echo "$path: $use_counter references"
-		}
-	else
-		if [ -z "$onlyfails" ]; then
-			red "$path: not found"
-		else
-			echo "$path"
-		fi
-	fi
+	[[ $used == false ]] && echo "$path"
 done < <(
+	# folder imports
 	fd -ae nix | grep '/default\.nix$' | xargs -I{} dirname {}
+	# file imports
 	fd -ae nix | rg --no-config -v '(/flake\.nix$|/default\.nix)'
 )
