54f29286ac12ec0dbbe43a2b0a13e09cd7860490
Arsen Nuramatov <x@ctrl.sn>
2025-07-27 21:32:42 +0200

    auto: anuramat-ll7

---
+++ home/tui/bin/findfile.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+filename="$1"
+[[ -z $filename ]] && {
+	echo "Usage: $0 <filename>" >&2
+	exit 1
+}
+
+dir="$PWD"
+files=()
+while true; do
+	[[ -f "$dir/$filename" ]] && files=("$dir/$filename" "${files[@]}")
+	echo "$dir"
+	[[ $dir == "/" ]] && break
+	dir="$(dirname "$dir")"
+done
+
+for file in "${files[@]}"; do cat "$file"; done
