2b2be3e97046d14d747d0de3cd9e59f9b242eeee
Arsen Nuramatov <x@ctrl.sn>
2025-07-29 14:57:17 +0200

    auto: anuramat-ll7

--- home/lang/python.nix
+++ home/lang/python.nix
@@ -7,42 +7,69 @@
 let
   # Convert nested attrset to Python config assignment statements
   toYAML = lib.generators.toYAML { };
+  toJSON = lib.generators.toJSON { };
 in
 {
-  xdg.configFile."conda/condarc".text = toYAML {
-    channels = [
-      "conda-forge"
-      "conda"
-    ];
-    auto_activate_base = false;
-    changeps1 = false;
-  };
-
   home.sessionVariables.JUPYTER_CONFIG_DIR = "${config.xdg.configHome}/jupyter"; # ~/.jupyter/
-  xdg.configFile."jupyter/jupyter_server_config.py".text =
-    let
-      cfg = {
-        ContentsManager.allow_hidden = false; # show .files
-        ServerApp = {
-          ip = "0.0.0.0";
-          port = 8888;
-          open_browser = false;
-          password = "";
-          token = "";
-          disable_check_xsrf = true; # required by molten
-        };
+  xdg.configFile = (
+    {
+      "python/pythonrc".text = # python
+        '''';
+      "ipython/profile_default/startup/00-default.py".text = # python
+        '''';
+      "conda/condarc".text = toYAML {
+        channels = [
+          "conda-forge"
+          "conda"
+        ];
+        auto_activate_base = false;
+        changeps1 = false;
       };
-      root = "c";
-    in
-    # python
-    ''
-      ${root} = get_config()
-      ${hax.common.pythonConfig root cfg}
-    '';
+      "jupyter/jupyter_server_config.py".text =
+        let
+          cfg = {
+            ContentsManager.allow_hidden = false; # show .files
+            ServerApp = {
+              ip = "0.0.0.0";
+              port = 8888;
+              open_browser = false;
+              password = "";
+              token = "";
+              disable_check_xsrf = true; # required by molten
+            };
+          };
+          root = "c";
+        in
+        # python
+        ''
+          ${root} = get_config()
+          ${hax.common.pythonConfig root cfg}
+        '';
+    }
+    // (
+      let
+        mkSettingsJSON =
+          root: settings: lib.mapAttrs' (file: contents: lib.nameValuePair (root + file) (toJSON contents));
+      in
+      mkSettingsJSON "jupyter/lab/user-settings/@jupyterlab/" {
+        "docmanager-extension/plugin.jupyterlab-settings" = {
+          autosave = false;
+        };
+        "extensionmanager-extension/plugin.jupyterlab-settings" = {
+          disclaimed = true;
+        };
+        "apputils-extension/notification.jupyterlab-settings" = {
+          fetchNews = "false"; # sic (!)
+        };
+
+        "apputils-extension/themes.jupyterlab-settings" = {
+          adaptive-theme = true;
+        };
+      }
+    )
+  );
 
   home.sessionVariables.IPYTHONDIR = "${config.xdg.configHome}/ipython"; # ~/.ipython/; mixes configs with data
-  xdg.configFile."ipython/profile_default/startup/00-default.py".text = # python
-    '''';
 
   home.sessionVariables = {
     PYTHONPYCACHEPREFIX = "${config.xdg.cacheHome}/python";
@@ -50,8 +77,6 @@ in
     PYTHONUSERBASE = "${config.xdg.dataHome}/python";
     PYTHON_HISTORY = "${config.xdg.stateHome}/python/history";
   };
-  xdg.configFile."python/pythonrc".text = # python
-    '''';
 
   programs.matplotlib = {
     enable = true;
