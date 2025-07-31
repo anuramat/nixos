24c7ce05bd57edc70ef749a69431e481fb5a7aa9
Arsen Nuramatov <x@ctrl.sn>
2025-07-31 13:59:21 +0200

    auto: anuramat-ll7

--- inputs.nix
+++ inputs.nix
@@ -14,25 +14,30 @@ in
   mdmath = mkInput "github:anuramat/mdmath.nvim";
   subcat = mkInput "github:anuramat/subcat";
 
+  # `follows` as instructed (sure? TODO check)
   agenix = mkInput "github:yaxitech/ragenix";
   claude-desktop = mkInput "github:k3d3/claude-desktop-linux-flake";
   home-manager = mkInput "github:nix-community/home-manager/release-25.05";
   nixvim = mkInput "github:nix-community/nixvim";
   stylix = mkInput "github:danth/stylix/release-25.05";
 
-  # definitely no `follows`:
+  # definitely not:
   flake-utils.url = "github:numtide/flake-utils"; # no dependencies anyway
-  neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay"; # binary cache sake
+  neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay"; # has a cache
   nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # too dangerous
   # not even a flake
   tt-schemes = {
     url = "github:tinted-theming/schemes";
-    flake = false;
+    flake = false; # TODO how does this work
   };
 
-  # these could work, but it's still a risk
+  # these could work, but it's still a risk (TODO try)
   codex.url = "github:openai/codex";
   mcp-nixos.url = "github:utensils/mcp-nixos";
   nil.url = "github:oxalica/nil/main";
   spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+
+  # TODO check if any of these are in the community cache
+  # TODO is it possible to only look up nixpkgs stuff in cache
+  # TODO use max-jobs to fetch caches
 }
