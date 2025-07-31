33ca5c271436da7f88561aac4a83bce9ecbc3bc6
Arsen Nuramatov <x@ctrl.sn>
2025-07-31 13:28:34 +0200

    auto: anuramat-ll7

--- inputs.nix
+++ inputs.nix
@@ -1,29 +1,32 @@
 let
   mkInput = x: {
-    url = "github:${x}";
+    url = x;
     inputs.nixpkgs.follows = "nixpkgs";
   };
 in
 {
-  nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-  nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-  nixpkgs-old.url = "github:nixos/nixpkgs/nixos-24.11";
   nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  nixpkgs-old.url = "github:nixos/nixpkgs/nixos-24.11";
+  nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-  agenix = mkInput "yaxitech/ragenix";
-  claude-desktop = mkInput "k3d3/claude-desktop-linux-flake";
-  codex = mkInput "openai/codex";
-  ctrlsn.url = "git+ssh://git@github.com/anuramat/ctrl.sn?ref=main";
-  flake-utils = mkInput "numtide/flake-utils";
-  home-manager = mkInput "nix-community/home-manager/release-25.05";
-  mcp-nixos = mkInput "utensils/mcp-nixos";
-  mdformat-myst = mkInput "anuramat/mdformat-myst/dev";
-  mdmath = mkInput "anuramat/mdmath.nvim";
-  neovim-nightly-overlay = mkInput "nix-community/neovim-nightly-overlay";
-  nil = mkInput "oxalica/nil/main";
-  nixvim = mkInput "nix-community/nixvim";
-  spicetify-nix = mkInput "Gerg-L/spicetify-nix";
-  stylix = mkInput "danth/stylix/release-25.05";
-  subcat = mkInput "anuramat/subcat";
-  tt-schemes = mkInput "tinted-theming/schemes";
+  ctrlsn = mkInput "git+ssh://git@github.com/anuramat/ctrl.sn?ref=main";
+  mdformat-myst = mkInput "github:anuramat/mdformat-myst/dev";
+  mdmath = mkInput "github:anuramat/mdmath.nvim";
+  subcat = mkInput "github:anuramat/subcat";
+
+  agenix = mkInput "github:yaxitech/ragenix";
+  claude-desktop = mkInput "github:k3d3/claude-desktop-linux-flake";
+  codex = mkInput "github:openai/codex";
+  home-manager = mkInput "github:nix-community/home-manager/release-25.05";
+  nixvim = mkInput "github:nix-community/nixvim";
+  stylix = mkInput "github:danth/stylix/release-25.05";
+  tt-schemes = mkInput "github:tinted-theming/schemes";
+
+  flake-utils.url = "github:numtide/flake-utils"; # no dependencies anyway
+  neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay"; # binary cache sake
+
+  spicetify-nix.url = "github:Gerg-L/spicetify-nix"; # no data
+  mcp-nixos.url = "github:utensils/mcp-nixos"; # no data
+  nil.url = "github:oxalica/nil/main"; # no data
 }
