90cf43421cb75058a1e425717ba50d37d5054069
Arsen Nuramatov <x@ctrl.sn>
2025-07-31 10:51:32 +0200

    auto: anuramat-ll7

--- flake.nix
+++ flake.nix
@@ -1,98 +1,5 @@
 {
-  outputs =
-    {
-      nixpkgs,
-      home-manager,
-      nixvim,
-      flake-utils,
-      ...
-    }@inputs:
-    let
-      inherit (nixpkgs) lib;
-      hax = import ./hax { inherit lib inputs; };
-      hostnames = hax.hosts.getHostnames ./hosts;
-      user = {
-        username = "anuramat";
-        fullname = "Arsen Nuramatov";
-        email = "x@ctrl.sn";
-        tz = "Europe/Berlin";
-        locale = "en_US.UTF-8";
-      };
-      args = {
-        inherit inputs user hax;
-      };
-      mkSystem =
-        name:
-        let
-          args2 = args // {
-            cluster = hax.hosts.mkCluster ./hosts hostnames name;
-          };
-        in
-        lib.nixosSystem {
-          specialArgs = args2;
-          modules = [
-            (
-              { config, ... }:
-              {
-                networking.hostName = name;
-                home-manager = {
-                  extraSpecialArgs = args2;
-                  users.${user.username} = ./home;
-                };
-
-              }
-            )
-            ./system
-
-            ./common/overlays.nix
-
-            inputs.agenix.nixosModules.default
-            ./secrets/age.nix
-            inputs.stylix.nixosModules.stylix
-            ./common/stylix.nix
-
-            ./hosts/external_keys.nix
-          ]
-          ++ [
-            ./hosts/${name}
-          ];
-        };
-    in
-    {
-      nixosConfigurations =
-        hostnames
-        |> map (hostname: {
-          name = hostname;
-          value = mkSystem hostname;
-        })
-        |> builtins.listToAttrs;
-      homeConfigurations.${user.username} = home-manager.lib.homeManagerConfiguration {
-        # TODO check if this even builds
-        specialArgs = args;
-        modules = [
-          ./home
-
-          ./common/overlays.nix
-          inputs.stylix.nixosModules.stylix # TODO should be a different module -- hm specific
-          inputs.agenix.homeManagerModules.default
-          ./common/stylix.nix
-          ./secrets/age.nix
-        ];
-      };
-    }
-    // (flake-utils.lib.eachDefaultSystem (system: {
-      packages.neovim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-        inherit system;
-        extraSpecialArgs = { inherit inputs hax; };
-        module = {
-          imports = [
-            ./common/overlays.nix
-            ./home/nixvim
-          ];
-        };
-      };
-    }));
-
+  outputs = args: import ./outputs.nix args;
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -142,10 +49,5 @@
       url = "github:k3d3/claude-desktop-linux-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    forge = {
-      url = "github:anuramat/claude-desktop-linux-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 }
-# vim: fdl=0 fdm=marker
---
+++ outputs.nix
@@ -0,0 +1,92 @@
+{
+  nixpkgs,
+  home-manager,
+  nixvim,
+  flake-utils,
+  ...
+}@inputs:
+let
+  inherit (nixpkgs) lib;
+  hax = import ./hax { inherit lib inputs; };
+  hostnames = hax.hosts.getHostnames ./hosts;
+  user = {
+    username = "anuramat";
+    fullname = "Arsen Nuramatov";
+    email = "x@ctrl.sn";
+    tz = "Europe/Berlin";
+    locale = "en_US.UTF-8";
+  };
+  args = {
+    inherit inputs user hax;
+  };
+  mkSystem =
+    name:
+    let
+      args2 = args // {
+        cluster = hax.hosts.mkCluster ./hosts hostnames name;
+      };
+    in
+    lib.nixosSystem {
+      specialArgs = args2;
+      modules = [
+        (
+          { config, ... }:
+          {
+            networking.hostName = name;
+            home-manager = {
+              extraSpecialArgs = args2;
+              users.${user.username} = ./home;
+            };
+
+          }
+        )
+        ./system
+
+        ./common/overlays.nix
+
+        inputs.agenix.nixosModules.default
+        ./secrets/age.nix
+        inputs.stylix.nixosModules.stylix
+        ./common/stylix.nix
+
+        ./hosts/external_keys.nix
+      ]
+      ++ [
+        ./hosts/${name}
+      ];
+    };
+in
+{
+  nixosConfigurations =
+    hostnames
+    |> map (hostname: {
+      name = hostname;
+      value = mkSystem hostname;
+    })
+    |> builtins.listToAttrs;
+  homeConfigurations.${user.username} = home-manager.lib.homeManagerConfiguration {
+    # TODO check if this even builds
+    specialArgs = args;
+    modules = [
+      ./home
+
+      ./common/overlays.nix
+      inputs.stylix.nixosModules.stylix # TODO should be a different module -- hm specific
+      inputs.agenix.homeManagerModules.default
+      ./common/stylix.nix
+      ./secrets/age.nix
+    ];
+  };
+}
+// (flake-utils.lib.eachDefaultSystem (system: {
+  packages.neovim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+    inherit system;
+    extraSpecialArgs = { inherit inputs hax; };
+    module = {
+      imports = [
+        ./common/overlays.nix
+        ./home/nixvim
+      ];
+    };
+  };
+}))
