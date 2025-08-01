00c0744bc47b21b6b81c1919ecc67489ccc45c0e
Arsen Nuramatov <x@ctrl.sn>
2025-08-01 05:03:46 +0200

    auto: anuramat-ll7

--- home/agents/frontends/crush.nix
+++ home/agents/frontends/crush.nix
@@ -5,38 +5,28 @@
   ...
 }:
 let
-  inherit (lib)
-    mapAttrs
-    filterAttrs
-    getExe
-    ;
-  inherit (builtins)
-    tail
-    ;
+  inherit (lib) mapAttrs filterAttrs getExe;
+  inherit (builtins) tail;
   lsp =
-    let
-      servers =
-        config.programs.nixvim.plugins.lsp.servers
-        |> filterAttrs (n: v: v.enable)
-        |> mapAttrs (
-          n: v: {
-            command = getExe v.package;
-            args = if v.cmd == null then [ ] else tail v.cmd;
-            options = v.settings;
-          }
-        );
-    in
+    with config.programs.nixvim.plugins.lsp.servers;
     {
-      go = servers.gopls;
-      nix = servers.nil-ls;
-      python = servers.pyright;
-    };
+      go = gopls;
+      nix = nil-ls;
+      python = pyright;
+    }
+    |> filterAttrs (n: v: v.enable)
+    |> mapAttrs (
+      n: v: {
+        command = getExe v.package;
+        args = if v.cmd == null then [ ] else tail v.cmd;
+        options = v.settings;
+      }
+    );
   mcp = config.lib.agents.mcp.json.file;
   general = {
     context_paths = config.lib.agents.contextFiles ++ [
       config.lib.agents.instructions.path
     ];
-    # tui.compact_mode = true;
   };
 in
 {
