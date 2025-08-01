26f0835eb87f1a92f9c852b7b988ceec67a5b08f
Arsen Nuramatov <x@ctrl.sn>
2025-08-01 02:17:07 +0200

    auto: anuramat-ll7

--- secrets/age.nix
+++ secrets/age.nix
@@ -1,43 +1,16 @@
 { lib, user, ... }:
-let
-  sex =
+{
+  age.secrets =
     with builtins;
     readDir ./.
     |> attrNames
     |> filter (lib.hasSuffix ".age")
     |> map (x: {
       name = lib.removeSuffix ".age" x;
-      value = ./${x};
+      value = {
+        file = ./${x};
+        owner = user.username;
+      };
     })
     |> builtins.listToAttrs;
-in
-{
-  age = {
-    secrets = {
-      ghmcp = {
-        file = ./ghmcp.age;
-        owner = user.username;
-      };
-      litellm = {
-        file = ./litellm.age;
-        mode = "400";
-      };
-      claudecode = {
-        file = ./claudecode.age;
-        owner = user.username;
-      };
-      gemini = {
-        file = ./gemini.age;
-        owner = user.username;
-      };
-      openrouter = {
-        file = ./openrouter.age;
-        owner = user.username;
-      };
-      anthropic = {
-        file = ./anthropic.age;
-        owner = user.username;
-      };
-    };
-  };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,16 +1,19 @@
 let
-  # lib = import <lib>;
   lib = (import <nixpkgs> { }).lib;
   inherit (import ../hax/hosts.nix { inherit lib; }) getAllKeys getAllHostkeys;
+  # these need refactoring downstairs
   hostkeys = getAllHostkeys ../hosts;
   keyPaths = getAllKeys ../hosts;
   keys = hostkeys ++ map (v: builtins.readFile v) keyPaths;
 in
-{
-  "ghmcp.age".publicKeys = keys;
-  "litellm.age".publicKeys = keys;
-  "claudecode.age".publicKeys = keys;
-  "gemini.age".publicKeys = keys;
-  "openrouter.age".publicKeys = keys;
-  "anthropic.age".publicKeys = keys;
-}
+with builtins;
+readDir ./.
+|> attrNames
+|> filter (lib.hasSuffix ".age")
+|> map (x: {
+  name = x;
+  value = {
+    publicKeys = keys;
+  };
+})
+|> lib.listToAttrs
