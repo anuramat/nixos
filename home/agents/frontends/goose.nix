9a8223e17f465caf421bdfac8fc5d40c8d2ebe6e
Arsen Nuramatov <x@ctrl.sn>
2025-08-04 05:20:36 +0200

    auto: anuramat-ll7

--- home/agents/frontends/goose.nix
+++ home/agents/frontends/goose.nix
@@ -4,11 +4,36 @@
   ...
 }:
 let
-  # TODO
+  # TODO mcp; check out extensions and recipes in https://block.github.io/goose/docs/
   config = {
     GOOSE_MODE = "auto";
     GOOSE_MAX_TURNS = 9999999;
   };
+  # GOOSE_MODE: auto
+  # GOOSE_MODEL: gpt-4.1
+  # GOOSE_PROVIDER: github_copilot
+  # extensions:
+  #   computercontroller:
+  #     bundled: true
+  #     display_name: Computer Controller
+  #     enabled: true
+  #     name: computercontroller
+  #     timeout: 300
+  #     type: builtin
+  #   developer:
+  #     bundled: true
+  #     display_name: Developer Tools
+  #     enabled: true
+  #     name: developer
+  #     timeout: 300
+  #     type: builtin
+  #   memory:
+  #     bundled: true
+  #     display_name: Memory
+  #     enabled: true
+  #     name: memory
+  #     timeout: 300
+  #     type: builtin
 in
 {
   home.packages = [
