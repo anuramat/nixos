a3d9a0e11a7767ffaa409ef3b998b5b1abf0dc6e
Arsen Nuramatov <x@ctrl.sn>
2025-08-13 01:23:47 +0200

    auto: anuramat-ll7

--- common/overlays.nix
+++ common/overlays.nix
@@ -108,7 +108,7 @@ in
       });
 
       claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
-        version = "1.0.72";
+        version = "1.0.77";
         src = prev.fetchzip {
           url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
           hash = "sha256-1vIElqZ5sk62o1amdfOqhmSG4B5wzKWDLcCgvQO4a5o=";
