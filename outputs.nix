0d03b7c8b71da0bb910c4500e64fe93f6cf3cff7
Arsen Nuramatov <x@ctrl.sn>
2025-08-13 20:45:57 +0200

    auto: anuramat-ll7

--- outputs.nix
+++ outputs.nix
@@ -121,7 +121,7 @@ flake-parts.lib.mkFlake { inherit inputs; } {
     };
 
   perSystem =
-    { system, ... }:
+    { system, lib, ... }:
     let
       pkgs = import nixpkgs {
         inherit system;
@@ -129,6 +129,32 @@ flake-parts.lib.mkFlake { inherit inputs; } {
       };
     in
     {
+      treefmt = {
+        settings.formatter = {
+          shfmt.options = [
+            "--write"
+            "--simplify"
+            "--case-indent"
+            "--binary-next-line"
+          ];
+          shellharden = {
+            includes = [ "*.sh*" ];
+            command = lib.getExe pkgs.shellharden;
+            options = [ "--replace" ];
+          };
+        };
+        programs = {
+          nixfmt.enable = true;
+          stylua.enable = true;
+          shfmt = {
+            enable = true;
+            indent_size = 0;
+          };
+          yamlfmt.enable = true;
+          black.enable = true;
+          just.enable = true;
+        };
+      };
       topology = {
         modules = [
           {
