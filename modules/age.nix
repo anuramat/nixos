5d7d941e94498571040cf1b26ac6051a83e159dd
Arsen Nuramatov <x@ctrl.sn>
2025-08-14 18:14:11 +0200

    auto: anuramat-ll7

--- modules/age.nix
+++ modules/age.nix
@@ -7,17 +7,18 @@
 }:
 let
   isNixOS = config ? boot.kernelPackages;
+  secretsRoot = /${root}/secrets;
 in
 {
   age.secrets =
     with builtins;
-    readDir (root + "/secrets")
+    readDir secretsRoot
     |> attrNames
     |> filter (lib.hasSuffix ".age")
     |> map (x: {
       name = lib.removeSuffix ".age" x;
       value = {
-        file = root + x;
+        file = /${secretsRoot}/${x};
       }
       // (
         if isNixOS then
