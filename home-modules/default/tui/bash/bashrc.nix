5e0c56a0f96f43c2cc67ce072b7ff8e5b7fb4853
Arsen Nuramatov <x@ctrl.sn>
2025-08-20 04:31:41 +0200

    auto: anuramat-ll7

--- home-modules/default/tui/bash/bashrc.nix
+++ home-modules/default/tui/bash/bashrc.nix
@@ -11,21 +11,21 @@ let
       export UNDISTRACT_TOLERANCE=5
 
       __undistract_preexec() {
-      	__undistract_last_command_start_time=$(date +%s)
-      	__undistract_last_command="$1"
+        __undistract_last_command_start_time=$(date +%s)
+        __undistract_last_command="$1"
       }
       preexec_functions+=(__undistract_preexec)
 
       __undistract() {
-      	[ -z "$__undistract_last_command_start_time" ] && return
-      	local diff="$(($(date +%s) - __undistract_last_command_start_time))"
-      	((diff > UNDISTRACT_TOLERANCE)) && [[ $TERM =~ ^(foot)$ ]] && {
-      		tput bel
-      		printf "\e]777;notify;%s;%s\e\\" "Executed" "$__undistract_last_command"
-      	}
-      	# 777: foot, ghostty
-      	# 99: foot
-      	# '\e]99;;%s\e\\'
+        [ -z "$__undistract_last_command_start_time" ] && return
+        local diff="$(($(date +%s) - __undistract_last_command_start_time))"
+        ((diff > UNDISTRACT_TOLERANCE)) && [[ $TERM =~ ^(foot)$ ]] && {
+          tput bel
+          printf "\e]777;notify;%s;%s\e\\" "Executed" "$__undistract_last_command"
+        }
+        # 777: foot, ghostty
+        # 99: foot
+        # '\e]99;;%s\e\\'
       }
       precmd_functions+=(__undistract)
     '';
@@ -137,7 +137,7 @@ in
       [[ $- == *i* ]] || return
       # WARN here order matters for sure
       source ${./git.sh}
-      source ${./ps1.sh}
+      # source ${./ps1.sh}
 
       source ${./bashrc.sh}
 
