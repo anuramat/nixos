a4bbb454fc9dbd1307b613c4d2f4e7d5f96d4afc
Arsen Nuramatov <x@ctrl.sn>
2025-08-27 00:55:12 +0200

    auto: anuramat-ll7

--- nixos-configurations/anuramat-root/web/pastebin.nix
+++ nixos-configurations/anuramat-root/web/pastebin.nix
@@ -16,6 +16,7 @@ lib.mkMerge (
         settings = {
           WASTEBIN_BASE_URL = "https://${w.domain}";
           WASTEBIN_ADDRESS_PORT = "127.0.0.1:${w.port}";
+          WASTEBIN_MAX_BODY_SIZE = 1024 * 1024;
         };
       };
     })
