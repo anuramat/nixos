0900f0d114d23c6e45bcf3ea05e3d411d7e36dba
Arsen Nuramatov <x@ctrl.sn>
2025-09-08 19:44:38 +0200

    auto: anuramat-ll7

--- parts/pre-commit.nix
+++ parts/pre-commit.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-    # TODO nix fmt; wait for https://github.com/cachix/git-hooks.nix/issues/287
+  # TODO nix fmt; wait for https://github.com/cachix/git-hooks.nix/issues/287
   settings.hooks = {
     treefmt.enable = true;
   };
