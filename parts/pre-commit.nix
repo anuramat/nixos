ad9a4acc4e97bd48625a8e72e4ff3db681fa140e
Arsen Nuramatov <x@ctrl.sn>
2025-09-09 01:16:49 +0200

    auto: anuramat-ll7

--- parts/pre-commit.nix
+++ parts/pre-commit.nix
@@ -1,20 +1,20 @@
 { pkgs, ... }:
 {
   # TODO nix fmt; wait for https://github.com/cachix/git-hooks.nix/issues/287
-  # See https://flake.parts/options/git-hooks-nix.html for flake-parts git-hooks.nix documentation
+  # https://flake.parts/options/git-hooks-nix.html
   settings.hooks = {
     treefmt.enable = true;
     flake-inputs-freshness = {
       enable = true;
       name = "Check flake inputs freshness";
-      entry = "${pkgs.writeShellScriptBin "flake-inputs-check" ''
+      entry = pkgs.writeShellScript "flake-inputs-check" ''
         #!/usr/bin/env bash
         echo "pre commit flake inputs check"
         prev="$(mktemp)"
         cat ./flake.nix >"$prev"
         just flake &>/dev/null
         diff "$prev" ./flake.nix || exit 1
-      ''}/bin/flake-inputs-check";
+      '';
       files = "^flake\\.nix$";
       language = "system";
     };
