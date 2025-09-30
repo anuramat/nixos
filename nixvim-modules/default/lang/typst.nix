6045331125cd52dc1c5c8f96f213b6d6daa829e5
Arsen Nuramatov <x@ctrl.sn>
2025-09-30 21:30:58 +0200

    auto: anuramat-t480

--- nixvim-modules/default/lang/typst.nix
+++ nixvim-modules/default/lang/typst.nix
@@ -4,7 +4,16 @@
     # pkgs.vimPlugins.typst-term-preview-nvim
   ];
   plugins = {
-    typst-preview.enable = true;
+    typst-preview = {
+      enable = true;
+      settings.open_cmd =
+        let
+          firefoxWrapper = pkgs.writeShellScript "firefox-typst-preview" ''
+            nohup ${pkgs.firefox}/bin/firefox --new-window "$1" &>/dev/null
+          '';
+        in
+        "${firefoxWrapper} %s";
+    };
     lsp.servers.tinymist = {
       enable = true;
     };
