9168d709705e2fcf4b71ef755253abf025c22d98
Arsen Nuramatov <x@ctrl.sn>
2025-10-01 18:45:06 +0200

    auto: anuramat-t480

--- nixvim-modules/default/lang/typst.nix
+++ nixvim-modules/default/lang/typst.nix
@@ -4,7 +4,10 @@
     typst-preview = {
       enable = true;
       # TODO requires manual setup on first launch; can be solved with the next home-manager release
-      settings.open_cmd = "${lib.getExe pkgs.firefox} -P typst-preview %s";
+      settings = {
+        open_cmd = "${lib.getExe pkgs.firefox} -P typst-preview %s";
+        invert_colors = "auto";
+      };
     };
     lsp.servers.tinymist = {
       enable = true;
