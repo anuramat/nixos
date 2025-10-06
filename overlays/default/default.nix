dac0acf514c0a945f7dba123ea0ff49f2ee928ba
Arsen Nuramatov <x@ctrl.sn>
2025-10-06 22:15:05 +0200

    auto: anuramat-ll7

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -44,7 +44,7 @@ let
         ghostty
         ;
       vimPlugins = prev.vimPlugins // {
-        inherit (unstable)
+        inherit (unstable.vimPlugins)
           tinted-nvim
           ;
       };
