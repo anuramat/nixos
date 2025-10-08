3f4c6d4b1b046857e614e592eb2d7e653bfaa3ca
Arsen Nuramatov <x@ctrl.sn>
2025-10-09 00:24:30 +0200

    auto: anuramat-ll7

--- nixvim-modules/default/lang/python.nix
+++ nixvim-modules/default/lang/python.nix
@@ -5,17 +5,5 @@
       "black"
     ];
     lsp.servers.pyright.enable = true;
-
-    jupytext = {
-      enable = true;
-      # BUG TODO jupytext doesn't get installed automatically, report
-      # related but closed: <https://github.com/nix-community/nixvim/issues/2367>
-      python3Dependencies = ps: with ps; [ jupytext ];
-      settings = {
-        force_ft = "markdown";
-        output_extension = "md";
-        style = "markdown";
-      };
-    };
   };
 }
