ad02f71e4f5215c295dda6f0958e996ba13311de
Arsen Nuramatov <x@ctrl.sn>
2025-10-23 01:02:18 +0200

    auto: anuramat-f12

--- home-modules/heavy-linux/desktop/sway/inputs.nix
+++ home-modules/heavy-linux/desktop/sway/inputs.nix
@@ -1,11 +1,25 @@
 {
   wayland.windowManager.sway.config.input = {
     "*" = {
+      accel_profile = "flat";
+    };
+
+    "type:keyboard" = {
       repeat_delay = "250";
       repeat_rate = "50";
       xkb_layout = "us,ru";
       xkb_options = "grp:alt_space_toggle";
-      accel_profile = "flat";
+    };
+
+    "type:touchpad" = {
+      accel_profile = "adaptive";
+      click_method = "clickfinger";
+      drag = "enabled"; # tap-drag
+      drag_lock = "enabled"; # grace period for tap-drag
+      dwt = "enabled";
+      natural_scroll = "enabled";
+      scroll_method = "two_finger";
+      tap = "disabled";
     };
 
     "5426:98:Razer_Razer_Atheris_-_Mobile_Gaming_Mouse_Keyboard" = {
@@ -18,17 +32,10 @@
     "5426:138:Razer_Razer_Viper_Mini" = {
     };
 
+    # anuramat-f12
+
     # anuramat-ll7
     "1267:12928:ELAN06FA:00_04F3:3280_Touchpad" = {
-      dwt = "enabled";
-      click_method = "clickfinger";
-      natural_scroll = "enabled";
-      # pointer_accel = "-0.05";
-      scroll_method = "two_finger";
-      tap = "disabled";
-      drag = "enabled"; # tap-drag
-      drag_lock = "enabled"; # grace period for tap-drag
-      accel_profile = "adaptive";
     };
 
     "1165:51607:ITE_Tech._Inc._ITE_Device(8258)_Keyboard" = {
