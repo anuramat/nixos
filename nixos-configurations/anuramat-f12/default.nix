697fc793cec19c266acbb5e4cefc327b690b5350
Arsen Nuramatov <x@ctrl.sn>
2025-10-22 19:48:09 +0200

    auto: anuramat-ll7

---
+++ nixos-configurations/anuramat-f12/default.nix
@@ -0,0 +1,40 @@
+{
+  inputs,
+  ...
+}:
+{
+  nix.distributedBuilds = true;
+  system.stateVersion = "25.05";
+  home-manager.users.anuramat.home.stateVersion = "25.05";
+  services.keyd.keyboards.main.ids = [
+  ];
+
+  networking.hostName = "anuramat-f12";
+
+  imports = [
+    inputs.self.nixosModules.default
+    inputs.self.nixosModules.local
+    inputs.self.nixosModules.anuramat
+    inputs.nixos-hardware.nixosModules.framework-12-13th-gen-intel
+    ./hardware-configuration.nix
+  ];
+
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_MIN_PERF_ON_AC = 100;
+  #     CPU_MAX_PERF_ON_AC = 100;
+  #     CPU_MIN_PERF_ON_BAT = 50;
+  #     CPU_MAX_PERF_ON_BAT = 100;
+  #   };
+  # };
+
+  # swapDevices = [
+  #   {
+  #     device = "/var/lib/swapfile";
+  #     size = 32 * 1024;
+  #   }
+  # ];
+
+  # programs.captive-browser.interface = "wlp3s0";
+}
