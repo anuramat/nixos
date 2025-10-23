ab8e3c319c2102d51f114b858398c4347fbc321e
Arsen Nuramatov <x@ctrl.sn>
2025-10-23 15:16:42 +0200

    auto: anuramat-f12

--- home-modules/heavy-linux/desktop/sway/outputs.nix
+++ home-modules/heavy-linux/desktop/sway/outputs.nix
@@ -47,7 +47,7 @@ let
           ];
           text = ''
             monitor-sensor | mawk -W interactive '/Accelerometer orientation changed:/ { print $NF; fflush();}' | while read -r line; do
-              mode=tablet
+              # mode=tablet
               case "$line" in
                 normal)
                   swaymsg output eDP-1 transform 0
@@ -80,7 +80,7 @@ in
         bindswitch --locked lid:off output ${out.int} enable
       '';
     config = {
-      startup = reloadKanshi;
+      startup = reloadKanshi ++ maybeAutorotate;
 
       workspaceOutputAssign =
         let
