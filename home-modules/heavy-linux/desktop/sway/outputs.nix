054288c3e1ab3d5bd67e1826cb6340651e88abd9
Arsen Nuramatov <x@ctrl.sn>
2025-10-23 14:21:58 +0200

    auto: anuramat-f12

--- home-modules/heavy-linux/desktop/sway/outputs.nix
+++ home-modules/heavy-linux/desktop/sway/outputs.nix
@@ -37,12 +37,17 @@ let
       text = ''
         #!/usr/bin/env bash
         monitor-sensor | mawk -W interactive '/Accelerometer orientation changed:/ { print $NF; fflush();}' | while read -r line; do
+          mode=tablet
           case "$line" in
-            normal) swaymsg output eDP-1 transform 0 ;;
+            normal)
+              swaymsg output eDP-1 transform 0
+              mode=laptop
+              ;;
             bottom-up) swaymsg output eDP-1 transform 180 ;;
             right-up) swaymsg output eDP-1 transform 90 ;;
             left-up) swaymsg output eDP-1 transform 270 ;;
           esac
+          # framework_tool --tablet-mode "$mode"
         done
       '';
     }
