974b3bafefe0fdd8fdb7e243a01187400f071d46
Arsen Nuramatov <x@ctrl.sn>
2025-10-23 12:15:06 +0200

    auto: anuramat-f12

--- nixvim-modules/default/lang/typst.nix
+++ nixvim-modules/default/lang/typst.nix
@@ -5,7 +5,8 @@
       enable = true;
       # TODO requires manual setup on first launch; can be solved with the next home-manager release
       settings = {
-        open_cmd = "${lib.getExe pkgs.firefox} -P typst-preview %s";
+        # open_cmd = "${lib.getExe pkgs.firefox} --kiosk -P typst-preview %s";
+        open_cmd = "${lib.getExe pkgs.chromium} --app=%s";
         invert_colors = "auto";
       };
     };
