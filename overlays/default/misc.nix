d5c8faaea2bb57f08ea5955cfb941837cc1139d5
Arsen Nuramatov <x@ctrl.sn>
2025-11-20 06:09:25 +0100

    update codex to 0.60.1

--- overlays/default/misc.nix
+++ overlays/default/misc.nix
@@ -81,10 +81,10 @@ inputs:
 
   codex = prev.stdenv.mkDerivation rec {
     pname = "codex";
-    version = "0.58.0";
+    version = "0.60.1";
     src = prev.fetchurl {
       url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-x86_64-unknown-linux-musl.zst";
-      sha256 = "sha256-p/k9kV7RATaIzrno6icP0I0HgCytIYMrleg2dq5Q/3g=";
+      sha256 = "sha256-MUHtJvcqC/ZusOyw5Hlrlrl1pS0BNtkfR512VfNgtao=";
     };
     dontUnpack = true;
     nativeBuildInputs = [ prev.zstd ];
