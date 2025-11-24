1a2d70c4d2f91e790b1f709f064340e1035a3b0b
Arsen Nuramatov <x@ctrl.sn>
2025-11-24 14:00:52 +0100

    auto: anuramat-f12

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export TRITON_LIBCUDA_PATH=$(nix eval --raw nixpkgs#cudaPackages.cuda_cudart --impure)/lib
-export LIBRARY_PATH=/run/opengl-driver/lib:$(nix eval --raw nixpkgs#cudaPackages.cuda_cudart --impure)/lib
-export C_INCLUDE_PATH=$(nix eval --raw nixpkgs#cudaPackages.cuda_cudart --impure)/include
-export CPLUS_INCLUDE_PATH=$(nix eval --raw nixpkgs#cudaPackages.cuda_cudart --impure)/include
-uv run --with vllm vllm serve "Qwen/Qwen3-Omni-30B-A3B-Instruct" --trust-remote-code --cpu-offload-gb 99999 --max-model-len 8192
