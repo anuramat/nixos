3dc3d9a0f019be14e7947c79050f8a4bf28c62af
Arsen Nuramatov <x@ctrl.sn>
2025-12-02 15:08:44 +0100

    auto: anuramat-f12

--- home-modules/heavy-linux/agents/frontends/gemini.nix
+++ home-modules/heavy-linux/agents/frontends/gemini.nix
@@ -3,14 +3,44 @@
   config,
   ...
 }:
+
+# "general": {
+#   "previewFeatures": true,
+#   "disableAutoUpdate": true,
+#   "sessionRetention": {
+#     "enabled": true
+#   },
+#   "enablePromptCompletion": true
+# },
+# "output": {
+#   "format": "text"
+# },
+# "ui": {
+#   "showStatusInTitle": false,
+#   "hideTips": false,
+#   "hideBanner": true,
+#   "footer": {
+#     "hideCWD": false,
+#     "hideContextPercentage": false
+#   },
+#   "showMemoryUsage": true,
+#   "showCitations": true,
+#   "showModelInfoInChat": true,
+#   "hideWindowTitle": true,
+#   "useAlternateBuffer": true
+# },
+# "tools": {
+#   "shell": {
+#     "showColor": true
+#   },
+#   "autoAccept": true
+# }
+
 let
+  cfg = { };
   inherit (config.lib) agents;
   contextFileName = "AGENTS.md";
-  geminiConfig = config.lib.home.json.set {
-    inherit contextFileName;
-    hideTips = true;
-    hideBanner = true;
-  } (config.home.homeDirectory + "/.gemini/settings.json");
+  cfgFile = config.lib.home.json.set cfg (config.home.homeDirectory + "/.gemini/settings.json");
   pkg = config.lib.agents.mkPackages {
     package = pkgs.gemini-cli;
     args = [ "--yolo" ];
@@ -23,7 +53,7 @@ in
 {
   home = {
     activation = {
-      inherit geminiConfig;
+      geminiConfig = cfgFile;
     };
     file.".gemini/${contextFileName}".text = agents.instructions.generic;
     packages = [ pkg ];
