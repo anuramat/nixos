befa81e2c0720b0a8e5b4ee7d89261642a2784f6
Arsen Nuramatov <x@ctrl.sn>
2025-12-20 22:56:54 +0100

    completion for template script

--- home-modules/default/bash/template.nix
+++ home-modules/default/bash/template.nix
@@ -1,15 +1,44 @@
 {
   pkgs,
+  config,
   ...
 }:
+
 let
-  template = pkgs.writeShellApplication {
+
+  root = "${config.home.homeDirectory}/notes/templates";
+
+  completion =
+    pkgs.writeText "template-completion.sh"
+      # bash
+      ''
+        _template() {
+          local cur root
+          root="${root}"
+
+          # only first argument and if root exists
+          if ((COMP_CWORD != 1)) || [ ! -d "$root" ]; then
+            COMPREPLY=()
+            return 0
+          fi
+
+          local -a candidates
+          mapfile -d "" -t candidates < <(
+            find "$root" -maxdepth 1 -mindepth 1 -type d -printf '%f\0' 2>/dev/null
+          )
+
+          COMPREPLY=($(compgen -W "''${candidates[*]}" -- "''${COMP_WORDS[COMP_CWORD]}"))
+        }
+        complete -F _template template
+      '';
+
+  binpkg = pkgs.writeShellApplication {
     name = "template";
     text = # bash
       ''
         set +u
         main() (
-          local root="$HOME/notes/templates"
+          local root="${root}"
           if [ -z "$1" ]; then
             ls "$root"
             return
@@ -25,10 +54,16 @@ let
       '';
   };
 
+  pkg = binpkg.overrideAttrs (old: {
+    buildCommand =
+      old.buildCommand
+      + "install -Dm644 ${completion} $out/share/bash-completion/completions/template.bash";
+  });
+
 in
 
 {
   home.packages = [
-    template
+    pkg
   ];
 }
