86589c1fb7fb84b4eac57d8edda9fdaf60efbb1d
Arsen Nuramatov <x@ctrl.sn>
2026-02-20 16:15:47 +0100

    ...

--- home-modules/heavy-linux/desktop/niri/keys.nix
+++ home-modules/heavy-linux/desktop/niri/keys.nix
@@ -124,6 +124,9 @@ in
       repeat = false;
     };
     "Mod+semicolon".action.spawn = "foot";
+    "Mod+Shift+Slash".action.show-hotkey-overlay = { };
+    "Mod+Space".action.spawn = pickers.drun;
+    "Mod+Ctrl+Q".action.spawn = ctl.lock;
 
     "Mod+H".action."focus-column-left" = { };
     "Mod+L".action."focus-column-right" = { };
@@ -134,6 +137,39 @@ in
     "Mod+Ctrl+L".action."move-column-right" = { };
     "Mod+Ctrl+J".action."move-window-down" = { };
     "Mod+Ctrl+K".action."move-window-up" = { };
+
+    "Mod+Shift+H".action."focus-monitor-left" = { };
+    "Mod+Shift+L".action."focus-monitor-right" = { };
+    "Mod+Shift+J".action."focus-monitor-down" = { };
+    "Mod+Shift+K".action."focus-monitor-up" = { };
+
+    # Keep in mind that niri is a dynamic workspace system, so these commands
+    # are kind of "best effort". Trying to refer to a workspace index bigger
+    # than the current workspace count will instead refer to the bottommost
+    # (empty) workspace.
+    #
+    # For example, with 2 workspaces + 1 empty, indices 3, 4, 5 and so on will
+    # all refer to the 3rd workspace.
+
+    "Mod+1".action."focus-workspace" = 1;
+    "Mod+2".action."focus-workspace" = 2;
+    "Mod+3".action."focus-workspace" = 3;
+    "Mod+4".action."focus-workspace" = 4;
+    "Mod+5".action."focus-workspace" = 5;
+    "Mod+6".action."focus-workspace" = 6;
+    "Mod+7".action."focus-workspace" = 7;
+    "Mod+8".action."focus-workspace" = 8;
+    "Mod+9".action."focus-workspace" = 9;
+
+    "Mod+Ctrl+1".action."move-column-to-workspace" = 1;
+    "Mod+Ctrl+2".action."move-column-to-workspace" = 2;
+    "Mod+Ctrl+3".action."move-column-to-workspace" = 3;
+    "Mod+Ctrl+4".action."move-column-to-workspace" = 4;
+    "Mod+Ctrl+5".action."move-column-to-workspace" = 5;
+    "Mod+Ctrl+6".action."move-column-to-workspace" = 6;
+    "Mod+Ctrl+7".action."move-column-to-workspace" = 7;
+    "Mod+Ctrl+8".action."move-column-to-workspace" = 8;
+    "Mod+Ctrl+9".action."move-column-to-workspace" = 9;
 
     "XF86MonBrightnessDown" = mkCtl ctl.brightness.down;
     "XF86MonBrightnessUp" = mkCtl ctl.brightness.up;
