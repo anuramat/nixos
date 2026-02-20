cc6c63dc4cddbbeda69eb5bab809c8fce3bcada8
Arsen Nuramatov <x@ctrl.sn>
2026-02-20 17:50:22 +0100

    ...

--- home-modules/heavy-linux/desktop/niri/keys.nix
+++ home-modules/heavy-linux/desktop/niri/keys.nix
@@ -168,33 +168,32 @@ in
 
     "Mod+Minus".action."set-column-width" = "-10%";
     "Mod+Equal".action."set-column-width" = "+10%";
-    "Mod+Shift+Minus".action."set-window-height" = "-10%";
-    "Mod+Shift+Equal".action."set-window-height" = "+10%";
+    "Mod+Ctrl+Minus".action."set-window-height" = "-10%";
+    "Mod+Ctrl+Equal".action."set-window-height" = "+10%";
 
+    # TODO
     # focus-column-first
     # focus-column-last
-    "Mod+H".action."focus-column-left" = { };
-    "Mod+L".action."focus-column-right" = { };
-    # focus-window-down
-    # focus-workspace-down
-    "Mod+J".action."focus-window-or-workspace-down" = { };
-    # focus-window-up
-    # focus-workspace-up
-    "Mod+K".action."focus-window-or-workspace-up" = { };
-
-    "Mod+Alt+H".action."consume-or-expel-window-left" = { };
-    "Mod+Alt+L".action."consume-or-expel-window-right" = { };
-
     # move-column-to-first
     # move-column-to-last
-    "Mod+Ctrl+H".action."move-column-left" = { };
-    "Mod+Ctrl+L".action."move-column-right" = { };
-    # move-window-down
-    # move-window-to-workspace-down
+
+    "Mod+H".action."focus-column-left" = { };
+    "Mod+Ctrl+H".action."consume-or-expel-window-left" = { };
+    "Mod+Shift+H".action."move-column-left" = { };
+
+    "Mod+L".action."focus-column-right" = { };
+    "Mod+Ctrl+L".action."consume-or-expel-window-right" = { };
+    "Mod+Shift+L".action."move-column-right" = { };
+
+    "Mod+J".action."focus-window-or-workspace-down" = { };
     "Mod+Ctrl+J".action."move-window-down-or-to-workspace-down" = { };
-    # move-window-up
-    # move-window-to-workspace-up
+    "Mod+Shift+J".action."move-column-to-workspace-down" = { };
+    "Mod+Alt+J".action."move-workspace-down" = { };
+
+    "Mod+K".action."focus-window-or-workspace-up" = { };
     "Mod+Ctrl+K".action."move-window-up-or-to-workspace-up" = { };
+    "Mod+Shift+K".action."move-column-to-workspace-up" = { };
+    "Mod+Alt+K".action."move-workspace-up" = { };
 
     "Mod+Ctrl+M".action."toggle-window-floating" = { };
     "Mod+M".action."switch-focus-between-floating-and-tiling" = { };
@@ -202,19 +201,12 @@ in
 
     # XXX --------------------------------------------------------------------------------------------
 
-    # move-column-to-workspace-down
-    # move-column-to-workspace-up
-    # move-workspace-down
-    # move-workspace-up
-
-    # move-window-to-workspace 1
-
     "Mod+Comma".action."consume-window-into-column" = { };
     "Mod+Period".action."expel-window-from-column" = { };
 
     "Mod+R".action."switch-preset-column-width" = { };
-    "Mod+Shift+R".action."switch-preset-window-height" = { };
-    "Mod+Ctrl+R".action."reset-window-height" = { };
+    "Mod+Ctrl+R".action."switch-preset-window-height" = { };
+    "Mod+Shift+R".action."reset-window-height" = { };
 
     "Mod+F".action."fullscreen-window" = { };
     "Mod+Ctrl+F".action."expand-column-to-available-width" = { };
@@ -235,15 +227,25 @@ in
     "Mod+8".action."focus-workspace" = 8;
     "Mod+9".action."focus-workspace" = 9;
 
-    "Mod+Ctrl+1".action."move-column-to-workspace" = 1;
-    "Mod+Ctrl+2".action."move-column-to-workspace" = 2;
-    "Mod+Ctrl+3".action."move-column-to-workspace" = 3;
-    "Mod+Ctrl+4".action."move-column-to-workspace" = 4;
-    "Mod+Ctrl+5".action."move-column-to-workspace" = 5;
-    "Mod+Ctrl+6".action."move-column-to-workspace" = 6;
-    "Mod+Ctrl+7".action."move-column-to-workspace" = 7;
-    "Mod+Ctrl+8".action."move-column-to-workspace" = 8;
-    "Mod+Ctrl+9".action."move-column-to-workspace" = 9;
+    "Mod+Ctrl+1".action."move-window-to-workspace" = 1;
+    "Mod+Ctrl+2".action."move-window-to-workspace" = 2;
+    "Mod+Ctrl+3".action."move-window-to-workspace" = 3;
+    "Mod+Ctrl+4".action."move-window-to-workspace" = 4;
+    "Mod+Ctrl+5".action."move-window-to-workspace" = 5;
+    "Mod+Ctrl+6".action."move-window-to-workspace" = 6;
+    "Mod+Ctrl+7".action."move-window-to-workspace" = 7;
+    "Mod+Ctrl+8".action."move-window-to-workspace" = 8;
+    "Mod+Ctrl+9".action."move-window-to-workspace" = 9;
+
+    "Mod+Shift+1".action."move-column-to-workspace" = 1;
+    "Mod+Shift+2".action."move-column-to-workspace" = 2;
+    "Mod+Shift+3".action."move-column-to-workspace" = 3;
+    "Mod+Shift+4".action."move-column-to-workspace" = 4;
+    "Mod+Shift+5".action."move-column-to-workspace" = 5;
+    "Mod+Shift+6".action."move-column-to-workspace" = 6;
+    "Mod+Shift+7".action."move-column-to-workspace" = 7;
+    "Mod+Shift+8".action."move-column-to-workspace" = 8;
+    "Mod+Shift+9".action."move-column-to-workspace" = 9;
 
     "XF86MonBrightnessDown" = mkCtl ctl.brightness.down;
     "XF86MonBrightnessUp" = mkCtl ctl.brightness.up;
