e18093e2f0a27a0b155104ea5599d948c49cc0da
Arsen Nuramatov <x@ctrl.sn>
2026-02-21 20:57:28 +0100

    ...

--- home-modules/heavy-linux/desktop/niri/default.nix
+++ home-modules/heavy-linux/desktop/niri/default.nix
@@ -21,7 +21,10 @@
     package = pkgs.niri;
     settings = {
       screenshot-path = "${config.home.sessionVariables.XDG_PICTURES_DIR}/screen/shot_%F_%T.png";
-      xwayland-satellite.enable = true;
+      xwayland-satellite = {
+        path = lib.getExe pkgs.xwayland-satellite;
+        enable = true;
+      };
       prefer-no-csd = true;
       hotkey-overlay.skip-at-startup = true;
 
