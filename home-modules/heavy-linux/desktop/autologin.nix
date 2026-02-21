64b3bf0aecead09a6f93761aa3c2e7c75be1f201
Arsen Nuramatov <x@ctrl.sn>
2026-02-21 16:20:30 +0100

    wip

--- home-modules/heavy-linux/desktop/autologin.nix
+++ home-modules/heavy-linux/desktop/autologin.nix
@@ -1,5 +1,21 @@
+{ pkgs, ... }:
 let
-  wm = "niri-session";
+  niri = pkgs.writeShellScript "niri" ''
+    if systemctl --user -q is-active niri.service; then
+      echo 'A niri session is already running.'
+      exit 1
+    fi
+
+    systemctl --user reset-failed
+    # systemctl --user import-environment
+    dbus-update-activation-environment --all --systemd
+    systemctl --user unset-environment SHLVL
+    systemctl --user --wait start niri.service
+    systemctl --user start --job-mode=replace-irreversibly niri-shutdown.target
+    systemctl --user unset-environment WAYLAND_DISPLAY DISPLAY XDG_SESSION_TYPE XDG_CURRENT_DESKTOP NIRI_SOCKET
+  '';
+
+  wm = niri;
 in
 {
   # auto start on 1st tty
@@ -8,16 +24,12 @@ in
       # bash
       ''
         	if [[ -z $WAYLAND_DISPLAY ]] \
-        	  && [[ -z $WM_STARTED ]] \
         	  && [[ $XDG_VTNR == "1" ]] \
         	  && command -v ${wm} >/dev/null 2>&1; then
-        	  export WM_STARTED
-        	  WM_STARTED=1
         	  if [[ -v WLR_DRM_DEVICES ]]; then
         	    export WLR_DRM_DEVICES=$(realpath "$WLR_DRM_DEVICES")
         	  fi
         	  exec ${wm}
-        	  # TODO try sourcing instead
         	fi
       '';
   };
