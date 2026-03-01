488b137fe9973d65ac91e609553701db27309aae
Arsen Nuramatov <x@ctrl.sn>
2026-03-01 06:54:51 +0100

    ...

--- nixos-configurations/anuramat-bgm5/default.nix
+++ nixos-configurations/anuramat-bgm5/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   inputs,
   ...
 }:
@@ -8,12 +9,24 @@
   home-manager.users.anuramat.home.stateVersion = "25.11";
   networking.hostName = "anuramat-bgm5";
 
+  nixpkgs.config.rocmSupport = true;
+
   imports = [
     inputs.self.nixosModules.default
     inputs.self.nixosModules.local
     inputs.self.nixosModules.anuramat
     ./hardware-configuration.nix
+
+    # originally for framework desktop
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+
   ];
+
+  # recommended in nixos-hardware readme for framework desktop
+  boot.kernelPackages = pkgs.linuxPackages_6_19;
 
   programs.captive-browser.interface = "wlp195s0";
 }
