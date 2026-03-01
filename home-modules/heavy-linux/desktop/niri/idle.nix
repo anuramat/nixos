35eea522d4cff29ef90391b32a850819f3668801
Arsen Nuramatov <x@ctrl.sn>
2026-03-01 06:09:01 +0100

    wip

---
+++ home-modules/heavy-linux/desktop/niri/idle.nix
@@ -0,0 +1,114 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) getExe;
+in
+{
+  screen =
+    let
+      niri = getExe pkgs.niri;
+    in
+    {
+      off = "${niri} msg action power-off-monitors";
+      on = "${niri} msg action power-on-monitors";
+    };
+  lock =
+    let
+      screen = "${lib.getExe pkgs.swaylock} -f";
+      keyring = getExe config.lib.keyring.lock;
+    in
+    "${keyring}; ${screen}";
+
+  services = {
+    swayidle = {
+      enable = true;
+      # idlehint = 300; # TODO implement/beg
+      # BUG duplicate events overwrite previous definitions: <https://github.com/nix-community/home-manager/issues/4432>
+      timeouts = [
+        {
+          timeout = 300;
+          command = lock;
+        }
+        {
+          timeout = 600;
+          command = screen.off;
+          resumeCommand = screen.on;
+        }
+      ];
+      events = [
+        {
+          event = "before-sleep";
+          command = lock;
+        }
+        {
+          event = "lock";
+          command = lock;
+        }
+      ];
+    };
+  };
+  home.packages = [
+    erotic
+    unerotic
+  ];
+
+  # TODO put env stuff in a reusable var
+  # TODO put all deps in runtimeInputs
+  # TODO make a single script with args instead of two separate ones
+  # TODO turn off keyboard/mouse lights
+  erotic = pkgs.writeShellApplication {
+    name = "erotic";
+    bashOptions = [
+      "nounset"
+      "pipefail"
+    ];
+    runtimeInputs = [
+      pkgs.procps
+      pkgs.swaylock
+    ];
+    # TODO auto disown
+    text = ''
+      SWAYSOCK=$(systemctl --user show-environment | sed -n 's/^SWAYSOCK=//p')
+      export SWAYSOCK
+
+      # unlock
+      pkill -SIGUSR1 swaylock
+      # stop idle service
+      systemctl --user --machine="$USER@.host" stop swayidle.service
+      # enable screen
+      ${screen.on}
+      # set brightness
+      swaymsg 'exec "${lib.getExe pkgs.brightnessctl} s 100%"'
+      ddcutil setvcp 10 100 --display 1
+      # lock with the chosen color; restart idle on unlock
+      sh -c "WAYLAND_DISPLAY=wayland-1 swaylock --color $1; systemctl --user --machine=$USER@.host start swayidle.service" &>/dev/null &
+    '';
+  };
+
+  unerotic = pkgs.writeShellApplication {
+    name = "unerotic";
+    bashOptions = [
+      "nounset"
+      "pipefail"
+    ];
+    runtimeInputs = [
+      pkgs.procps
+      pkgs.swaylock
+    ];
+    text = ''
+      SWAYSOCK=$(systemctl --user show-environment | sed -n 's/^SWAYSOCK=//p')
+      export SWAYSOCK
+      WAYLAND_DISPLAY=wayland-1
+      export WAYLAND_DISPLAY
+
+      systemctl --user --machine="$USER@.host" start swayidle.service
+      pkill -SIGUSR1 swaylock
+      swaylock --daemonize
+      pkill -SIGUSR1 swayidle
+    '';
+  };
+}
