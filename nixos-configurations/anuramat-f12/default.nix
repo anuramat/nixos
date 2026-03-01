4ee2505ab3354def0ad7df5e067b763f18847af2
Arsen Nuramatov <x@ctrl.sn>
2026-03-01 03:18:22 +0100

    ......

--- nixos-configurations/anuramat-f12/default.nix
+++ nixos-configurations/anuramat-f12/default.nix
@@ -3,17 +3,16 @@
   ...
 }:
 {
+  system.stateVersion = "25.05";
+  home-manager.users.anuramat.home.stateVersion = "25.05";
 
   # swap partition
   boot.initrd.luks.devices."luks-ffc8e21f-2272-442f-8258-30742e29e1f0".device =
     "/dev/disk/by-uuid/ffc8e21f-2272-442f-8258-30742e29e1f0";
 
   nix.distributedBuilds = true;
-  system.stateVersion = "25.05";
-  home-manager.users.anuramat.home.stateVersion = "25.05";
-  services.keyd.keyboards.main.ids = [
-    "0001:0001:70533846"
-  ];
+
+  services.keyd.keyboards.main.ids = [ "0001:0001:70533846" ];
 
   networking.hostName = "anuramat-f12";
 
@@ -24,23 +23,6 @@
     inputs.nixos-hardware.nixosModules.framework-12-13th-gen-intel
     ./hardware-configuration.nix
   ];
-
-  # services.tlp = {
-  #   enable = true;
-  #   settings = {
-  #     CPU_MIN_PERF_ON_AC = 100;
-  #     CPU_MAX_PERF_ON_AC = 100;
-  #     CPU_MIN_PERF_ON_BAT = 50;
-  #     CPU_MAX_PERF_ON_BAT = 100;
-  #   };
-  # };
-
-  # swapDevices = [
-  #   {
-  #     device = "/var/lib/swapfile";
-  #     size = 32 * 1024;
-  #   }
-  # ];
 
   programs.captive-browser.interface = "wlp0s20f3";
 }
