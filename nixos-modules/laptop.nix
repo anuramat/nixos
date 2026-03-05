1c3c3c8b402adf43f4dbcd36b99dd775b84a5b80
Arsen Nuramatov <x@ctrl.sn>
2026-03-05 18:42:46 +0100

    ...

--- nixos-modules/laptop.nix
+++ nixos-modules/laptop.nix
@@ -6,26 +6,28 @@ let
   inherit (lib) mkDefault;
 in
 {
-  thermald.enable = true; # cooling
-  tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  services = {
+    thermald.enable = true; # cooling
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-      CPU_MIN_PERF_ON_AC = mkDefault 50;
-      CPU_MAX_PERF_ON_AC = mkDefault 100;
-      CPU_MIN_PERF_ON_BAT = mkDefault 0;
-      CPU_MAX_PERF_ON_BAT = mkDefault 30;
+        CPU_MIN_PERF_ON_AC = mkDefault 50;
+        CPU_MAX_PERF_ON_AC = mkDefault 100;
+        CPU_MIN_PERF_ON_BAT = mkDefault 0;
+        CPU_MAX_PERF_ON_BAT = mkDefault 30;
+      };
     };
-  };
-  upower = {
-    enable = true; # sleep on low battery
-    usePercentageForPolicy = true;
-    percentageCritical = 10;
-    criticalPowerAction = "Hibernate";
+    upower = {
+      enable = true; # sleep on low battery
+      usePercentageForPolicy = true;
+      percentageCritical = 10;
+      criticalPowerAction = "Hibernate";
+    };
   };
 }
