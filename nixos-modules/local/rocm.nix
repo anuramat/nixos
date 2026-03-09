db65edc058e328808ec5990ec3a2013787139443
Arsen Nuramatov <x@ctrl.sn>
2026-03-09 23:42:29 +0100

    ...

--- nixos-modules/local/rocm.nix
+++ nixos-modules/local/rocm.nix
@@ -8,7 +8,6 @@ lib.mkIf (config.nixpkgs.config.rocmSupport or false) {
   environment.systemPackages = with pkgs; [
     rocmPackages.rocm-smi
     libdrm
-    nvtopPackages.amd
   ];
 
   hardware.amdgpu.opencl.enable = true;
