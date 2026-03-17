ee96e81f692fd0fcdf19175d1b7a349dc6f667d0
Arsen Nuramatov <x@ctrl.sn>
2026-03-17 04:16:23 +0100

    ...

--- nixvim-modules/default/lang/rust.nix
+++ nixvim-modules/default/lang/rust.nix
@@ -1,9 +1,6 @@
 {
   plugins = {
     lint.lintersByFt.rust = [ "clippy" ];
-    conform-nvim.settings.formatters_by_ft.rust = [
-      "rustfmt"
-    ];
     rustaceanvim = {
       enable = true;
     };
