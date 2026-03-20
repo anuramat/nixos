c25f974bccc31c67b9db929f1a1c24ab4e3d22c0
Arsen Nuramatov <x@ctrl.sn>
2026-03-20 14:52:56 +0100

    wip

--- home-modules/heavy-linux/desktop/autologin.nix
+++ home-modules/heavy-linux/desktop/autologin.nix
@@ -8,10 +8,11 @@ let
 
     systemctl --user reset-failed
     # systemctl --user import-environment ...
+    # TODO create a new nix var with env vars that we put in the env
     dbus-update-activation-environment --systemd XDG_VTNR XDG_SESSION_TYPE XDG_SESSION_ID BEMENU_OPTS
-    # GNUPG_HOME
     systemctl --user --wait start niri.service
     systemctl --user start --job-mode=replace-irreversibly niri-shutdown.target
+    # TODO these as a nix var as well
     systemctl --user unset-environment WAYLAND_DISPLAY DISPLAY XDG_SESSION_TYPE XDG_CURRENT_DESKTOP NIRI_SOCKET
   '';
 
@@ -26,9 +27,6 @@ in
         	if [[ -z $WAYLAND_DISPLAY ]] \
         	  && [[ $XDG_VTNR == "1" ]] \
         	  && command -v ${wm} >/dev/null 2>&1; then
-        	  if [[ -v WLR_DRM_DEVICES ]]; then
-        	    export WLR_DRM_DEVICES=$(realpath "$WLR_DRM_DEVICES")
-        	  fi
         	  exec ${wm}
         	fi
       '';
