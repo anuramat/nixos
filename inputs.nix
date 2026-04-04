49b1287750d482644f9f6176e5747febb9a47875
Arsen Nuramatov <x@ctrl.sn>
2026-04-04 06:57:02 +0200

    wip

--- inputs.nix
+++ inputs.nix
@@ -41,7 +41,7 @@ in
   figtree = mkInput "github:anuramat/figtree.nvim";
   subcat = mkInput "github:anuramat/subcat";
   todo = mkInput "github:anuramat/todo";
-  vicode = mkInput "github:anuramat/vicode/dev";
+  vicode.url = "github:anuramat/vicode/dev";
 
   # forks
   mods.url = "github:anuramat/mods/dev";
