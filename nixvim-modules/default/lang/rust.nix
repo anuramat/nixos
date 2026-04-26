9640c2f12c6897d1ed864bd1b35d3b5d1ed617fd
Arsen Nuramatov <x@ctrl.sn>
2026-04-26 02:08:34 +0200

    WIP

--- nixvim-modules/default/lang/rust.nix
+++ nixvim-modules/default/lang/rust.nix
@@ -1,8 +1,17 @@
 {
   plugins = {
     # NOTE clippy and cargofmt already in somehow
+    crates = {
+      enable = true;
+    };
     rustaceanvim = {
       enable = true;
+      settings.server.default_settings = {
+        references = {
+          excludeTests = true;
+          excludeImports = true;
+        };
+      };
     };
   };
 }
