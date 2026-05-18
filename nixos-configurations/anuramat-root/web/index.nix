f5f7266aaa32cb4d7f26603ef9739a5e9e042a40
Arsen Nuramatov <x@ctrl.sn>
2026-05-18 09:35:45 +0200

    refactor(hax): update callsites from hax/web to config.web

--- nixos-configurations/anuramat-root/web/index.nix
+++ nixos-configurations/anuramat-root/web/index.nix
@@ -1,17 +1,17 @@
 {
   pkgs,
-  lib,
   inputs,
-  hax,
   ...
 }:
-lib.mkMerge (
-  hax.web.serveBinary rec {
-    noRobots = true;
-    domain = "ctrl.sn";
-    root = domain;
-    cwd = "/var/www/";
-    port = "8080";
-    binary = "${inputs.ctrlsn.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/ctrl.sn";
-  }
-)
+{
+  web.sites = [
+    {
+      noRobots = true;
+      domain = "ctrl.sn";
+      root = "ctrl.sn";
+      cwd = "/var/www/";
+      port = "8080";
+      binary = "${inputs.ctrlsn.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/ctrl.sn";
+    }
+  ];
+}
--- nixos-configurations/anuramat-root/web/pastebin.nix
+++ nixos-configurations/anuramat-root/web/pastebin.nix
@@ -1,4 +1,4 @@
-{ hax, lib, ... }:
+{ ... }:
 let
   w = {
     root = "ctrl.sn";
@@ -7,18 +7,14 @@ let
     noRobots = true;
   };
 in
-lib.mkMerge (
-  (hax.web.serve w)
-  ++ [
-    {
-      services.wastebin = {
-        enable = true;
-        settings = {
-          WASTEBIN_BASE_URL = "https://${w.domain}";
-          WASTEBIN_ADDRESS_PORT = "127.0.0.1:${w.port}";
-          WASTEBIN_MAX_BODY_SIZE = 1024 * 1024;
-        };
-      };
-    }
-  ]
-)
+{
+  web.sites = [ w ];
+  services.wastebin = {
+    enable = true;
+    settings = {
+      WASTEBIN_BASE_URL = "https://${w.domain}";
+      WASTEBIN_ADDRESS_PORT = "127.0.0.1:${w.port}";
+      WASTEBIN_MAX_BODY_SIZE = 1024 * 1024;
+    };
+  };
+}
