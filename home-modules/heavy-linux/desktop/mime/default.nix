980391a5e336d3aff0f90d4fb9de81825a810320
Arsen Nuramatov <x@ctrl.sn>
2026-05-18 09:38:21 +0200

    mime: inline helpers into consumer, drop hax/mime.nix

--- home-modules/heavy-linux/desktop/mime/default.nix
+++ home-modules/heavy-linux/desktop/mime/default.nix
@@ -1,10 +1,97 @@
 {
-  hax,
+  lib,
   pkgs,
   ...
 }:
 let
-  inherit (hax.mime) setMany generateMimeTypes mimeFromDesktop;
+  inherit (builtins)
+    readFile
+    readDir
+    attrNames
+    concatLists
+    listToAttrs
+    mapAttrs
+    typeOf
+    toJSON
+    ;
+  inherit (lib)
+    splitString
+    filter
+    filterAttrs
+    strings
+    attrsets
+    unique
+    ;
+
+  readLines = v: v |> readFile |> splitString "\n" |> filter (x: x != "");
+  getSchema = attrsets.mapAttrsRecursive (_path: typeOf);
+  getMatches =
+    patterns: x:
+    mapAttrs (
+      _name: schema: if typeOf x == "set" then schema == getSchema x else schema == typeOf x
+    ) patterns;
+
+  mimeFromDesktop =
+    package:
+    let
+      dir = package.outPath + "/share/applications";
+      desktopFiles =
+        dir
+        |> readDir
+        |> filterAttrs (n: _: strings.hasSuffix ".desktop" n)
+        |> attrNames
+        |> map (v: dir + "/" + v);
+      lineToMimes =
+        line: line |> strings.removePrefix "MimeType=" |> splitString ";" |> filter (v: v != "");
+    in
+    desktopFiles
+    |> map readLines
+    |> concatLists
+    |> filter (strings.hasPrefix "MimeType")
+    |> map lineToMimes
+    |> concatLists
+    |> unique;
+
+  # one app, many types
+  setMany =
+    app: types:
+    types
+    |> map (v: {
+      name = v;
+      value = app;
+    })
+    |> listToAttrs;
+
+  mimePatterns = {
+    parts = {
+      prefix = "string";
+      suffixes = "list";
+    };
+    exact = "string";
+    exactList = "list";
+    file = "path";
+  };
+  # thingie to actual mime type list
+  generateMimeTypes =
+    things:
+    things
+    |> map (
+      v:
+      let
+        matches = getMatches mimePatterns v;
+      in
+      if matches.parts then
+        map (suffix: "${v.prefix}/${suffix}") v.suffixes
+      else if matches.exact then
+        [ v ]
+      else if matches.exactList then
+        v
+      else if matches.file then
+        v |> readLines
+      else
+        throw "illegal pattern: ${toJSON v}"
+    )
+    |> concatLists;
 
   applications = {
     browser = "firefox.desktop";
