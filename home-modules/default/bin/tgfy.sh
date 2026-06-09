d2cff8fc10bafab8e97f0d427db2fd89ce45d06b
Arsen Nuramatov <x@ctrl.sn>
2026-06-09 20:38:34 +0200

    WIP

--- home-modules/default/bin/tgfy.sh
+++ home-modules/default/bin/tgfy.sh
@@ -1,17 +1,36 @@
 #!/usr/bin/env bash
 
-# text on stdin, files in args
+usage() {
+	cat <<'EOF'
+Usage:
+  tgfy.sh [-h|--help]
+  command | tgfy.sh
+  command | tgfy.sh FILE...
+EOF
+}
+
+case "${1-}" in
+	-h | --help)
+		usage
+		exit
+		;;
+	*) ;;
+esac
+
+TEXT=$(cat)
 
 CHAT_ID=$(cat /run/agenix/tgfy-id)
 BOT_TOKEN=$(cat /run/agenix/tgfy-token)
+API_URL="https://api.telegram.org/bot$BOT_TOKEN"
 
-# echo "Run finished — FID 4.2" | curl -s \
-# 	-d chat_id="$CHAT_ID" \
-# 	--data-urlencode "text@-" \
-# 	"https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
+curl -s \
+	-d chat_id="$CHAT_ID" \
+	--data-urlencode "text=$TEXT" \
+	"$API_URL/sendMessage"
 
-# echo "Run finished — FID 4.2" | curl -s \
-# 	-F chat_id="$CHAT_ID" \
-# 	-F document=@"$1" \
-# 	-F "caption=<-" \
-# 	"https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+for file; do
+	curl -s \
+		-F chat_id="$CHAT_ID" \
+		-F document=@"$file" \
+		"$API_URL/sendDocument"
+done
