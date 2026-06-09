8afaaf91f47ce77fb723c0a7302cbeadfc083777
Arsen Nuramatov <x@ctrl.sn>
2026-06-09 20:30:25 +0200

    WIP

--- home-modules/default/bin/tgfy.sh
+++ home-modules/default/bin/tgfy.sh
@@ -5,13 +5,13 @@
 CHAT_ID=$(cat /run/agenix/tgfy-id)
 BOT_TOKEN=$(cat /run/agenix/tgfy-token)
 
-echo "Run finished — FID 4.2" | curl -s \
-	-d chat_id="$CHAT_ID" \
-	--data-urlencode "text@-" \
-	"https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
+# echo "Run finished — FID 4.2" | curl -s \
+# 	-d chat_id="$CHAT_ID" \
+# 	--data-urlencode "text@-" \
+# 	"https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
 
-echo "Run finished — FID 4.2" | curl -s \
-	-F chat_id="$CHAT_ID" \
-	-F document=@"$1" \
-	-F "caption=<-" \
-	"https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+# echo "Run finished — FID 4.2" | curl -s \
+# 	-F chat_id="$CHAT_ID" \
+# 	-F document=@"$1" \
+# 	-F "caption=<-" \
+# 	"https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
