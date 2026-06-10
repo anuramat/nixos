696e1a8164fa2c902d31423165df32aa249fddd3
Arsen Nuramatov <x@ctrl.sn>
2026-06-10 23:25:54 +0200

    hm/linux: gate desktop-only bits behind gui

--- home-modules/linux.nix
+++ home-modules/linux.nix
@@ -1,62 +1,77 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  home.packages =
+    with pkgs;
+    [
 
-    zenith-nvidia # top WITH nvidia GPUs
-    nvitop # nvidia gpu
+      zenith-nvidia # top WITH nvidia GPUs
+      nvitop # nvidia gpu
 
-    bubblewrap # sandboxing
-    fuse-overlayfs
+      bubblewrap # sandboxing
+      fuse-overlayfs
 
-    parted
-    geteduroam-cli
-    wine
-    distrobox
-    wayidle # runs a command on idle (one-off, thus orthogonal to swayidle)
-    subcat
-    trashy # `trash`
+      parted
+      geteduroam-cli
+      distrobox
+      subcat
+      trashy # `trash`
 
-    # hardware
-    acpi # battery status etc
-    dmidecode # read hw info from bios using smbios/dmi
-    efibootmgr # EFI boot manager editor
-    hwinfo
-    libva-utils # vainfo - info on va-api
-    lm_sensors
-    lshw # hw info
-    nvme-cli
-    smem # ram usage
-    v4l-utils # camera stuff
-    wirelesstools # iwconfig etc
+      # hardware
+      acpi # battery status etc
+      dmidecode # read hw info from bios using smbios/dmi
+      efibootmgr # EFI boot manager editor
+      hwinfo
+      libva-utils # vainfo - info on va-api
+      lm_sensors
+      lshw # hw info
+      nvme-cli
+      smem # ram usage
+      v4l-utils # camera stuff
+      wirelesstools # iwconfig etc
 
-    seahorse
-  ];
+    ]
+    ++ lib.optionals config.gui [
+      seahorse
+      wayidle # runs a command on idle (one-off, thus orthogonal to swayidle)
+      wine
+    ];
 
   services.pss.enable = true; # secret service api -- exposes password-store over dbus
-  programs.wayprompt.enable = true;
+  programs.wayprompt.enable = config.gui;
   services.gpg-agent = {
     pinentry =
-      let
-        name = "pinentry-auto";
-        package = pkgs.writeShellApplication {
-          inherit name;
-          runtimeInputs = [
-            pkgs.wayprompt
-            pkgs.pinentry-tty
-          ];
-          text = ''
-            if [ -v DISPLAY ]; then
-              exec ${pkgs.wayprompt}/bin/pinentry-wayprompt "$@"
-            else
-              exec ${pkgs.pinentry-tty}/bin/pinentry-tty "$@"
-            fi
-          '';
+      if config.gui then
+        let
+          name = "pinentry-auto";
+          package = pkgs.writeShellApplication {
+            inherit name;
+            runtimeInputs = [
+              pkgs.wayprompt
+              pkgs.pinentry-tty
+            ];
+            # DISPLAY check so that it still works over ssh
+            text = ''
+              if [ -v DISPLAY ]; then
+                exec ${pkgs.wayprompt}/bin/pinentry-wayprompt "$@"
+              else
+                exec ${pkgs.pinentry-tty}/bin/pinentry-tty "$@"
+              fi
+            '';
+          };
+        in
+        {
+          inherit package;
+          program = name;
+        }
+      else
+        {
+          package = pkgs.pinentry-tty;
+          program = "pinentry-tty";
         };
-      in
-      {
-        inherit package;
-        program = name;
-      };
   };
-
 }
