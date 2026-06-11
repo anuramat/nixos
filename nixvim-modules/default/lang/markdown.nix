9e020418d39ce40346f061213e565aa9c137b00f
Arsen Nuramatov <x@ctrl.sn>
2026-06-11 02:50:42 +0200

    nixvim: drop disabled mdformat config

--- nixvim-modules/default/lang/markdown.nix
+++ nixvim-modules/default/lang/markdown.nix
@@ -8,19 +8,5 @@
     # runtime ftplugin maps gO to a TOC picker
     extraConfigLua = ''vim.keymap.del("n", "gO", { buffer = 0 })'';
   };
-  plugins = {
-    lsp.servers.marksman.enable = true;
-    conform-nvim.settings = {
-      formatters_by_ft.markdown = [
-        # "mdformat"
-        # "injected"
-      ];
-      formatters = {
-        mdformat = {
-          "inherit" = true;
-          prepend_args = [ "--number" ];
-        };
-      };
-    };
-  };
+  plugins.lsp.servers.marksman.enable = true;
 }
