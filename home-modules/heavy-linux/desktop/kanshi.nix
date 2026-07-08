a386f0ab08faab8853b71afa8e1db7a4525b0605
Arsen Nuramatov <x@ctrl.sn>
2026-07-08 20:19:36 +0200

    kanshi: chore

--- home-modules/heavy-linux/desktop/kanshi.nix
+++ home-modules/heavy-linux/desktop/kanshi.nix
@@ -48,20 +48,22 @@ in
 
             profiles =
               let
+                # NOTE negative/larger y values break xwayland
+                extPos = "0,9999";
                 home = {
                   criteria = "Dell Inc. DELL S2722QC 192SH24";
                   scale = 1.5;
                   adaptiveSync = false;
-                  position = "0,9999";
+                  position = extPos;
                 };
                 generic = {
                   criteria = "*";
-                  position = "0,9999";
+                  position = extPos;
                 };
                 audimax = {
                   criteria = "PNP(LTM) RallyBar Mini 0x88888800";
                   scale = 1.0;
-                  position = "0,9999";
+                  position = extPos;
                 };
                 builtinDisplay = cfg.builtinDisplay // {
                   position = "0,0";
