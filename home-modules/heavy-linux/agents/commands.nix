f17d4e073444edfa30fab299c3ded4f0b933fa25
Arsen Nuramatov <x@ctrl.sn>
2026-07-08 19:12:58 +0200

    agents: update proompt

--- home-modules/heavy-linux/agents/commands.nix
+++ home-modules/heavy-linux/agents/commands.nix
@@ -29,21 +29,12 @@ in
       '';
     };
     iterate_spec = rec {
-      description = "refine feature specification/implementation plan in files like SPEC.md/PLAN.md";
+      description = "refine feature specification/implementation plan/design document/SPEC.md/PLAN.md";
       withFM = prependFrontmatter text;
       text = ''
-        Specification describes a feature that was not yet implemented.
-
-        Properties of a good spec:
-
-        - internal consistency: spec should not contradict itself
-        - consistency with existing code: spec should not make incorrect assumptions about existing behaviour
-        - completeness of decription/deterministic result: there should be one obvious way to implement the spec
-        - quality of the end result: resulting implementation should be maintainable and correct
-
-        Your task is to review the specification for implementation risks. You
-        may inspect relevant parts of the codebase if needed to assess conflicts
-        with existing behaviour.
+        Specification describes a feature that was not yet implemented. Your
+        task is to review the specification and report to user. Do not edit the
+        files.
 
         Identify and report:
 
@@ -54,12 +45,12 @@ in
         - inconsistent naming or terminology
         - anything else that is likely to cause problems in the future: bugs, maintainability issues, etc.
 
-        Note:
+        Properties of a good spec:
 
-        If the spec references components that don't exist, assume that they are
-        a part of the feature descibed by the spec. If it's not already obvious
-        from the spec, you must update the document to explicitly mention that
-        they are a part of the feature.
+        - internal consistency: spec should not contradict itself
+        - consistency with existing code: spec should not make incorrect assumptions about existing behaviour
+        - completeness of decription/deterministic result: there should be one obvious way to implement the spec
+        - quality of the end result: resulting implementation should be maintainable and correct
       '';
 
     };
