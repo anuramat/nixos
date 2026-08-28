6faabbab1e1137f60e2d5f4a6a71010efa24edf1
Arsen Nuramatov <x@ctrl.sn>
2026-08-28 14:56:51 +0200

    zsa: use upstream option

--- nixos-modules/local/peripherals.nix
+++ nixos-modules/local/peripherals.nix
@@ -12,6 +12,7 @@ in
   hardware = {
     # openrazer.enable = true;
     openrazer.users = [ username ];
+    keyboard.zsa.enable = true;
   };
   environment.systemPackages = with pkgs; [
     android-tools
@@ -23,14 +24,6 @@ in
     rpi-imager # raspbery pi
   ];
   services = {
-    # ZSA Voyager
-    udev.extraRules = ''
-      # Rules for Oryx web flashing and live training
-      KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
-      KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
-      # Keymapp Flashing rules for the Voyager
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
-    '';
     hardware.openrgb.enable = true;
   };
 }
